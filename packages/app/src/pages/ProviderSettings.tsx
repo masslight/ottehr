ca42093e115049b7f82c142c565911b4ad994b54
Samir Omarov <samir.omarov@gmail.com>
2023-10-03 17:03:19 +0200

    refactor components to better align, profile set.

--- packages/app/src/pages/ProviderSettings.tsx
+++ packages/app/src/pages/ProviderSettings.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import {
+  Button,
+  TextField,
+  Box,
+  Container,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Typography,
+} from '@mui/material';
+
 import { Footer } from '../components';
-import { Container, Box, Grid, Typography, Button } from '@mui/material';
-import defaultProvider from '../assets/icons/defaultProvider.png';
-import { useTranslation } from 'react-i18next';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import TopAppBar from '../components/AppBar';
+import ProviderHeaderSection from '../components/ProviderHeaderSection';
+import { useState } from 'react';
+import CheckIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const ProviderSettings = (): JSX.Element => {
-  const { t } = useTranslation();
+  const handleSubmit = (event: any): void => {
+    event.preventDefault();
+    // TODO: form submission structure
+  };
+
+  const mockData = ['aykhanahmadli', 'samiromarov'];
+  const [roomName, setRoomName] = useState('samir');
+
+  const isError = mockData.includes(roomName);
+  const helperText = isError ? 'This name is already taken, please use another one' : '';
 
   return (
     <Container
@@ -22,93 +41,62 @@ const ProviderSettings = (): JSX.Element => {
       }}
     >
       <TopAppBar />
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'row' }}>
-        <Box
-          sx={{
-            width: '60%',
-            flexGrow: 1,
-            backgroundColor: 'transparent',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            pt: 10,
-            pl: 10,
-            pr: 10,
-          }}
-        >
-          <Grid container>
-            <Grid item xs={6}>
-              <Typography variant="h5" color="primary.light" fontWeight={500}>
-                {t('general.goodMorning')}
-              </Typography>
+      <ProviderHeaderSection providerName="Dr. Olivia Smith" title="My profile" />
 
-              <Typography variant="h4" color="text.light" mt={1}>
-                Dr. Olivia Smith
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              sx={{
-                textAlign: 'right',
-              }}
-            >
-              <img src={defaultProvider} alt="Provider Image" width="100px" />
-            </Grid>
-          </Grid>
-          <Box
-            sx={{
-              width: '100%',
-              backgroundColor: '#e5f2f8',
-              borderRadius: 1,
-              margin: 3,
-              p: 3,
-              boxSizing: 'border-box',
-            }}
-          >
-            <Typography variant="body1" color="text.light">
-              {t('general.shareLink')}
-            </Typography>
+      <Box
+        sx={{
+          flex: '1 1 auto',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          p: 5,
+        }}
+      >
+        <Box maxWidth="md" width="100%">
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', mx: 12.5 }}>
+            <form onSubmit={handleSubmit}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', gap: 2 }}>
+                <FormControl variant="outlined">
+                  <InputLabel>Title</InputLabel>
+                  <Select label="Title">
+                    <MenuItem value="dr">Dr.</MenuItem>
+                    <MenuItem value="nurse">Nurse</MenuItem>
+                    <MenuItem value="assistant">Assistant</MenuItem>
+                  </Select>
+                </FormControl>
+                <TextField variant="outlined" label="First Name" />
+                <TextField variant="outlined" label="Last Name" />
+                <TextField
+                  variant="outlined"
+                  label="Room Name (slug)"
+                  value={roomName}
+                  onChange={(e) => setRoomName(e.target.value)}
+                  error={isError}
+                  helperText={helperText}
+                />
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Box sx={{ mr: '1' }}>{isError ? <CancelIcon color="error" /> : <CheckIcon color="success" />}</Box>
+                  <Typography variant="body2">{`https://zapehr.app/${roomName}`}</Typography>
+                </Box>
 
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'flex-start',
-                gap: 2,
-                marginTop: 2,
-              }}
-            >
-              <Button variant="contained" color="primary" startIcon={<ContentCopyIcon />}>
-                {t('general.copyLink')}
-              </Button>
-              <Button variant="outlined" color="primary" startIcon={<MailOutlineIcon />}>
-                {t('general.sendEmail')}
-              </Button>
-            </Box>
+                <TextField variant="outlined" label="Email Address" />
+
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{
+                    width: '100%',
+                    backgroundColor: 'primary.main',
+                    color: 'white',
+                    py: 1.5,
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  Update
+                </Button>
+              </Box>
+            </form>
           </Box>
-        </Box>
-
-        <Box
-          sx={{
-            width: '40%',
-            flexGrow: 1,
-            background: 'linear-gradient(21deg, rgba(40, 150, 198, 0.60) 3.6%, rgba(80, 96, 241, 0.00) 40%), #263954',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-start',
-            pt: 10,
-            pl: 10,
-            pr: 10,
-          }}
-        >
-          <Typography variant="h5" color="primary.light" fontWeight={500}>
-            {t('general.patientsQueue')}
-          </Typography>
-
-          <Typography variant="body2" sx={{ opacity: 0.6 }} color="primary.contrast" fontWeight={500}>
-            {t('general.waiting')}
-          </Typography>
         </Box>
       </Box>
       <Footer />
