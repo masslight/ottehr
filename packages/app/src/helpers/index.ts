9be99b443d20686e18a9b885cf2f36a03e0e96fb
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2023-10-10 13:59:36 +0100

    Clean up helpers files

--- packages/app/src/helpers/index.ts
+++ packages/app/src/helpers/index.ts
@@ -1,13 +1,5 @@
-export { CustomAdapterLuxon } from './CustomAdapterLuxon';
+export * from './CustomAdapterLuxon';
 export * from './dateUtils';
-export { findLabelFromOptions, findValueFromOptions } from './findPropertyFromOptions';
-export { stateNames } from './stateNames';
-export {
-  ageIsInRange,
-  cardExpirationRegex,
-  MINIMUM_AGE,
-  MAXIMUM_AGE,
-  phoneRegex,
-  stateRegex,
-  zipRegex,
-} from './validation';
+export * from './findPropertyFromOptions';
+export * from './stateNames';
+export * from './validation';
