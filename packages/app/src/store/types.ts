5ae9eb0c86d0f9a013de036c1b5b596fc5f316c1
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2023-10-10 17:04:26 +0100

    Remove cross-package imports

--- packages/app/src/store/types.ts
+++ packages/app/src/store/types.ts
@@ -14,35 +14,11 @@ export enum AdditionalInformationOptions {
   'Drive by/Signage' = 'Drive by/Signage',
 }
 
-export enum CancellationReasonCodes {
-  'Duplicate Visit or Account Error' = 'duplicate-visit-account-error',
-  'Financial Responsibility Concern' = 'financial-concern',
-  'Non-par or inactive insurance' = 'inactive-insurance',
-  'Patient improved' = 'patient-improved',
-  'Wait Time' = 'wait-time',
-  'Went to outside facility' = 'outside-facility',
-}
-
-export enum CancellationReasonOptions {
-  'Duplicate Visit or Account Error' = 'Duplicate Visit or Account Error',
-  'Financial Responsibility Concern' = 'Financial Responsibility Concern',
-  'Non-par or inactive insurance' = 'Non-par or inactive insurance',
-  'Patient improved' = 'Patient improved',
-  'Wait Time' = 'Wait Time',
-  'Went to outside facility' = 'Went to outside facility',
-}
-
 export enum PatientEthnicity {
   'Hispanic/Latino' = 'Hispanic/Latino',
   'Not Hispanic/Latino' = 'Not Hispanic/Latino',
   'Prefer not to answer' = 'Prefer not to answer',
 }
-
-export const PatientEthnicityCode = {
-  'Hispanic/Latino': '2135-2',
-  'Not Hispanic/Latino': '2186-5',
-  'Prefer not to answer': undefined,
-};
 
 export enum PatientRace {
   'American Indian or Alaskan Native' = 'American Indian or Alaskan Native',
@@ -54,17 +30,6 @@ export enum PatientRace {
   'Unknown' = 'Unknown',
   'White' = 'White',
 }
-
-export const PatientRaceCode = {
-  'American Indian or Alaskan Native': '1002-5',
-  Asian: '2028-9',
-  'Black or African American': '2054-5',
-  'Hawaiian or Pacific Islander': '2076-8',
-  Other: '2131-1',
-  'Prefer not to answer': undefined,
-  Unknown: undefined,
-  White: '2106-3',
-};
 
 export enum PatientSex {
   Male = 'male',
