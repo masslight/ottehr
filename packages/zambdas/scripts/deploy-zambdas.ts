73c93f19d42829225eaf552d10568927f3eb0421
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2023-10-16 13:03:05 +0100

    Reduce code duplication in api url generation

--- packages/zambdas/scripts/deploy-zambdas.ts
+++ packages/zambdas/scripts/deploy-zambdas.ts
@@ -1,13 +1,14 @@
+import { FhirClient, ZambdaClient } from '@zapehr/sdk';
+import { Operation } from 'fast-json-patch';
+import { Subscription } from 'fhir/r4';
 import fs from 'fs';
+import fetch from 'node-fetch';
 import devConfig from '../.env/dev.json';
 // import testingConfig from '../.env/testing.json';
 // import stagingConfig from '../.env/staging.json';
 // import productionConfig from '../.env/production.json';
 import { getAuth0Token } from '../src/shared';
-import { FhirClient, ZambdaClient } from '@zapehr/sdk';
-import { Subscription } from 'fhir/r4';
-import { Operation } from 'fast-json-patch';
-import fetch from 'node-fetch';
+import { fhirApiUrlFromAuth0Audience, projectApiUrlFromAuth0Audience } from './common';
 
 interface DeployZambda {
   type: 'http_open' | 'http_auth' | 'subscription';
@@ -62,41 +63,6 @@ const ZAMBDAS: { [name: string]: DeployZambda } = {
     event: undefined,
     criteria: 'Patient',
   },
-};
-
-const fhirApiUrlFromAuth0Audience = (auth0Audience: string): string => {
-  switch (auth0Audience) {
-    case 'https://dev.api.zapehr.com':
-      return 'https://dev.fhir-api.zapehr.com';
-    case 'https://dev2.api.zapehr.com':
-      return 'https://dev2.fhir-api.zapehr.com';
-    case 'https://testing.api.zapehr.com':
-      return 'https://testing.fhir-api.zapehr.com';
-    case 'https://staging.api.zapehr.com':
-      return 'https://staging.fhir-api.zapehr.com';
-    case 'https://api.zapehr.com':
-      return 'https://fhir-api.zapehr.com';
-    default:
-      throw `Unexpected auth0 audience value, could not map to a projectApiUrl. auth0Audience was: ${auth0Audience}`;
-  }
-};
-
-// todo remove code duplication with configure-zapehr-secrets
-const projectApiUrlFromAuth0Audience = (auth0Audience: string): string => {
-  switch (auth0Audience) {
-    case 'https://dev.api.zapehr.com':
-      return 'https://dev.project-api.zapehr.com/v1';
-    case 'https://dev2.api.zapehr.com':
-      return 'https://dev2.project-api.zapehr.com/v1';
-    case 'https://testing.api.zapehr.com':
-      return 'https://testing.project-api.zapehr.com/v1';
-    case 'https://staging.api.zapehr.com':
-      return 'https://staging.project-api.zapehr.com/v1';
-    case 'https://api.zapehr.com':
-      return 'https://project-api.zapehr.com/v1';
-    default:
-      throw `Unexpected auth0 audience value, could not map to a projectApiUrl. auth0Audience was: ${auth0Audience}`;
-  }
 };
 
 const updateZambdas = async (config: any): Promise<void> => {
