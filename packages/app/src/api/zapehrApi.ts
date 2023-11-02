d28d49a8f1e6c586419b7b96643dcfb7428190b6
Samir Omarov <samir.omarov@gmail.com>
2023-11-03 00:52:00 +0100

    remove redundant api var

--- packages/app/src/api/zapehrApi.ts
+++ packages/app/src/api/zapehrApi.ts
@@ -1,113 +1,16 @@
+import { ZambdaClient } from '@zapehr/sdk';
+import { apiErrorToThrow } from './apiErrorToThrow';
+
 export interface ZapehrSearchParameter {
   key: string;
   value: string;
 }
 import { Encounter } from 'fhir/r4';
-const CHECK_IN_ZAMBDA_ID = import.meta.env.VITE_CHECK_IN_ZAMBDA_ID;
-const GET_APPOINTMENTS_ZAMBDA_ID = import.meta.env.VITE_GET_APPOINTMENTS_ZAMBDA_ID;
-const GET_PATIENTS_ZAMBDA_ID = import.meta.env.VITE_GET_PATIENTS_ZAMBDA_ID;
+// TODO: add env
 // const TELEMED_API_URL = import.meta.env.VITE_TELEMED_API_URL;
 const TELEMED_API_URL = 'http://localhost:3301/local/zambda';
 
 class API {
-  async checkIn(zambdaClient: ZambdaClient, appointmentId: string): Promise<any> {
-    try {
-      if (CHECK_IN_ZAMBDA_ID == null) {
-        throw new Error('check in environment variable could not be loaded');
-      }
-      const response = await zambdaClient?.invokePublicZambda({
-        payload: { appointment: appointmentId },
-        zambdaId: CHECK_IN_ZAMBDA_ID,
-      });
-      return response;
-    } catch (error: unknown) {
-      throw apiErrorToThrow(error);
-    }
-  }
-
-  async getPatients(zambdaClient: ZambdaClient): Promise<any> {
-    try {
-      if (GET_PATIENTS_ZAMBDA_ID == null) {
-        throw new Error('get patients environment variable could not be loaded');
-      }
-
-      const response = await zambdaClient?.invokeZambda({
-        zambdaId: GET_PATIENTS_ZAMBDA_ID,
-      });
-      return response;
-    } catch (error: unknown) {
-      throw apiErrorToThrow(error);
-    }
-  }
-
-  async getAppointments(zambdaClient: ZambdaClient): Promise<any> {
-    try {
-      if (GET_APPOINTMENTS_ZAMBDA_ID == null) {
-        throw new Error('get appointments environment variable could not be loaded');
-      }
-
-      const response = await zambdaClient?.invokeZambda({
-        zambdaId: GET_APPOINTMENTS_ZAMBDA_ID,
-      });
-      return response;
-    } catch (error: unknown) {
-      throw apiErrorToThrow(error);
-    }
-  }
-
-  async createZ3Object(url: string, file: File, token: string): Promise<any> {
-    try {
-      // Create the Z3 object
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        method: 'PUT',
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to create object');
-      }
-      const json = await response.json();
-      const presignedUrl = json.signedUrl;
-
-      // Upload the file to S3
-      const uploadResponse = await fetch(presignedUrl, {
-        body: file,
-        headers: {
-          'Content-Type': file.type,
-        },
-        method: 'PUT',
-      });
-
-      // console.log('res: ', uploadResponse);
-      if (!uploadResponse.ok) {
-        throw new Error('Failed to upload file');
-      }
-
-      return uploadResponse.ok;
-    } catch (error: unknown) {
-      throw apiErrorToThrow(error);
-    }
-  }
-
-  async getZ3Object(url: string, token: string): Promise<string> {
-    try {
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        method: 'GET',
-      });
-
-      const json = await response.json();
-      const signedUrl = json.signedUrl;
-      return signedUrl;
-    } catch (error: unknown) {
-      throw apiErrorToThrow(error);
-    }
-  }
-
   async createTelemedRoom(): Promise<Encounter | null> {
     try {
       const response = await fetch(`${TELEMED_API_URL}/telemed-room/execute-public`, {
