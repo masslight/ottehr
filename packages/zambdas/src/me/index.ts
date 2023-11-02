e0dadd6feb0a125b8fe2ba06c37da2f9c37ec821
aykhanahmadli <botl12020@gmail.com>
2023-11-02 03:05:21 +0100

    feat: test for provider register zambda

--- packages/zambdas/src/me/index.ts
+++ packages/zambdas/src/me/index.ts
@@ -9,43 +9,43 @@ export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> 
     const url = 'https://project-api.zapehr.com/v1/project';
     const response = await fetch(url, {
       body: JSON.stringify({
-        name: 'SANDBOX',
-        description: 'updated project description',
-        signupEnabled: true,
         defaultPatientAccessPolicy: {
           rule: [
             {
-              resource: ['FHIR:Patient:*'],
               action: ['FHIR:Read', 'FHIR:Search'],
               effect: 'Allow',
+              resource: ['FHIR:Patient:*'],
             },
           ],
         },
+        description: 'updated project description',
+        name: 'SANDBOX',
+        signupEnabled: true,
       }),
-      method: 'PUT',
       headers: {
         accept: 'application/json',
-        'content-type': 'application/json',
         authorization: `Bearer ${accessToken}`,
+        'content-type': 'application/json',
       },
+      method: 'PUT',
     });
     const data = await response.json();
     console.log(data);
     if (!accessToken) {
       return {
-        statusCode: 404,
         body: JSON.stringify({ error: 'User not found' }),
+        statusCode: 404,
       };
     }
 
     return {
-      statusCode: 200,
       body: JSON.stringify(response),
+      statusCode: 200,
     };
   } catch (error) {
     return {
-      statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' }),
+      statusCode: 500,
     };
   }
 };
--- packages/zambdas/src/registration/index.ts
+++ packages/zambdas/src/registration/index.ts
@@ -1,29 +1,183 @@
+/* eslint-disable sort-keys */
 import { APIGatewayProxyResult } from 'aws-lambda';
-import { ZambdaInput, ProviderInfo } from '../types';
-import { getAuth0Token } from '../shared/getAuth0Token';
+import fetch from 'node-fetch'; // Make sure to install this package if not already installed
+import { createZambdaFromSkeleton } from '../shared/zambdaSkeleton';
+import { ZambdaFunctionInput, ZambdaFunctionResponse, ZambdaInput } from '../types';
+import { getAuth0Token } from '../shared';
 
-export const register = async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+  return createZambdaFromSkeleton(input, postProviderProfile);
+};
+
+// interface PostProviderProfileInput {
+//   checkboxes: boolean;
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+//   slug: string;
+//   title: 'Mr' | 'Ms' | 'Mrs' | 'Dr' | 'Nurse' | 'Assistant';
+// }
+
+const postProviderProfile = async (input: ZambdaFunctionInput): Promise<ZambdaFunctionResponse> => {
+  const { secrets } = input;
+  // const { checkboxes, email, firstName, lastName, slug, title } = body as PostProviderProfileInput;
+
+  const authToken = await getAuth0Token(secrets);
+
+  console.log(authToken);
+
+  // const patientResource = {
+  //   resourceType: 'Patient',
+  //   id: '00000000-0000-0000-0000-000000000000',
+  //   text: {
+  //     status: 'generated',
+  //     div: '<div xmlns="http://www.w3.org/1999/xhtml"> Patient MINT_TEST, ID = MINT1234. Age = 56y, Size =\n      1.83m, Weight = 72.58kg </div>',
+  //   },
+  //   extension: [
+  //     {
+  //       url: 'http://nema.org/fhir/extensions#0010:1010',
+  //       valueQuantity: {
+  //         value: 56,
+  //         unit: 'Y',
+  //       },
+  //     },
+  //     {
+  //       url: 'http://nema.org/fhir/extensions#0010:1020',
+  //       valueQuantity: {
+  //         value: 1.83,
+  //         unit: 'm',
+  //       },
+  //     },
+  //     {
+  //       url: 'http://nema.org/fhir/extensions#0010:1030',
+  //       valueQuantity: {
+  //         value: 72.58,
+  //         unit: 'kg',
+  //       },
+  //     },
+  //   ],
+  //   identifier: [
+  //     {
+  //       system: 'http://nema.org/examples/patients',
+  //       value: 'MINT1234',
+  //     },
+  //   ],
+  //   active: true,
+  //   name: [
+  //     {
+  //       family: 'MINT_TEST',
+  //     },
+  //   ],
+  //   gender: 'male',
+  //   _gender: {
+  //     extension: [
+  //       {
+  //         url: 'http://nema.org/examples/extensions#gender',
+  //         valueCoding: {
+  //           system: 'http://nema.org/examples/gender',
+  //           code: 'M',
+  //         },
+  //       },
+  //     ],
+  //   },
+  //   managingOrganization: {
+  //     reference: 'Organization/1',
+  //   },
+  //   meta: {
+  //     versionId: '00000000-0000-0000-0000-000000000000',
+  //     lastUpdated: '2023-01-01T00:00:00.000Z',
+  //   },
+  // };
+
   try {
-    const accessToken = await getAuth0Token(input.secrets);
-    const providerInfo: ProviderInfo = JSON.parse(input.body || '{}');
+    const response = await fetch(`https://fhir-api.zapehr.com/r4/Patient`, {
+      body: JSON.stringify({
+        resourceType: 'Patient',
+        id: '00000000-0000-0000-0000-000000000000',
+        text: {
+          status: 'generated',
+          div: '<div xmlns="http://www.w3.org/1999/xhtml"> Patient MINT_TEST, ID = MINT1234. Age = 56y, Size =1.83m, Weight = 72.58kg </div>',
+        },
+        extension: [
+          {
+            url: 'http://nema.org/fhir/extensions#0010:1010',
+            valueQuantity: {
+              value: 56,
+              unit: 'Y',
+            },
+          },
+          {
+            url: 'http://nema.org/fhir/extensions#0010:1020',
+            valueQuantity: {
+              value: 1.83,
+              unit: 'm',
+            },
+          },
+          {
+            url: 'http://nema.org/fhir/extensions#0010:1030',
+            valueQuantity: {
+              value: 72.58,
+              unit: 'kg',
+            },
+          },
+        ],
+        identifier: [
+          {
+            system: 'http://nema.org/examples/patients',
+            value: 'MINT1234',
+          },
+        ],
+        active: true,
+        name: [
+          {
+            family: 'MINT_TEST',
+          },
+        ],
+        gender: 'male',
+        _gender: {
+          extension: [
+            {
+              url: 'http://nema.org/examples/extensions#gender',
+              valueCoding: {
+                system: 'http://nema.org/examples/gender',
+                code: 'M',
+              },
+            },
+          ],
+        },
+        managingOrganization: {
+          reference: 'Organization/1',
+        },
+        meta: {
+          versionId: '00000000-0000-0000-0000-000000000000',
+          lastUpdated: '2023-01-01T00:00:00.000Z',
+        },
+      }),
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
 
-    // const result = await registerUser(accessToken, providerInfo); // to do register function
-
-    // if (!result.success) {
-    //   return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({ error: 'Registration failed' }),
-    //   };
-    // }
-
+    console.log('response', response);
+    if (response.status === 201) {
+      const responseData = await response.json();
+      return {
+        error: undefined,
+        response: responseData,
+      };
+    } else {
+      const errorData = await response.json();
+      return {
+        error: errorData,
+        response: undefined,
+      };
+    }
+  } catch (error: any) {
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Registration successful' }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
+      error: error.message,
+      response: undefined,
     };
   }
 };
