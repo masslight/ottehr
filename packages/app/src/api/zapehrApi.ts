2ab360cef2b58f2cd021d82405353b607f66a4da
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2023-11-06 15:14:21 -0500

    Add getSlugAvailabilityError api call

--- packages/app/src/api/zapehrApi.ts
+++ packages/app/src/api/zapehrApi.ts
@@ -6,8 +6,28 @@ import { Encounter } from 'fhir/r4';
 // TODO: add env
 // const TELEMED_API_URL = import.meta.env.VITE_TELEMED_API_URL;
 const TELEMED_API_URL = 'http://localhost:3301/local/zambda';
+const PROJECT_API_URL = import.meta.env.VITE_PROJECT_API_URL;
 
 class API {
+  async getSlugAvailabilityError(slug: string): Promise<string> {
+    try {
+      const GET_SLUG_AVAILABILITY_ZAMBDA_ID = import.meta.env.VITE_GET_SLUG_AVAILABILITY_ZAMBDA_ID;
+      const responseBody = await callZambda(GET_SLUG_AVAILABILITY_ZAMBDA_ID, 'open', { slug });
+      if (responseBody.output.error) {
+        return responseBody.output.error;
+      }
+      const available = responseBody.output.response?.available as boolean;
+      if (available) {
+        return '';
+      } else {
+        return 'This slug is already taken, please use another one.';
+      }
+    } catch (error) {
+      console.error('Error checking availability:', error);
+      return 'An unexpected error occurred. Please try again.';
+    }
+  }
+
   async createTelemedRoom(): Promise<Encounter | null> {
     try {
       const response = await fetch(`${TELEMED_API_URL}/telemed-room/execute-public`, {
@@ -81,3 +101,29 @@ class API {
 }
 
 export const zapehrApi = new API();
+
+export interface ZambdaFunctionResponse {
+  output: {
+    error?: string;
+    response?: Record<string, any>;
+  };
+}
+async function callZambda(zambdaId: string, auth: 'auth' | 'open', body: object): Promise<ZambdaFunctionResponse> {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  const response = await fetch(`${PROJECT_API_URL}/zambda/${zambdaId}/execute${auth === 'open' ? '-public' : ''}`, {
+    body: JSON.stringify(body),
+    headers: auth
+      ? {
+          ...headers,
+          // Putting the ternary here annoys TS
+          Authorization: `Bearer ${'token'}`,
+        }
+      : headers,
+    method: 'POST',
+  });
+  return await response.json();
+}
