59463612a86c0409ac7bac0670d8615ccdec8b52
Nathan Greene <github@mail.robinhart.me>
2023-11-10 13:04:15 -0800

    Removed unused code

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -43,64 +43,6 @@ export default function App(): JSX.Element {
     });
   }
 
-  // useEffect(() => {
-  //   const fetchAccessToken = async (): Promise<void> => {
-  //     try {
-  //       console.log('auth', isAuthenticated);
-  //       if (isLoading && isAuthenticated) {
-  //         const token = await getAccessTokenSilently();
-  //         console.log('Access token:', token);
-  //       } else {
-  //         setTimeout(async () => {
-  //           await loginWithRedirect();
-  //         }, 1000);
-  //       }
-  //     } catch (error) {
-  //       console.error('Error occurred while fetching the access token:', error);
-  //     }
-  //   };
-  //   setTimeout(async () => {
-  //     fetchAccessToken()
-  //       .then(() => {
-  //         console.log('Access token fetched successfully');
-  //       })
-  //       .catch((error) => {
-  //         console.error('Error fetching access token:', error);
-  //       });
-  //   }, 5000);
-  // }, [isLoading, isAuthenticated, getAccessTokenSilently, loginWithRedirect]);
-
-  // useEffect(() => {
-  //   const fetchUserDetails = async (): Promise<void> => {
-  //     if (isAuthenticated) {
-  //       try {
-  //         const token = await getAccessTokenSilently();
-  //         console.log('token is', token);
-  //         axios
-  //           .get('http://localhost:3301/local/zambda/get-slug-availability/execute-public', {
-  //             headers: {
-  //               Authorization: `Bearer ${token}`,
-  //             },
-  //           })
-  //           .then((response) => {
-  //             if (response.status === 404) {
-  //               console.log('user not found');
-  //             }
-  //           })
-  //           .catch((error) => {
-  //             console.error('An error occurred:', error);
-  //           });
-  //       } catch (e) {
-  //         console.error('Error while getting the access token:', e);
-  //       }
-  //     }
-  //   };
-
-  //   fetchUserDetails().catch((error) => {
-  //     console.error('An error occurred while fetching user details:', error);
-  //   });
-  // }, [isAuthenticated, getAccessTokenSilently]);
-
   return (
     <OttehrThemeProvider>
       <Router>
