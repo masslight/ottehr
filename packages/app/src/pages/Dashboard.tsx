9ebea93b8f1fbf5f30ceeea6c4461683d3468da5
Samir Omarov <samir.omarov@gmail.com>
2023-12-13 16:49:17 +0100

    lint fixes

--- packages/app/src/pages/Dashboard.tsx
+++ packages/app/src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ import { defaultProvider } from '../assets/icons';
 import { CustomButton, Footer, LoadingSpinner, PatientQueue, PatientQueueProps, TopAppBar } from '../components';
 import { createProviderName, createSlugUrl } from '../helpers';
 import { JSX } from 'react/jsx-runtime';
-import { usePractitioner, useVideoParticipant } from '../store';
+import { usePractitioner } from '../store';
 import { useAuth0 } from '@auth0/auth0-react';
 import { getPatientQueue } from '../api';
 import { queueNotification } from '../assets/sounds';
