5671930243fc7cf6edcd40284512ce6ace6f0758
aykhanahmadli <botl12020@gmail.com>
2023-12-14 23:31:20 +0100

    style: refactor

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -12,33 +12,24 @@ export default function App(): JSX.Element {
         <VideoParticipantProvider>
           <Routes>
             <Route element={<PractitionerProvider />}>
-              <Route element={<Version />} path="/" />
-              <Route
-                element={
-                  <PrivateRoute>
-                    <Dashboard />
-                  </PrivateRoute>
-                }
-                path={'/dashboard'}
-              />
-              <Route
-                element={
-                  <PrivateRoute>
-                    <Profile />
-                  </PrivateRoute>
-                }
-                path={'/profile'}
-              />
-            </Route>
-            <Route element={<ParticipantProvider />}>
-              <Route element={<VideoChatPage />} path={'/video-call'} />;
-              <Route element={<VideoSettings />} path={'/video-settings'} />;
-              <Route element={<WaitingRoom />} path={'/waiting-room'} />;
-              <Route element={<CheckIn />} path={'/:slug'} />;
-            </Route>
-            <Route element={<ParticipantProvider />}>
-              <Route element={<PractitionerProvider />}>
-                <Route element={<PostCall />} path={'/patient-post-call'} />;
+              <Route element={<ParticipantProvider />}>
+                <Route element={<Version />} path="/" />
+                <Route
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                  path={'/dashboard'}
+                />
+                <Route
+                  element={
+                    <PrivateRoute>
+                      <Profile />
+                    </PrivateRoute>
+                  }
+                  path={'/profile'}
+                />
                 <Route
                   element={
                     <PrivateRoute>
@@ -47,6 +38,11 @@ export default function App(): JSX.Element {
                   }
                   path={'/provider-post-call'}
                 />
+                <Route element={<VideoChatPage />} path={'/video-call'} />;
+                <Route element={<VideoSettings />} path={'/video-settings'} />;
+                <Route element={<WaitingRoom />} path={'/waiting-room'} />;
+                <Route element={<CheckIn />} path={'/:slug'} />;
+                <Route element={<PostCall />} path={'/patient-post-call'} />;
               </Route>
             </Route>
           </Routes>
