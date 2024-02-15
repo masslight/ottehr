5ca7bd896ffe442efc93200b3f7f9570892c3ef0
saewitz <jonathansaewitz@gmail.com>
2024-02-14 19:26:39 -0500

    test

--- packages/ehr/zambdas/src/get-token-for-conversation/index.ts
+++ packages/ehr/zambdas/src/get-token-for-conversation/index.ts
@@ -11,7 +11,7 @@ export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> 
     const response: any = {};
     const AUTH0_ENDPOINT = getSecret(SecretsKeys.AUTH0_ENDPOINT, secrets);
     const AUTH0_AUDIENCE = getSecret(SecretsKeys.AUTH0_AUDIENCE, secrets);
-    if (true) {
+    if (!zapehrMessagingToken) {
       console.log('getting messaging token first');
       zapehrMessagingToken = await getAuth0Token(secrets, 'messaging');
       const twilioTokenRequest = await fetch(
@@ -23,7 +23,6 @@ export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> 
         },
       );
       const twilioTokenRequestJson = (await twilioTokenRequest.json()) as { token: string };
-      console.log(twilioTokenRequestJson);
       twilioMessagingToken = twilioTokenRequestJson.token;
     } else {
       console.log('already have messaging token first');
