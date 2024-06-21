4e31672fe933f3f62a38bdc6260bc0201d27415a
saewitz <jonathansaewitz@gmail.com>
2024-06-21 15:19:55 -0400

    # This is a combination of 17 commits. # This is the 1st commit message:
    fix build errors and create deploy file
    
    # This is the commit message #2:
    
    remove vercel from deploy yml
    
    # This is the commit message #3:
    
    fix build error related to vercel front end
    
    # This is the commit message #4:
    
    configure zapehr secrets for telemed intake zambdas
    
    # This is the commit message #5:
    
    fix deploy zambdas for demo env
    
    # This is the commit message #6:
    
    fix package json
    
    # This is the commit message #7:
    
    fix zapehr env error
    
    # This is the commit message #8:
    
    checkout the env variables from secrets repo
    
    # This is the commit message #9:
    
    update the private ssh keys
    
    # This is the commit message #10:
    
    configure deploy zambdas
    
    # This is the commit message #11:
    
    fix typo
    
    # This is the commit message #12:
    
    fix the directory for the build
    
    # This is the commit message #13:
    
    add build
    
    # This is the commit message #14:
    
    configure build with the deploy
    
    # This is the commit message #15:
    
    configure build file without pushd
    
    # This is the commit message #16:
    
    create separate build file for intake
    
    # This is the commit message #17:
    
    schedule types fix

--- scripts/build-telemed-intake.sh
+++ scripts/build-telemed-intake.sh
@@ -19,18 +19,6 @@ echo "pnpm version: $(pnpm --version)"
 echo "Installing dependencies..."
 pnpm install
 
-# Build the telemed-ehr/app
-echo "Building package: telemed-ehr/app"
-cd "packages/telemed-ehr/app"
-pnpm build
-cd ../../../
-
-# Build the telemed-ehr/zambdas
-echo "Building package: telemed-ehr/zambdas"
-cd "packages/telemed-ehr/zambdas"
-pnpm build
-cd ../../../
-
 # Build the telemed-intake/app
 echo "Building package: telemed-intake/app"
 cd "packages/telemed-intake/app"
@@ -40,18 +28,6 @@ cd ../../../
 # Build the telemed-intake/zambdas
 echo "Building package: telemed-intake/zambdas"
 cd "packages/telemed-intake/zambdas"
-pnpm build
-cd ../../../
-
-# Build the urgent-care/app
-echo "Building package: urgent-care/app"
-cd "packages/urgent-care-intake/app"
-pnpm build
-cd ../../../
-
-# Build the urgent-care/zambdas
-echo "Building package: urgent-care/zambdas"
-cd "packages/urgent-care-intake/zambdas"
 pnpm build
 cd ../../../
 
