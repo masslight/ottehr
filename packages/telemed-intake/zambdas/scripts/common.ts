a9505a2a2eb65bfd29821852faf335bfdadd4e19
saewitz <jonathansaewitz@gmail.com>
2024-06-21 15:19:32 -0400

    # This is a combination of 7 commits. # This is the 1st commit message:
    fix build errors and create deploy file
    
    # This is the commit message #2:
    
    remove vercel from deploy yml
    
    # This is the commit message #3:
    
    fix build error related to vercel front end
    
    # This is the commit message #4:
    
    configure zapehr secrets for telemed intake zambdas
    
    # This is the commit message #5:
    
    fix deploy zambdas for demo env
    
    # This is the commit message #6:
    
    fix package json
    
    # This is the commit message #7:
    
    fix zapehr env error

--- packages/telemed-intake/zambdas/scripts/common.ts
+++ packages/telemed-intake/zambdas/scripts/common.ts
@@ -1,15 +1,18 @@
-// import devConfig from '../.env/dev.json';
+import devConfig from '../.env/dev.json';
 // import testingConfig from '../.env/testing.json';
 import { FhirClient, ZambdaClient } from '@zapehr/sdk';
 import { getM2MClientToken } from '../src/shared';
 
 export const performEffectWithEnvFile = async (callback: (config: any) => void) => {
-  const env = process.argv[2];
+  console.log(process.argv, 'process.argv');
+  const env = process.env.ENV;
+
+  console.log(env, 'env');
 
   switch (env) {
-    // case 'dev':
-    //   await callback(devConfig);
-    //   break;
+    case 'dev':
+      await callback(devConfig);
+      break;
     // case 'testing':
     //   await callback(testingConfig);
     //   break;
@@ -19,6 +22,7 @@ export const performEffectWithEnvFile = async (callback: (config: any) => void) 
 };
 
 export const createZambdaClient = async (config: any): Promise<ZambdaClient> => {
+  console.log(config, 'config');
   const token = await getM2MClientToken(config);
 
   if (!token) {
