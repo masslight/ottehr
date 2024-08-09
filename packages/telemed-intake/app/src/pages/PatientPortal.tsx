73ac1ea09f36d09ec01320b3048578ea00c13c8a
aykhanahmadli <botl12020@gmail.com>
2024-08-09 17:59:01 +0200

    handle request visit from patient portal

--- packages/telemed-intake/app/src/pages/PatientPortal.tsx
+++ packages/telemed-intake/app/src/pages/PatientPortal.tsx
@@ -23,7 +23,9 @@ const PatientPortal = (): JSX.Element => {
   const appointmentID = activeAppointment?.id || '';
 
   const handleRequestVisit = (): void => {
-    navigate(`${IntakeFlowPageRoute.SelectPatient.path}?flow=requestVisit`);
+    navigate(
+      `${IntakeFlowPageRoute.Welcome.path.replace(':schedule-type', 'provider').replace(':slug', 'daniel').replace(':visit-service', 'in-person').replace(':visit-type', 'prebook')}`,
+    );
   };
 
   const handleReturnToCall = (): void => {
