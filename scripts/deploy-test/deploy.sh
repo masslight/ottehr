473dc8e8a0f1647aaec94aaaed56a51e54d94af4
saewitz <jonathansaewitz@gmail.com>
2024-09-19 12:34:22 -0400

    update deploy

--- scripts/deploy-test/deploy.sh
+++ scripts/deploy-test/deploy.sh
@@ -4,9 +4,21 @@ provider_email=$(jq -r '.provider_email' config.json)
 environment=$(jq -r '.environment' config.json)
 ENV=$environment
 
+
+if [ -f "../../packages/telemed-intake/app/env/.env.development" ]; then
+    first_setup=false
+else
+    first_setup=true
+fi
+
+echo $first_setup
+
 npm install
 cd ../../
-sh scripts/ottehr-setup.sh $project_id $access_token $provider_email
+
+if $first_setup; then
+    sh scripts/ottehr-setup.sh $project_id $access_token $provider_email $environment
+fi
 
 cd packages/telemed-intake/zambdas
 pnpm run deploy-zambdas $environment
@@ -23,8 +35,10 @@ cd ../app
 pnpm run build:$environment
 
 cd ../../../scripts/deploy-test
-cdk bootstrap
-cdk deploy
+if $first_setup; then
+    cdk bootstrap
+    cdk deploy
+fi
 
 cd ../../packages/telemed-intake/app
 pnpm run build:$environment
