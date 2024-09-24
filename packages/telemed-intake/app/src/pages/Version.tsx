a750e3d28ffef040cd8b20e5728084dff8182722
Gilad Schneider <gschneider@masslight.com>
2024-09-24 13:48:10 -0400

    more attempts

--- packages/telemed-intake/app/src/pages/Version.tsx
+++ packages/telemed-intake/app/src/pages/Version.tsx
@@ -1,9 +1,9 @@
 const Version = (): JSX.Element => {
-  const { ENV, VITE_APP_SHA, VITE_APP_VERSION } = import.meta.env;
+  const { MODE, VITE_APP_SHA, VITE_APP_VERSION } = import.meta.env;
 
   return (
     <div>
-      <p>Env: {ENV}</p>
+      <p>Env: {MODE}</p>
       <p>Hash: {VITE_APP_SHA}</p>
       <p>Version: {VITE_APP_VERSION}</p>
     </div>
