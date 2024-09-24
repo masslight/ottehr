0550be0a0ff070ee3a0f2a6d885132b89fd524e2
Gilad Schneider <gschneider@masslight.com>
2024-09-24 10:49:21 -0400

    VITE_APP_ENV -> ENV

--- packages/telemed-intake/app/src/pages/Version.tsx
+++ packages/telemed-intake/app/src/pages/Version.tsx
@@ -1,9 +1,9 @@
 const Version = (): JSX.Element => {
-  const { VITE_APP_ENV, VITE_APP_SHA, VITE_APP_VERSION } = import.meta.env;
+  const { ENV, VITE_APP_SHA, VITE_APP_VERSION } = import.meta.env;
 
   return (
     <div>
-      <p>Env: {VITE_APP_ENV}</p>
+      <p>Env: {ENV}</p>
       <p>Hash: {VITE_APP_SHA}</p>
       <p>Version: {VITE_APP_VERSION}</p>
     </div>
