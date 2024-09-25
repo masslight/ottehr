3a1402e2dbf034a2f4abe82aac35083f7f40e14c
Gilad Schneider <gschneider@masslight.com>
2024-09-25 15:37:53 -0400

    fixes build

--- packages/telemed-intake/app/src/features/auth/ProtectedRoute.tsx
+++ packages/telemed-intake/app/src/features/auth/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
-import { IntakeFlowPageRoute } from '../../App';
 
 export const ProtectedRoute: FC<{
   loadingFallback: JSX.Element;
