025cb07efe3f0a7c4e42f7d5e50459bfc23f1491
Uladzimir Davidchyk <udavidchyk@objectstyle.com>
2024-10-16 16:52:23 +0200

    clean Exam tab

--- packages/telemed-ehr/app/src/telemed/features/appointment/ExamTab/ExamTab.tsx
+++ packages/telemed-ehr/app/src/telemed/features/appointment/ExamTab/ExamTab.tsx
@@ -2,19 +2,6 @@ import React, { FC } from 'react';
 import { Box, CircularProgress } from '@mui/material';
 import { VitalsCard } from './VitalsCard';
 import { GeneralCard } from './GeneralCard';
-import { HeadCard } from './HeadCard';
-import { EyesCard } from './EyesCard';
-import { MouthCard } from './MouthCard';
-import { NeckCard } from './NeckCard';
-import { ChestCard } from './ChestCard';
-import { BackCard } from './BackCard';
-import { SkinCard } from './SkinCard';
-import { AbdomenCard } from './AbdomenCard';
-import { MusculoskeletalCard } from './MusculoskeletalCard';
-import { NeurologicalCard } from './NeurologicalCard';
-import { PsychCard } from './PsychCard';
-import { NoseCard } from './NoseCard';
-import { EarsCard } from './EarsCard';
 import { getSelectors } from '../../../../shared/store/getSelectors';
 import { useAppointmentStore } from '../../../state';
 import { ReadOnlyCard } from './ReadOnlyCard';
@@ -47,19 +34,6 @@ export const ExamTab: FC = () => {
         <>
           <VitalsCard />
           <GeneralCard />
-          <HeadCard />
-          <EyesCard />
-          <NoseCard />
-          <EarsCard />
-          <MouthCard />
-          <NeckCard />
-          <ChestCard />
-          <BackCard />
-          <SkinCard />
-          <AbdomenCard />
-          <MusculoskeletalCard />
-          <NeurologicalCard />
-          <PsychCard />
         </>
       )}
     </Box>
