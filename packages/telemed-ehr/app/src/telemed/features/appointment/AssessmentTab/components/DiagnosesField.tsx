ee646abb1790590605dbba1dc0dcba6c6a361152
Uladzimir Davidchyk <udavidchyk@objectstyle.com>
2024-10-18 12:55:21 +0200

    limit diagnosis codes

--- packages/telemed-ehr/app/src/telemed/features/appointment/AssessmentTab/components/DiagnosesField.tsx
+++ packages/telemed-ehr/app/src/telemed/features/appointment/AssessmentTab/components/DiagnosesField.tsx
@@ -1,8 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
-import { useDebounce } from '../../../../hooks';
-import { useGetIcd10Search } from '../../../../state';
 import { IcdSearchResponse } from 'ehr-utils';
+
+export type ICDSearchOption = {
+  code: string;
+  display: string;
+};
 
 type DiagnosesFieldProps = {
   onChange: (data: IcdSearchResponse['codes'][number]) => void;
@@ -13,20 +16,7 @@ type DiagnosesFieldProps = {
 export const DiagnosesField: FC<DiagnosesFieldProps> = (props) => {
   const { onChange, disabled, disableForPrimary } = props;
 
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
-  const [isLoading, setisLoading] = useState(false);
-  const { isFetching: isSearching, data } = useGetIcd10Search(debouncedSearchTerm);
-  const icdSearchOptions = data?.codes || [];
-
-  const { debounce } = useDebounce(800);
-
-  const debouncedHandleInputChange = (data: string): void => {
-    setisLoading(true);
-    debounce(() => {
-      setDebouncedSearchTerm(data);
-      setisLoading(false);
-    });
-  };
+  const icdSearchOptions = options;
 
   const onInternalChange = (_e: unknown, data: IcdSearchResponse['codes'][number] | null): void => {
     if (data) {
@@ -41,14 +31,12 @@ export const DiagnosesField: FC<DiagnosesFieldProps> = (props) => {
       options={icdSearchOptions}
       value={null}
       isOptionEqualToValue={(option, value) => value.code === option.code}
-      loading={isSearching || isLoading}
       onChange={onInternalChange}
       getOptionLabel={(option) => (typeof option === 'string' ? option : `${option.code} ${option.display}`)}
       getOptionDisabled={(option) => disableForPrimary && option.code.startsWith('W')}
       renderInput={(params) => (
         <TextField
           {...params}
-          onChange={(e) => debouncedHandleInputChange(e.target.value)}
           size="small"
           label="Search"
           placeholder="Diagnoses"
@@ -57,3 +45,18 @@ export const DiagnosesField: FC<DiagnosesFieldProps> = (props) => {
     />
   );
 };
+
+const options: ICDSearchOption[] = [
+  { display: 'Poisoning by opium, accidental (unintentional)', code: 'T40.0X1' },
+  { display: 'Poisoning by heroin, accidental (unintentional)', code: 'T40.1X' },
+  { display: 'Poisoning by other opioids, accidental (unintentional)', code: 'T40.2X1' },
+  { display: 'Poisoning by methadone, accidental (unintentional)', code: 'T40.3X1' },
+  { display: 'Poisoning by fentanyl, accidental (unintentional)', code: 'T40.411' },
+  { display: 'Poisoning by cocaine, accidental (unintentional)', code: 'T40.5X1' },
+  { display: 'Poisoning by narcotics, accidental (unintentional)', code: 'T40.601' },
+  { display: 'Poisoning by other narcotics, accidental (unintentional)', code: 'T40.691' },
+  { display: 'Opioid related disorders', code: 'F11' },
+  { display: 'Opioid abuse', code: 'F11.1' },
+  { display: 'Opioid dependence', code: 'F11.2' },
+  { display: 'Opioid use, unspecified', code: 'F11.9' },
+];
