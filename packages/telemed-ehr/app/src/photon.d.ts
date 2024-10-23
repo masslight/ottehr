49c1f046caab2b96a3db131c5aac43f47fb7ea43
Robert Zinger <rzinger@masslight.com>
2024-10-23 12:09:43 -0400

    make connection name optional

--- packages/telemed-ehr/app/src/photon.d.ts
+++ packages/telemed-ehr/app/src/photon.d.ts
@@ -6,7 +6,7 @@ declare namespace JSX {
       'dev-mode': string;
       'auto-login': string;
       'redirect-uri': string;
-      connection: string;
+      connection?: string;
       children: Element;
     };
     'photon-prescribe-workflow': {
