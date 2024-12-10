04f334883757f60ee0fd3dc651c1d52221063e35
Charles92011 <charles.johnson@civi.com>
2024-12-10 08:51:34 -0800

    WIP s3 deployment

---
+++ scripts/deploy-telemed-ehr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ~/conjure_ottehr/packages/telemed-ehr/app
+npm run deploy:dev
---
+++ scripts/deploy-zambdas.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~/conjure_ottehr/packages/telemed-ehr/zambdas
+npm run deploy-zambdas dev
+
+#cd ~/conjure_ottehr/packages/telemed-intake/zambdas
+#npm run deploy-zambdas dev
