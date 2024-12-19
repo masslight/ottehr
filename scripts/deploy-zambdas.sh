3941c9ef2842401e972d0b279ac838ca7e690993
Charles92011 <charles.johnson@civi.com>
2024-12-19 10:48:38 -0800

    updates to deploy patient intake

--- scripts/deploy-zambdas.sh
+++ scripts/deploy-zambdas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/workspace/ConjureEHR/packages/telemed-ehr/zambdas
-npm run deploy-zambdas dev
+npm run deploy-zambdas development
 
 #cd ~/workspace/ConjureEHR/packages/telemed-intake/zambdas
 #npm run deploy-zambdas dev
