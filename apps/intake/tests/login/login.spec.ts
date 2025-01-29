24cead78d34c1b7e4b711241f644e3c13c0edbd1
Local Auth <local_actions@localhost.com>
2025-01-29 14:44:41 +0400

    fix local e2e login cache

--- apps/intake/tests/login/login.spec.ts
+++ apps/intake/tests/login/login.spec.ts
@@ -1,9 +1,9 @@
 import fs from 'fs/promises';
-import { execSync } from 'child_process';
-import path from 'path';
+import { execSync, exec } from 'child_process';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { test, expect } from '@playwright/test';
 import { login } from 'test-utils';
-import { cloneRepoLocally, removeFolder, getRepoNameFromUrl } from '../../../../scripts/setup/setup.utils.mjs';
 
 test('Should log in if not authorized', async ({ context, page }) => {
   try {
@@ -38,6 +38,7 @@ test('Should log in if not authorized', async ({ context, page }) => {
 
           successLogin = true;
         } catch (error) {
+          console.error(error);
           attemptNumber++;
           console.log(`Attempt ${attemptNumber} failed, retrying...`);
           if (attemptNumber === maxAttempts) {
@@ -60,34 +61,28 @@ async function pushUserJsonToSecretsRepo(): Promise<void> {
     if (!repoName) throw new Error('secrets repo name not found');
 
     const currentDir = process.cwd();
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
     const repoUserJsonPath = path.join(currentDir, repoName, 'intake', 'app');
     const localUserJsonPath = path.join(__dirname, '../../playwright/user.json');
-
-    try {
-      await fs.access(localUserJsonPath, fs.constants.F_OK);
-    } catch {
-      throw new Error('Source user.json not found');
-    }
-
-    try {
-      await fs.access(repoUserJsonPath, fs.constants.F_OK);
-    } catch {
-      await fs.mkdir(repoUserJsonPath, { recursive: true });
-    }
 
     await cloneRepoLocally(repoUrl);
     await fs.copyFile(localUserJsonPath, path.join(repoUserJsonPath, 'user.json'));
     console.log(`Copied user.json to ${repoUserJsonPath}`);
     const repoPath = path.resolve(currentDir, repoName);
 
-    execSync('git config --local user.name "Local Auth"', { cwd: repoPath });
-    execSync('git config --local user.email "local_actions@localhost.com"', { cwd: repoPath });
+    console.log('repoPath', repoPath);
 
-    execSync('git add .', { cwd: repoPath });
     try {
-      execSync('git diff --quiet && git diff --staged --quiet', { cwd: repoPath });
-    } catch {
-      execSync('git commit -m "Update user.json from localhost" && git push', { cwd: repoPath });
+      execSync('git add .', { cwd: repoPath });
+      try {
+        execSync('git diff --quiet && git diff --staged --quiet', { cwd: repoPath });
+      } catch {
+        execSync('git commit -m "Update user.json from localhost" && git push', { cwd: repoPath });
+      }
+    } catch (error) {
+      console.error(error);
     }
     console.log('user.json pushed to secrets repo');
 
@@ -97,4 +92,52 @@ async function pushUserJsonToSecretsRepo(): Promise<void> {
     console.error('push user.json error:', error);
     throw error;
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function removeFolder(path: string) {
+  if (!path) {
+    return;
+  }
+  // Remove the cloned repository
+  console.log(`Removing folder ${path}...`);
+  return new Promise((resolve, reject) => {
+    exec(`rm -rf ${path}`, (error) => {
+      if (error) {
+        console.error(`Error removing folder ${path}: ${error.message}`);
+        reject(error);
+      }
+
+      console.log(`Removed folder ${path} successfully`);
+      resolve(null);
+    });
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function cloneRepoLocally(repoUrl: string) {
+  return new Promise((resolve, reject) => {
+    exec(`git clone ${repoUrl}`, (error) => {
+      if (error) {
+        console.error(`Error cloning repository: ${error.message}`);
+        reject(error);
+        return;
+      }
+      resolve(null);
+    });
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function getRepoNameFromUrl(repoUrl: string) {
+  const httpsPattern = /(?<=github\.com\/)[^/]+\/([^/]+)(?=\.git)/;
+  const sshPattern = /(?<=github\.com:|github\.com\/)[^/]+\/([^/]+)(?=\.git)/;
+  const match = repoUrl.match(httpsPattern) || repoUrl.match(sshPattern);
+
+  if (!match) {
+    throw new Error('Unsupported repository URL format');
+  }
+
+  const repoName = match[1];
+  return repoName;
 }
