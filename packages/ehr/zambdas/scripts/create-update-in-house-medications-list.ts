c7005dcedefcc6c7645d62771a33d39b571f71b7
Uladzislau Molastau <molostov.inc@gmail.com>
2025-01-31 14:43:11 +0100

    bring back performEffectWithEnvFile

--- packages/ehr/zambdas/scripts/create-update-in-house-medications-list.ts
+++ packages/ehr/zambdas/scripts/create-update-in-house-medications-list.ts
@@ -8,11 +8,10 @@ import {
   MEDICATION_IDENTIFIER_ADMIN_CODE_SYSTEM,
   MEDICATION_IDENTIFIER_NAME_SYSTEM,
   MEDICATION_TYPE_SYSTEM,
-  performEffectWithEnvFile,
 } from 'utils';
-import { getResourcesFromBatchInlineRequests } from 'utils';
-import { getMedicationTypeCode } from 'utils';
-import { CODE_SYSTEM_CPT, CODE_SYSTEM_NDC } from 'utils';
+
+import { CODE_SYSTEM_CPT, CODE_SYSTEM_NDC, getMedicationTypeCode, getResourcesFromBatchInlineRequests } from 'utils';
+import { performEffectWithEnvFile } from 'zambda-utils';
 import { getAuth0Token } from '../src/shared';
 import { fhirApiUrlFromAuth0Audience } from './helpers';
 
--- packages/zambda-utils/lib/secrets/index.ts
+++ packages/zambda-utils/lib/secrets/index.ts
@@ -1,3 +1,5 @@
+export * from './helpers';
+
 export interface Secrets {
   [secretName: string]: string;
 }
