0d70a636a1b42e560a428693be1a125153be19bc
aykhanahmadli <botl12020@gmail.com>
2025-01-31 12:38:12 +0100

    add icon paths

--- apps/ehr/src/theme/ottehr/index.ts
+++ apps/ehr/src/theme/ottehr/index.ts
@@ -3,4 +3,5 @@ declare module '*.svg';
 export {};
 
 export * from './colors';
+export * from './icons';
 export { default as notificationSound } from './notification.ogg';
--- apps/intake/src/theme/ottehr/index.ts
+++ apps/intake/src/theme/ottehr/index.ts
@@ -3,4 +3,5 @@ export { default as ottehrLogo } from './ottehrIntake.png';
 export {};
 
 export * from './colors';
+export * from './icons';
 export const typography = {};
