279a21386de189df000fcd83c1d42a260157132e
Olha Kovalenko <okovalenko@masslight.com>
2025-02-04 13:48:08 +0000

    fix

--- apps/ehr/test/e2e/specs/patientInformationPage.spec.ts
+++ apps/ehr/test/e2e/specs/patientInformationPage.spec.ts
@@ -1,20 +1,18 @@
 import { test } from '@playwright/test';
 import {
+  PATIENT_BIRTHDAY,
   PATIENT_FIRST_NAME,
   PATIENT_GENDER,
   PATIENT_LAST_NAME,
   ResourceHandler,
 } from '../../e2e-utils/resource-handler';
 import { expectPatientInformationPage } from '../page/PatientInformationPage';
-import { DateTime } from 'luxon';
+import { formatDOB } from 'utils';
 
-const HEADER_PATIENT_BIRTHDAY =
-  '01/01/2024 (' +
-  Math.floor(Math.abs(DateTime.fromFormat('01/01/2024', 'MM/dd/yyyy').diffNow('months').months)) +
-  ' mo)';
+const HEADER_PATIENT_BIRTHDAY = formatDOB(PATIENT_BIRTHDAY)!;
 const HEADER_PATIENT_GENDER = 'Male';
 const HEADER_PATIENT_NAME = PATIENT_LAST_NAME + ', ' + PATIENT_FIRST_NAME;
-const PATIENT_BIRTHDAY = '01/01/2024';
+const PATIENT_DATE_OF_BIRTH = '01/01/2024';
 
 const resourceHandler = new ResourceHandler();
 
@@ -44,6 +42,6 @@ test('Check patient info', async ({ page }) => {
   const patientInformationPage = await expectPatientInformationPage(page, resourceHandler.patient.id!);
   await patientInformationPage.verifyPatientLastName(PATIENT_LAST_NAME);
   await patientInformationPage.verifyPatientFirstName(PATIENT_FIRST_NAME);
-  await patientInformationPage.verifyPatientDateOfBirth(PATIENT_BIRTHDAY);
+  await patientInformationPage.verifyPatientDateOfBirth(PATIENT_DATE_OF_BIRTH);
   await patientInformationPage.verifyPatientBirthSex(PATIENT_GENDER);
 });
