0524808d67579ff29e4506665c15e7df5157cf4a
Roman Golchuk <rgolchuk@masslight.com>
2025-02-07 14:30:33 +0100

    feat: reset isDirty after save

--- apps/ehr/src/components/patient/ActionBar.tsx
+++ apps/ehr/src/components/patient/ActionBar.tsx
@@ -11,10 +11,12 @@ type ActionBarProps = {
 export const ActionBar: FC<ActionBarProps> = ({ handleDiscard }) => {
   const theme = useTheme();
 
-  const { patient, patchOperations, reset, tempInsurances } = usePatientStore();
+  const { patient, patchOperations, reset: resetPatchOperations, tempInsurances } = usePatientStore();
   const {
     formState: { isDirty },
+    getValues,
     trigger,
+    reset: resetForm,
   } = useFormContext();
   const { mutateAsync: mutatePatientMasterRecord } = useUpdatePatient();
 
@@ -36,7 +38,15 @@ export const ActionBar: FC<ActionBarProps> = ({ handleDiscard }) => {
     if (!isValid) return;
 
     await mutatePatientMasterRecord();
-    reset();
+    resetPatchOperations();
+    resetForm(
+      {
+        ...getValues(),
+      },
+      {
+        keepDirty: false,
+      }
+    );
   };
 
   return (
