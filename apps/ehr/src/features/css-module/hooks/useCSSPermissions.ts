f8671a6deb84006bfa9bd40f17e09c85fd20db81
imbenham <isaac.benham@gmail.com>
2025-02-12 19:10:03 -0500

    remove manager check

--- apps/ehr/src/features/css-module/hooks/useCSSPermissions.ts
+++ apps/ehr/src/features/css-module/hooks/useCSSPermissions.ts
@@ -1,16 +1,13 @@
 import { useMemo } from 'react';
-import { RoleType } from 'utils';
 import useEvolveUser from '../../../hooks/useEvolveUser';
 
 export const useCSSPermissions = (): { view: boolean; isPending: boolean } => {
-  //GetAppointmentAccessibilityDataResult
-
   const currentUser = useEvolveUser();
 
   const config = useMemo(
     () => ({
       isPending: !currentUser, // TODO suggested undefined will be for pending user, check if this is correct or unauthorized user is also undefined
-      view: Boolean(currentUser?.hasRole([RoleType.Manager])), // TODO use related role
+      view: true, // restrict who has access to clinical support service features: Boolean(currentUser?.hasRole([RoleType.YourRole]))
     }),
     [currentUser]
   );
