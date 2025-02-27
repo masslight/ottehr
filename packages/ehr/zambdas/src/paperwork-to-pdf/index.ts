095a19322d6705e7a36e71a60b461eb097b0461a
bholyshevskyi <bholyshevskyi@masslight.com>
2025-02-27 15:58:23 +0000

    doc ref

--- packages/ehr/zambdas/src/paperwork-to-pdf/index.ts
+++ packages/ehr/zambdas/src/paperwork-to-pdf/index.ts
@@ -6,9 +6,11 @@ import Oystehr from '@oystehr/sdk';
 import { createOystehrClient, validateJsonBody, validateString } from '../shared/helpers';
 import { createDocument } from './document';
 import { drawDocument } from './draw';
+import { DocumentReference } from 'fhir/r4b';
 
 interface Input {
   questionnaireResponseId: string;
+  documentReference: DocumentReference;
   secrets: Secrets | null;
 }
 
@@ -20,15 +22,30 @@ export const index = wrapHandler(async (input: ZambdaInput): Promise<APIGatewayP
   configSentry(ZAMBDA_NAME, input.secrets);
   console.log(`Input: ${JSON.stringify(input)}`);
   try {
-    const { questionnaireResponseId, secrets } = validateInput(input);
+    const { questionnaireResponseId, documentReference, secrets } = validateInput(input);
     const oystehr = await createOystehr(secrets);
     const document = await createDocument(questionnaireResponseId, oystehr);
     const pdfDocument = await drawDocument(document);
+    await oystehr.z3.uploadFile({
+      bucketName: 'todo',
+      'objectPath+': 'todo',
+      file: new Blob([new Uint8Array(await pdfDocument.save())]),
+    });
+    documentReference.content = [
+      {
+        attachment: {
+          url: 'todo',
+          contentType: 'application/pdf',
+          title: 'todo title',
+        },
+      },
+    ];
+    const { id } = await oystehr.fhir.create<DocumentReference>(documentReference);
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/pdf' },
-      body: Buffer.from(await pdfDocument.save()).toString('base64'),
-      isBase64Encoded: true,
+      body: JSON.stringify({
+        id: id,
+      }),
     };
   } catch (error: any) {
     return topLevelCatch(ZAMBDA_NAME, error, input.secrets, captureSentryException);
@@ -36,9 +53,13 @@ export const index = wrapHandler(async (input: ZambdaInput): Promise<APIGatewayP
 });
 
 function validateInput(input: ZambdaInput): Input {
-  const { questionnaireResponseId } = validateJsonBody(input);
+  const { questionnaireResponseId, documentReference } = validateJsonBody(input);
+  if (documentReference.resourceType !== 'DocumentReference') {
+    throw new Error('documentReference must be a "DocumentReference" resource');
+  }
   return {
     questionnaireResponseId: validateString(questionnaireResponseId, 'questionnaireResponseId'),
+    documentReference: documentReference,
     secrets: input.secrets,
   };
 }
