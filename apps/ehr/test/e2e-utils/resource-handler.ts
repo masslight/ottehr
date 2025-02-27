bc210c62f5e16314228302bb854993b0add6f211
Olha Kovalenko <okovalenko@masslight.com>
2025-02-27 15:39:59 +0000

    wip

--- apps/ehr/test/e2e-utils/resource-handler.ts
+++ apps/ehr/test/e2e-utils/resource-handler.ts
@@ -292,7 +292,7 @@ export class ResourceHandler {
     try {
       await this.initApi();
 
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 10; i++) {
         const appointment = (
           await this.apiClient.fhir.search({
             resourceType: 'Appointment',
