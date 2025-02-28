ec10f47f2cc1e035041a32135c33f4f654a1c7c0
Roman Golchuk <rgolchuk@masslight.com>
2025-02-28 11:31:40 +0100

    feat: add notification when patient information form invalid or issue occured in saving response

--- apps/ehr/src/hooks/useGetPatient.ts
+++ apps/ehr/src/hooks/useGetPatient.ts
@@ -30,6 +30,7 @@ import { getPatientNameSearchParams } from '../helpers/patientSearch';
 import { usePatientStore } from '../state/patient.store';
 import { getVisitTypeLabelForAppointment } from '../types/types';
 import { useApiClients } from './useAppClients';
+import { enqueueSnackbar } from 'notistack';
 
 const getTelemedLength = (history?: EncounterStatusHistory[]): number => {
   const value = history?.find((item) => item.status === 'in-progress');
@@ -406,6 +407,9 @@ export const useUpdatePatient = () => {
       }
     } catch (error) {
       console.error(error);
+      enqueueSnackbar('Save operation failed. The server encountered an error while processing your request.', {
+        variant: 'error',
+      });
       throw error;
     }
   });
