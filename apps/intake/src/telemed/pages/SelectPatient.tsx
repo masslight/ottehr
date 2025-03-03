3ab9ccb6f4973d48b2989848cbb7fdaf4ef0c4ba
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-03-03 13:38:58 -0500

    conditionally render reason for visit

--- apps/intake/src/telemed/pages/SelectPatient.tsx
+++ apps/intake/src/telemed/pages/SelectPatient.tsx
@@ -53,13 +53,18 @@ const SelectPatient = (): JSX.Element => {
     const selectedPatient = patientsStore.patients?.find((patient) => patient.id === data.patientId);
 
     if (selectedPatient) {
+      let reasonForVisit = undefined;
+      if (selectedPatient.id === currentPatientInfo.id) {
+        reasonForVisit = currentPatientInfo.reasonForVisit;
+      }
+
       usePatientInfoStore.setState(() => ({
         patientInfo: {
           ...selectedPatient,
           id: selectedPatient.id || undefined,
           newPatient: false,
           dateOfBirth: DateTime.fromISO(selectedPatient.dateOfBirth || '').toString(),
-          reasonForVisit: selectedPatient?.reasonForVisit,
+          reasonForVisit,
         },
         pendingPatientInfoUpdates: undefined,
       }));
