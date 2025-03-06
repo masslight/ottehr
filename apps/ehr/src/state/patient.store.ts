f437ceca92444745f13303b59bca0cde6beafe02
Roman Golchuk <rgolchuk@masslight.com>
2025-03-06 12:24:25 +0100

    fix: add special case for adding contact telecom item

--- apps/ehr/src/state/patient.store.ts
+++ apps/ehr/src/state/patient.store.ts
@@ -271,13 +271,23 @@ export const usePatientStore = create<PatientState & PatientStoreActions>()((set
         if (effectiveValue !== undefined) {
           newPatchOperation = { op: 'replace', path, value };
         } else {
-          const telecomConfig = contactTelecomConfigs[fieldType!];
-          const telecomItem = { system: telecomConfig.system, value };
-          newPatchOperation = {
-            op: 'add',
-            path: path.replace(/(\/telecom)\/(-1|\d+)\/value/, '$1/-'),
-            value: telecomItem,
-          };
+          if (path.startsWith('/contact/')) {
+            const telecomConfig = contactTelecomConfigs[fieldType!];
+            const telecomItem = { system: telecomConfig.system, value };
+            newPatchOperation = {
+              op: 'add',
+              path: path.replace(/(\/telecom)\/(-1|\d+)\/value/, '$1'),
+              value: [telecomItem],
+            };
+          } else {
+            const telecomConfig = contactTelecomConfigs[fieldType!];
+            const telecomItem = { system: telecomConfig.system, value };
+            newPatchOperation = {
+              op: 'add',
+              path: path.replace(/(\/telecom)\/(-1|\d+)\/value/, '$1/-'),
+              value: telecomItem,
+            };
+          }
         }
       }
     } else if (isResponsiblePartyBirthDate) {
