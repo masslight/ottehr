b9fe4a2d2c78c959a6f736b8cffccefb560eb583
Alex Willingham <awillingham@masslight.com>
2025-03-14 16:27:55 -0400

    delete old zambda packages, fix top level package.json scripts

--- packages/intake/zambdas/src/get-patients/validateRequestParameters.ts
+++
@@ -1,9 +0,0 @@
-import {} from 'utils';
-import { ZambdaInput } from 'zambda-utils';
-import { GetPatientsInput } from '.';
-
-export function validateRequestParameters(input: ZambdaInput): GetPatientsInput {
-  return {
-    secrets: input.secrets,
-  };
-}
--- packages/intake/zambdas/src/shared/validation.ts
+++
@@ -1,4 +0,0 @@
-// Phone number regex
-// ^(\+1)? match an optional +1 at the beginning of the string
-// \d{10}$ match exactly 10 digits at the end of the string
-export const phoneRegex = /^(\+1)?\d{10}$/;
