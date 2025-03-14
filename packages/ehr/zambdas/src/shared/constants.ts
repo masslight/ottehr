3dbb5ffdd3219417e98f7856343a783189923400
Alex Willingham <awillingham@masslight.com>
2025-03-14 16:27:55 -0400

    delete old zambda packages, fix top level package.json scripts

--- packages/ehr/zambdas/src/shared/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ENCOUNTER_VS_EXTENSION_URL = 'https://extensions.fhir.zapehr.com/encounter-virtual-service-pre-release';
-export const ENCOUNTER_VS_EXTENSION_CONVO_RELATIVE_URL = 'addressString';
-export const HOP_QUEUE_URI = 'hop-queue';
