ff2245425eec1ef32cad08475aac11e01a577d33
Roman Golchuk <rgolchuk@masslight.com>
2025-03-19 19:49:52 +0100

    Revert "fix: prevent putting empty answers to response"
    This reverts commit 96b51e61e4c634b7f1654a79b3a1cac4625dc683.

--- apps/intake/src/pages/PaperworkPage.tsx
+++ apps/intake/src/pages/PaperworkPage.tsx
@@ -483,7 +483,7 @@ export const PaperworkPage: FC = () => {
               });
               if (existingIdx >= 0) {
                 responseItems[existingIdx] = { linkId: key, answer: val };
-              } else if (val) {
+              } else {
                 responseItems.push({ linkId: key, answer: val });
               }
             });
