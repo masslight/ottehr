5092e35124e8ccd62bb90eafc581e0b49f47f483
Alex Willingham <awillingham@masslight.com>
2025-03-20 22:11:39 -0400

    fix authorization header, path mistakes in zambda specs, nodemon cleanup

--- packages/zambdas/src/local-server/index.ts
+++ packages/zambdas/src/local-server/index.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import ottehrSpec from '../../ottehr-spec.json';
 import { expressLambda } from './utils';
 
@@ -7,14 +7,22 @@ const app = express();
 
 app.use(express.text({ type: '*/*', limit: '6mb' }));
 
+// Upgrade lower-cased authorization into capitalized one the way API Gateway does
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  req.headers.Authorization = req.headers.authorization;
+  next();
+});
+
 app.use(cors());
 
 Object.entries(ottehrSpec.zambdas).forEach(([_key, spec]) => {
   const executeOrExecutePublic = spec.type === 'http_auth' ? 'execute' : 'execute-public';
-  app.post(`/local/zambda/${spec.name}/${executeOrExecutePublic}`, async (req, res) => {
+  const path = `/local/zambda/${spec.name}/${executeOrExecutePublic}`;
+  app.post(path, async (req, res) => {
     const { index } = await import(`../../${spec.src}`);
     await expressLambda(index, req, res);
   });
+  console.log(`Registered POST: ${path}`);
 });
 
 app.listen(3000, () => {
