cbf6045ce00f976ba45c91cacdfcec3db5fce93e
Alex Willingham <awillingham@masslight.com>
2025-03-24 14:15:30 -0400

    fix path to asset file. rename setup secrets script

--- packages/zambdas/src/ehr/shared/harvest/index.ts
+++ packages/zambdas/src/ehr/shared/harvest/index.ts
@@ -660,7 +660,9 @@ export async function createConsentResources(input: CreateConsentResourcesInput)
     patientResource.id
   }/${Date.now()}`;
   const consentDocument =
-    locationState === 'IL' ? './CTT.and.Guarantee.of.Payment.Illinois-S.pdf' : './CTT.and.Guarantee.of.Payment-S.pdf';
+    locationState === 'IL'
+      ? './assets/CTT.and.Guarantee.of.Payment.Illinois-S.pdf'
+      : './assets/CTT.and.Guarantee.of.Payment-S.pdf';
   const pdfsToCreate = [
     {
       uploadURL: `${baseUploadURL}-consent-to-treat.pdf`,
@@ -680,7 +682,7 @@ export async function createConsentResources(input: CreateConsentResourcesInput)
     },
     {
       uploadURL: `${baseUploadURL}-hippa-acknowledgement.pdf`,
-      copyFromPath: './HIPAA.Acknowledgement-S.pdf',
+      copyFromPath: './assets/HIPAA.Acknowledgement-S.pdf',
       formTitle: 'HIPAA Acknowledgement',
       resourceTitle: 'HIPPA forms',
       type: {
