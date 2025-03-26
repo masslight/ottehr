a61fb04f3e869475a3e1e5b3d2fbb2262757c016
Roman Golchuk <rgolchuk@masslight.com>
2025-03-26 15:56:51 +0100

    feat: add batch processing

--- packages/zambdas/src/scripts/setup-document-explorer.ts
+++ packages/zambdas/src/scripts/setup-document-explorer.ts
@@ -10,6 +10,8 @@ import { performEffectWithEnvFile } from 'zambda-utils';
 import { getAuth0Token } from '../patient/shared';
 import { fhirApiUrlFromAuth0Audience } from './helpers';
 
+const BATCH_SIZE = 25;
+
 const checkAndUpdateListResources = async (config: any): Promise<void> => {
   const token = await getAuth0Token(config);
   if (!token) throw new Error('Failed to fetch auth token.');
@@ -18,90 +20,68 @@ const checkAndUpdateListResources = async (config: any): Promise<void> => {
     accessToken: token,
   });
 
-  console.log('Fetching all Patients with Lists and DocumentReferences from fhir.');
-  const {patients, lists, documentReferences} = await getDocumentExplorerResources(oystehr);
-  
-  const createListsRequests: BatchInputPostRequest<List>[] = [];
-  const patchListsRequests: BatchInputRequest<List>[] = [];
+  console.log('Fetching Patients with Lists and DocumentReferences in batches.');
 
-  for (const patient of patients) {
-    console.log(`Processing patient: ${patient.id}`);
-    
-    const patientReference = `Patient/${patient.id}`;
+  let offset = 0;
+  let totalProcessed = 0;
+  let batchLength = -1;
+  let totalCreateRequests = 0;
+  let totalPatchRequests = 0;
 
-    const patientLists = lists.filter((list) => list.subject?.reference === patientReference);
+  while (batchLength !== 0) {
+    console.log(`Processing batch starting at offset: ${offset}`);
 
-    const patientDocumentReferences = documentReferences.filter((docRef) => docRef.subject?.reference === patientReference);
+    const { patients, lists, documentReferences } = await getDocumentExplorerResourcesBatch(
+      oystehr,
+      offset,
+      BATCH_SIZE
+    );
 
-    for (const folder of FOLDERS_CONFIG) {
-      let list = patientLists.find(l => l.code?.coding?.some(c => c.code === folder.documentTypeCode));
-      const relevantDocs = patientDocumentReferences.filter((doc) => 
-        Array.isArray(folder.documentTypeCode) 
-          ? folder.documentTypeCode.includes(doc.type?.coding?.[0]?.code!)
-          : doc.type?.coding?.[0]?.code === folder.documentTypeCode
+    batchLength = patients.length;
+    totalProcessed += batchLength;
+
+    if (batchLength > 0) {
+      console.time(`Patient batch processing (offset: ${offset})`);
+      const { createListsRequests, patchListsRequests } = await process(
+        patients,
+        lists,
+        documentReferences
       );
-      
-      if (!list) {
-        console.log(`Creating missing List for folder: ${folder.title}`);
-        list = createPatientDocumentList(patientReference, folder);
-        
-        const entries = relevantDocs
-          .map(doc => ({
-            date: doc.date,
-            item: { type: 'DocumentReference', reference: `DocumentReference/${doc.id}` },
-          }));
-        
-        list.entry = entries;
+      console.timeEnd(`Patient batch processing (offset: ${offset})`);
 
-        createListsRequests.push({
-          method: 'POST',
-          url: '/List',
-          resource: list,
-        })
-      }
-    }
+      totalCreateRequests += createListsRequests.length;
+      totalPatchRequests += patchListsRequests.length;
 
-    for (const list of patientLists) {
-      const folder = FOLDERS_CONFIG.find((config) => config.title === list.title)
-      
-      if (folder) {
-        const existingEntries = new Set(list.entry?.map(e => e.item?.reference) ?? []);
-
-        const relevantDocs = patientDocumentReferences.filter((doc) => 
-          Array.isArray(folder.documentTypeCode) 
-            ? folder.documentTypeCode.includes(doc.type?.coding?.[0]?.code!)
-            : doc.type?.coding?.[0]?.code === folder.documentTypeCode
-        );
-
-        const newEntries = relevantDocs
-          .filter(doc => !existingEntries.has(`DocumentReference/${doc.id}`))
-          .map(doc => ({
-            date: doc.date,
-            item: { type: 'DocumentReference', reference: `DocumentReference/${doc.id}` },
-          }));
-    
-        if (newEntries.length > 0) {
-          console.log(`Updating List for ${folder.title} with ${newEntries.length} new documents.`);
-          patchListsRequests.push(getPatchBinary({
-            resourceType: 'List',
-            resourceId: list.id!,
-            patchOperations: [{ op: list.entry ? 'replace' : 'add', path: '/entry', value: [...(list.entry ?? []), ...newEntries] }],
-          }));
+      if (createListsRequests.length > 0 || patchListsRequests.length > 0) {
+        console.time(`Batch FHIR operations (offset: ${offset})`);
+        try {
+          await oystehr.fhir.batch({
+            requests: [
+              ...createListsRequests,
+              ...patchListsRequests,
+            ],
+          });
+        } catch (error) {
+          console.error(`Error during batch operations at offset ${offset}:`, JSON.stringify(error));
+          throw new Error(`Error during batch operations at offset ${offset}`);
         }
+        console.timeEnd(`Batch FHIR operations (offset: ${offset})`);
       }
     }
+
+    offset += BATCH_SIZE;
   }
 
-  await oystehr.fhir.batch({
-    requests: [
-      ...createListsRequests,
-      ...patchListsRequests,
-    ],
-  });
+  console.log(`[COMPLETE] Document explorer resource update summary:
+    - Total patients processed: ${totalProcessed}
+    - List resources created: ${totalCreateRequests}
+    - List resources patched: ${totalPatchRequests}`);
 };
 
-export async function getDocumentExplorerResources(
-  oystehr: Oystehr
+export async function getDocumentExplorerResourcesBatch(
+  oystehr: Oystehr,
+  offset: number,
+  count: number,
 ): Promise<{
   patients: Patient[];
   lists: List[];
@@ -111,6 +91,14 @@ export async function getDocumentExplorerResources(
     await oystehr.fhir.search<Patient | List | DocumentReference>({
       resourceType: 'Patient',
       params: [
+        {
+          name: '_count',
+          value: `${count}`,
+        },
+        {
+          name: '_offset',
+          value: `${offset}`,
+        },
         {
           name: '_revinclude',
           value: 'List:subject:Patient',
@@ -137,6 +125,87 @@ export async function getDocumentExplorerResources(
 
   return { patients, lists, documentReferences };
 }
+
+const process = async (
+  patients: Patient[],
+  lists: List[],
+  documentReferences: DocumentReference[]
+): Promise<{
+  createListsRequests: BatchInputPostRequest<List>[],
+  patchListsRequests: BatchInputRequest<List>[]
+}> => {
+  const createListsRequests: BatchInputPostRequest<List>[] = [];
+  const patchListsRequests: BatchInputRequest<List>[] = [];
+
+  for (const patient of patients) {
+    console.log(`Processing patient: ${patient.id}`);
+
+    const patientReference = `Patient/${patient.id}`;
+
+    const patientLists = lists.filter((list) => list.subject?.reference === patientReference);
+    const patientDocumentReferences = documentReferences.filter((docRef) => docRef.subject?.reference === patientReference);
+
+    for (const folder of FOLDERS_CONFIG) {
+      let list = patientLists.find(l => l.code?.coding?.some(c => c.code === folder.documentTypeCode));
+      const relevantDocs = patientDocumentReferences.filter((doc) =>
+        Array.isArray(folder.documentTypeCode)
+          ? folder.documentTypeCode.includes(doc.type?.coding?.[0]?.code!)
+          : doc.type?.coding?.[0]?.code === folder.documentTypeCode
+      );
+
+      if (!list) {
+        console.log(`Creating missing List for folder: ${folder.title}`);
+        list = createPatientDocumentList(patientReference, folder);
+
+        const entries = relevantDocs
+          .map(doc => ({
+            date: doc.date,
+            item: { type: 'DocumentReference', reference: `DocumentReference/${doc.id}` },
+          }));
+
+        list.entry = entries;
+
+        createListsRequests.push({
+          method: 'POST',
+          url: '/List',
+          resource: list,
+        });
+      }
+    }
+
+    for (const list of patientLists) {
+      const folder = FOLDERS_CONFIG.find((config) => config.title === list.title);
+
+      if (folder) {
+        const existingEntries = new Set(list.entry?.map(e => e.item?.reference) ?? []);
+
+        const relevantDocs = patientDocumentReferences.filter((doc) =>
+          Array.isArray(folder.documentTypeCode)
+            ? folder.documentTypeCode.includes(doc.type?.coding?.[0]?.code!)
+            : doc.type?.coding?.[0]?.code === folder.documentTypeCode
+        );
+
+        const newEntries = relevantDocs
+          .filter(doc => !existingEntries.has(`DocumentReference/${doc.id}`))
+          .map(doc => ({
+            date: doc.date,
+            item: { type: 'DocumentReference', reference: `DocumentReference/${doc.id}` },
+          }));
+
+        if (newEntries.length > 0) {
+          console.log(`Updating List for ${folder.title} with ${newEntries.length} new documents. List.id: ${list.id}`);
+          patchListsRequests.push(getPatchBinary({
+            resourceType: 'List',
+            resourceId: list.id!,
+            patchOperations: [{ op: list.entry ? 'replace' : 'add', path: '/entry', value: [...(list.entry ?? []), ...newEntries] }],
+          }));
+        }
+      }
+    }
+  }
+
+  return { createListsRequests, patchListsRequests };
+};
 
 const main = async (): Promise<void> => {
   try {
