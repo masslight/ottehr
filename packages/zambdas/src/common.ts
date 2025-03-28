5db7f4d89ee3b4059caa5da0bdda88c3ca93fbe6
bholyshevskyi <bholyshevskyi@masslight.com>
2025-03-28 10:54:28 +0000

    use includes

--- packages/zambdas/src/common.ts
+++ packages/zambdas/src/common.ts
@@ -4,7 +4,7 @@ import { inPersonIntakeQuestionnaire, virtualIntakeQuestionnaire } from 'utils';
 export function isNonPaperworkQuestionnaireResponse<T extends FhirResource>(resource: T): boolean {
   return (
     resource.resourceType === 'QuestionnaireResponse' &&
-    !resource.questionnaire?.startsWith(inPersonIntakeQuestionnaire.resource.url) &&
-    !resource.questionnaire?.startsWith(virtualIntakeQuestionnaire.resource.url)
+    !resource.questionnaire?.includes(inPersonIntakeQuestionnaire.resource.url) &&
+    !resource.questionnaire?.includes(virtualIntakeQuestionnaire.resource.url)
   );
 }
