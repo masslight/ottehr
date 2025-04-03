01bb850a1ee2b02885a48827e49c897878ce110e
imbenham <isaac.benham@gmail.com>
2025-04-03 00:06:29 -0400

    fix cleanup script bug

--- packages/zambdas/src/scripts/remove-test-patient-data.ts
+++ packages/zambdas/src/scripts/remove-test-patient-data.ts
@@ -7,7 +7,7 @@ import { createOystehrClientFromConfig, performEffectWithEnvFile } from './helpe
 const exec = promisify(execCb);
 
 const deleteTestPatientsData = async (config: any): Promise<void> => {
-  const env = process.argv[3];
+  const env = config.env;
 
   const oystehr = await createOystehrClientFromConfig(config);
 
