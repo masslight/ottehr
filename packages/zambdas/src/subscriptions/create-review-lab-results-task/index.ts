4cc1594817ee5acc40c740dc8eee7721d7566456
Athena Braun <abraun@masslight.com>
2025-04-04 15:22:06 -0400

    working zambda

--- packages/zambdas/src/subscriptions/create-review-lab-results-task/index.ts
+++ packages/zambdas/src/subscriptions/create-review-lab-results-task/index.ts
@@ -1,3 +1,4 @@
+import { BatchInputRequest } from '@oystehr/sdk';
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { DiagnosticReport, Task } from 'fhir/r4b';
 import { Secrets } from 'utils';
@@ -5,7 +6,7 @@ import { ZambdaInput } from '../../shared/types';
 import { validateRequestParameters } from './validateRequestParameters';
 import { LAB_ORDER_TASK } from 'utils';
 import { createOystehrClient } from '../../shared/helpers';
-import { getAuth0Token } from '../../shared';
+import { getAuth0Token, topLevelCatch } from '../../shared';
 
 export interface ReviewLabResultSubscriptionInput {
   diagnosticReport: DiagnosticReport;
@@ -18,83 +19,110 @@ let zapehrToken: string;
 export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
   console.log(`Input: ${JSON.stringify(input, undefined, 2)}`);
 
-  const { diagnosticReport, secrets } = validateRequestParameters(input);
+  try {
+    const { diagnosticReport, secrets } = validateRequestParameters(input);
 
-  if (!zapehrToken) {
-    console.log('getting token');
-    zapehrToken = await getAuth0Token(secrets);
-  } else {
-    console.log('already have token');
-  }
+    if (!diagnosticReport.id)
+      throw new Error(`Triggering DiagnosticReport did not have an id. ${JSON.stringify(diagnosticReport)}`);
 
-  const oystehr = createOystehrClient(zapehrToken, secrets);
-  // See if the diagnosticReport has any existing tasks associated in the
-  // if there were existing in-progress or ready tasks, then those should be set to 'cancelled' (two l's)
-  // these should also specifically be Result review tasks which is captured by code-RPRT or RFRT.
-  // {
-  //   system: 'external-lab-task',
-  //   code: 'PST' | 'RPRT' | 'RFRT',
-  // },
-  if (!diagnosticReport.id) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: `Triggering DiagnosticReport did not have an id. ${diagnosticReport}` }),
-    };
-  }
+    // TODO: in the future, this should probably also include 'corrected'. Corrected results only come in for final results,
+    // so it can still make a RFRT or we can another task type for corrected results
+    if (!['preliminary', 'final'].includes(diagnosticReport.status))
+      throw new Error(
+        `Triggering DiagnosticReport.status was not preliminary or final. ${JSON.stringify(diagnosticReport)}`
+      );
 
-  // const bundle = await oystehr.fhir.search<Task>({
-  //   resourceType: 'Task',
-  //   params: [
-  //     { name: 'basedOn', value: `DiagnosticReport/${diagnosticReport.id}` },
-  //     { name: 'code:not', value: LAB_ORDER_TASK.code.presubmission },
-  //   ],
-  // });
+    if (!zapehrToken) {
+      console.log('getting token');
+      zapehrToken = await getAuth0Token(secrets);
+    } else {
+      console.log('already have token');
+    }
 
-  // console.log(`>>> This is the task bundle, ${JSON.stringify(bundle, undefined, 2)}`);
+    const oystehr = createOystehrClient(zapehrToken, secrets);
+    const requests: BatchInputRequest<Task>[] = [];
 
-  // make a Task with status = ready, owner = undefined
-  // If DR.status == ‘preliminary’, Task.code = RPRT
-  // If the DR.status == ‘final’, Task.code = RFRT
-  if (!['preliminary', 'final'].includes(diagnosticReport.status)) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: `Triggering DiagnosticReport.status was not preliminary or final. ${diagnosticReport}`,
-      }),
-    };
-  }
+    // See if the diagnosticReport has any existing tasks associated in the
+    // if there were existing in-progress or ready tasks, then those should be set to 'cancelled' (two l's)
+    const existingTasks = (
+      await oystehr.fhir.search<Task>({
+        resourceType: 'Task',
+        params: [
+          { name: 'based-on', value: `DiagnosticReport/${diagnosticReport.id}` },
+          { name: 'code:not', value: LAB_ORDER_TASK.code.presubmission },
+          { name: 'status', value: 'ready,in-progress' },
+        ],
+      })
+    ).unbundle();
 
-  const task: Task = {
-    resourceType: 'Task',
-    intent: 'reflex-order', // TODO: should this be 'order' instead?
-    basedOn: [
-      {
-        type: 'DiagnosticReport',
-        reference: `DiagnosticReport/${diagnosticReport.id}`,
-      },
-    ],
-    status: 'ready',
-    code: {
-      coding: [
+    existingTasks.forEach((task) => {
+      if (task.id)
+        requests.push({
+          url: `/Task/${task.id}`,
+          method: 'PATCH',
+          operations: [
+            {
+              op: 'replace',
+              path: '/status',
+              value: 'cancelled',
+            },
+          ],
+        });
+    });
+
+    // make the new task
+    const newTask: Task = {
+      resourceType: 'Task',
+      intent: 'reflex-order', // TODO: should this be 'order' instead?
+      basedOn: [
         {
-          system: LAB_ORDER_TASK.system,
-          code:
-            diagnosticReport.status === 'preliminary'
-              ? LAB_ORDER_TASK.code.reviewPreliminaryResult
-              : LAB_ORDER_TASK.code.reviewFinalResult,
+          type: 'DiagnosticReport',
+          reference: `DiagnosticReport/${diagnosticReport.id}`,
         },
       ],
-    },
-  };
+      status: 'ready',
+      code: {
+        coding: [
+          {
+            system: LAB_ORDER_TASK.system,
+            code:
+              diagnosticReport.status === 'preliminary'
+                ? LAB_ORDER_TASK.code.reviewPreliminaryResult
+                : LAB_ORDER_TASK.code.reviewFinalResult,
+          },
+        ],
+      },
+    };
 
-  const createdTask = await oystehr.fhir.create<Task>(task);
+    requests.push({
+      method: 'POST',
+      url: '/Task',
+      resource: newTask,
+    });
 
-  const response = {
-    message: `Successfully created task, ${JSON.stringify(createdTask)}`,
-  };
+    const oystehrResponse = await oystehr.fhir.transaction<Task>({ requests });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-  };
+    const response: {
+      [key: string]: Task[];
+    } = {
+      updatedTasks: [],
+      createdTasks: [],
+    };
+
+    oystehrResponse.entry?.forEach((ent) => {
+      if (ent.response?.outcome?.id === 'ok' && ent.resource) response.updatedTasks.push(ent.resource);
+      else if (ent.response?.outcome?.id === 'created' && ent.resource) response.createdTasks.push(ent.resource);
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error: any) {
+    await topLevelCatch('create-review-lab-results-task', error, input.secrets);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
