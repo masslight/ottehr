c11d9c3e46b32c85e5b6a88b32f022779671916e
imbenham <isaac.benham@gmail.com>
2025-04-07 13:42:44 -0400

    fill required fields

--- apps/intake/tests/specs/in-person/PrebookFillPaperworkRequiredFields.spec.ts
+++ apps/intake/tests/specs/in-person/PrebookFillPaperworkRequiredFields.spec.ts
@@ -64,20 +64,25 @@ test.describe('Prebook In person visit - Paperwork submission flow with only req
     await paperwork.selectSelfPayPayment();
     await commonLocatorsHelper.clickContinue();
     await expect(locator.flowHeading).toBeVisible();
+    await expect(locator.flowHeading).toHaveText('Credit card details');
+  });
+  test('PRF-4 Skip Card selection and proceed to repsonsible party page', async () => {
+    await commonLocatorsHelper.clickContinue();
+    await expect(locator.flowHeading).toBeVisible();
     await expect(locator.flowHeading).toHaveText('Responsible party information');
   });
-  test('PRF-4 Fill responsible party details', async () => {
+  test('PRF-5 Fill responsible party details', async () => {
     await paperwork.fillResponsiblePartyDataSelf();
     await commonLocatorsHelper.clickContinue();
     await expect(locator.flowHeading).toHaveText('Photo ID');
   });
-  test('PRF-5 Skip photo ID and complete consent forms', async () => {
+  test('PRF-6 Skip photo ID and complete consent forms', async () => {
     await paperwork.skipPhotoID();
     await paperwork.fillConsentForms();
     await commonLocatorsHelper.clickContinue();
     await expect(locator.flowHeading).toHaveText('Review and submit');
   });
-  test('PRF-6 Submit paperwork', async () => {
+  test('PRF-7 Submit paperwork', async () => {
     await commonLocatorsHelper.clickContinue();
     await expect(locator.flowHeading).toBeVisible();
     await expect(locator.flowHeading).toHaveText('Thank you for choosing Ottehr!');
