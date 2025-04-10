a00d08df414d1394230b1725d6013b852a38fb65
Athena Braun <abraun@masslight.com>
2025-04-10 11:52:42 -0400

    update validation, add authoredOn

--- packages/zambdas/src/subscriptions/diagnostic-report/create-review-lab-results-task/validateRequestParameters.ts
+++ packages/zambdas/src/subscriptions/diagnostic-report/create-review-lab-results-task/validateRequestParameters.ts
@@ -14,6 +14,16 @@ export function validateRequestParameters(input: ZambdaInput): ReviewLabResultSu
     throw new Error(`resource parsed should be a DiagnosticReport but was a ${diagnosticReport.resourceType}`);
   }
 
+  if (!diagnosticReport.id)
+    throw new Error(`Triggering DiagnosticReport did not have an id. ${JSON.stringify(diagnosticReport)}`);
+
+  // TODO: in the future, this should probably also include 'corrected'. Corrected results only come in for final results,
+  // so it can still make a RFRT or we can another task type for corrected results
+  if (!['preliminary', 'final'].includes(diagnosticReport.status))
+    throw new Error(
+      `Triggering DiagnosticReport.status was not preliminary or final. ${JSON.stringify(diagnosticReport)}`
+    );
+
   return {
     diagnosticReport: diagnosticReport as DiagnosticReport,
     secrets: input.secrets,
