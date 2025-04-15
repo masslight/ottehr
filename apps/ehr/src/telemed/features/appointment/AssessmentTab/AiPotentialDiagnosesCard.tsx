557e1afb117607e2a2982d2d725f478adc70c5d2
bholyshevskyi <bholyshevskyi@masslight.com>
2025-04-15 12:56:53 +0000

    fix

--- apps/ehr/src/telemed/features/appointment/AssessmentTab/AiPotentialDiagnosesCard.tsx
+++ apps/ehr/src/telemed/features/appointment/AssessmentTab/AiPotentialDiagnosesCard.tsx
@@ -15,66 +15,65 @@ export const AiPotentialDiagnosesCard: FC = () => {
   const handleClose = (): void => {
     setVisible(false);
   };
-  return (
-    visible &&
-    aiPotentialDiagnoses.length > 0 && (
+  return visible && aiPotentialDiagnoses.length > 0 ? (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1px',
+        backgroundColor: theme.palette.background.paper,
+        border: `1px solid ${otherColors.solidLine}`,
+        borderRadius: 1,
+        marginBottom: '16px',
+        padding: '16px',
+      }}
+    >
       <Box
-        sx={{
+        style={{
           display: 'flex',
-          flexDirection: 'column',
-          gap: '1px',
-          backgroundColor: theme.palette.background.paper,
-          border: `1px solid ${otherColors.solidLine}`,
-          borderRadius: 1,
-          marginBottom: '16px',
-          padding: '16px',
+          borderRadius: '8px',
+          marginBottom: '8px',
+          alignItems: 'center',
+          justifyContent: 'space-between',
         }}
       >
         <Box
           style={{
             display: 'flex',
-            borderRadius: '8px',
-            marginBottom: '8px',
             alignItems: 'center',
-            justifyContent: 'space-between',
           }}
         >
-          <Box
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <img src={ottehrAiIcon} style={{ width: '30px', marginRight: '8px' }} />
-            <Typography variant="subtitle2" style={{ fontWeight: 700, fontSize: '14px' }}>
-              OTTEHR AI
-            </Typography>
-          </Box>
-          <IconButton onClick={handleClose} aria-label="Close">
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        <Box
-          style={{
-            background: '#FFF9EF',
-            borderRadius: '8px',
-            padding: '8px',
-          }}
-        >
-          <Typography variant="body1" style={{ fontWeight: 700, marginBottom: '8px' }}>
-            Potential Diagnoses with ICD-10 Codes
+          <img src={ottehrAiIcon} style={{ width: '30px', marginRight: '8px' }} />
+          <Typography variant="subtitle2" style={{ fontWeight: 700, fontSize: '14px' }}>
+            OTTEHR AI
           </Typography>
-          <ul>
-            {aiPotentialDiagnoses.map((diagnosis) => {
-              return (
-                <li key={diagnosis.code}>
-                  <Typography variant="body1">{diagnosis.code + ': ' + diagnosis.display}</Typography>
-                </li>
-              );
-            })}
-          </ul>
         </Box>
+        <IconButton onClick={handleClose} aria-label="Close">
+          <CloseIcon />
+        </IconButton>
       </Box>
-    )
+      <Box
+        style={{
+          background: '#FFF9EF',
+          borderRadius: '8px',
+          padding: '8px',
+        }}
+      >
+        <Typography variant="body1" style={{ fontWeight: 700, marginBottom: '8px' }}>
+          Potential Diagnoses with ICD-10 Codes
+        </Typography>
+        <ul>
+          {aiPotentialDiagnoses.map((diagnosis) => {
+            return (
+              <li key={diagnosis.code}>
+                <Typography variant="body1">{diagnosis.code + ': ' + diagnosis.display}</Typography>
+              </li>
+            );
+          })}
+        </ul>
+      </Box>
+    </Box>
+  ) : (
+    <></>
   );
 };
