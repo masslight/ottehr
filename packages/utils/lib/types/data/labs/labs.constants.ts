26a90408cbfaf484bc2e0d5e5fffbe8fb7f46d99
szaccagni <sarahzaccagni@gmail.com>
2025-04-25 09:04:57 -0400

    fix OYSTEHR_LAB_OI_CODE_SYSTEM

--- packages/utils/lib/types/data/labs/labs.constants.ts
+++ packages/utils/lib/types/data/labs/labs.constants.ts
@@ -43,7 +43,7 @@ export const ADDED_VIA_LAB_ORDER_SYSTEM = 'http://ottehr.org/fhir/StructureDefin
 // These are oystehr dependent
 // meaning that there is logic in oystehr labs specifically looking for these systems
 // so if we dont like any of them, we have to change there too
-export const OYSTEHR_LAB_OI_CODE_SYSTEM = 'https://terminology.fhir.oystehr.com/CodeSystem/oystehr-oi-codes';
+export const OYSTEHR_LAB_OI_CODE_SYSTEM = 'https://terminology.fhir.oystehr.com/CodeSystem/oystehr-lab-local-codes';
 export const FHIR_IDC10_VALUESET_SYSTEM = 'http://hl7.org/fhir/valueset-icd-10.html';
 ('http://snomed.info/sct');
 export const OYSTEHR_LAB_GUID_SYSTEM = 'https://identifiers.fhir.oystehr.com/lab-guid';
