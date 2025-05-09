a345fad70fafd0590ff9bbb3f98ebf2f60c7dd18
Olha Kovalenko <okovalenko@masslight.com>
2025-05-09 11:24:46 +0000

    wip

---
+++ apps/ehr/tests/e2e/page/patient-information/DiscardChangesDialog.ts
@@ -0,0 +1,27 @@
+import { Page } from '@playwright/test';
+import { dataTestIds } from '../../../../src/constants/data-test-ids';
+
+export class DiscardChangesDialog {
+  #page: Page;
+
+  constructor(page: Page) {
+    this.#page = page;
+  }
+
+  async clickDiscardChangesButton(): Promise<void> {
+    await this.#page.getByTestId(dataTestIds.dialog.proceedButton).click();
+  }
+
+  async clickCancelButton(): Promise<void> {
+    await this.#page.getByTestId(dataTestIds.dialog.closeButton).click();
+  }
+
+  async clickCloseIcon(): Promise<void> {
+    await this.#page.getByTestId(dataTestIds.closeIcon).click();
+  }
+}
+
+export async function expectDiscardChangesDialog(page: Page): Promise<DiscardChangesDialog> {
+  await page.getByTestId(dataTestIds.dialog.title).isVisible();
+  return new DiscardChangesDialog(page);
+}
---
+++ apps/ehr/tests/e2e/specs/patientRecordPageDiscardChangesDialog.spec.ts
@@ -0,0 +1,55 @@
+import { test } from '@playwright/test';
+import { PATIENT_FIRST_NAME, ResourceHandler } from '../../e2e-utils/resource-handler';
+
+import { expectPatientInformationPage, openPatientInformationPage } from '../page/PatientInformationPage';
+
+import { expectDiscardChangesDialog } from '../page/patient-information/DiscardChangesDialog';
+import { expectPatientRecordPage } from '../page/PatientRecordPage';
+const NEW_PATIENT_FIRST_NAME = 'Test_firstname';
+
+test.describe('Discard Changes Dialog non-mutating tests', () => {
+  const resourceHandler = new ResourceHandler();
+
+  test.beforeAll(async () => {
+    await resourceHandler.setResources();
+    await Promise.all([
+      resourceHandler.waitTillAppointmentPreprocessed(resourceHandler.appointment.id!),
+      resourceHandler.waitTillHarvestingDone(resourceHandler.appointment.id!),
+    ]);
+  });
+
+  test.afterAll(async () => {
+    await resourceHandler.cleanupResources();
+  });
+
+  test('Click on [Discard changes] button, Patient Record page is opened', async ({ page }) => {
+    let patientInformationPage = await openPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.enterPatientFirstName(NEW_PATIENT_FIRST_NAME);
+    await patientInformationPage.clickCloseButton();
+    const discardChangesDialog = await expectDiscardChangesDialog(page);
+    await discardChangesDialog.clickDiscardChangesButton();
+    await expectPatientRecordPage(resourceHandler.patient.id!, page);
+    patientInformationPage = await openPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.verifyPatientFirstName(PATIENT_FIRST_NAME);
+  });
+
+  test('Click on [Cancel] button, user stays on Patient Information page', async ({ page }) => {
+    const patientInformationPage = await openPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.enterPatientFirstName(NEW_PATIENT_FIRST_NAME);
+    await patientInformationPage.clickCloseButton();
+    const discardChangesDialog = await expectDiscardChangesDialog(page);
+    await discardChangesDialog.clickCancelButton();
+    await expectPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.verifyPatientFirstName(NEW_PATIENT_FIRST_NAME);
+  });
+
+  test('Click on [x] icon, user stays on Patient Information page', async ({ page }) => {
+    const patientInformationPage = await openPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.enterPatientFirstName(NEW_PATIENT_FIRST_NAME);
+    await patientInformationPage.clickCloseButton();
+    const discardChangesDialog = await expectDiscardChangesDialog(page);
+    await discardChangesDialog.clickCloseIcon();
+    await expectPatientInformationPage(page, resourceHandler.patient.id!);
+    await patientInformationPage.verifyPatientFirstName(NEW_PATIENT_FIRST_NAME);
+  });
+});
