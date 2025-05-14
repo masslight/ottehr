a53a303c1d44b905a11771a2c58c0b5d8e05c7cc
Athena Braun <abraun@masslight.com>
2025-05-14 00:17:51 -0400

    add correctedResults logs to get-lab-orders

--- packages/zambdas/src/ehr/get-lab-orders/helpers.ts
+++ packages/zambdas/src/ehr/get-lab-orders/helpers.ts
@@ -1024,6 +1024,7 @@ export const parseLabOrderStatus = (
         pendingStatusConditions,
         sentStatusConditions,
         prelimStatusConditions,
+        correctedResultConditions,
         receivedStatusConditions,
         reviewedStatusConditions,
       },
