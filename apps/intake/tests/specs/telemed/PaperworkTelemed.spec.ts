523cd64fb84abbc01e8b3b8897db37e178bf14a5
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-05-16 10:05:56 -0400

    remove `checkContactInformationPageOpens`

--- apps/intake/tests/specs/telemed/PaperworkTelemed.spec.ts
+++ apps/intake/tests/specs/telemed/PaperworkTelemed.spec.ts
@@ -66,7 +66,6 @@ test.describe('Contact information screen - Check and fill all fields', () => {
     await page.goto(bookingData.bookingURL);
     await paperwork.clickProceedToPaperwork();
     await paperwork.checkCorrectPageOpens('Contact information');
-    await paperwork.checkContactInformationPageOpens();
   });
   test('PCI-2 Fill Contact Information all fields', async () => {
     await paperwork.fillContactInformationAllFields();
