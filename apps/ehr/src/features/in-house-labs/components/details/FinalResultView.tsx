1310376a59bfd5ffe32fd7e3f639407d1be8e113
szaccagni <sarahzaccagni@gmail.com>
2025-05-29 12:57:55 -0400

    adds repeat tests to final view

--- apps/ehr/src/features/in-house-labs/components/details/FinalResultView.tsx
+++ apps/ehr/src/features/in-house-labs/components/details/FinalResultView.tsx
@@ -1,102 +1,50 @@
-import React, { useState } from 'react';
-import { Box, Paper, Typography, Button, Chip } from '@mui/material';
-import { getFormattedDiagnoses, ResultEntryInput } from 'utils';
-import { ResultEntryRadioButton } from './ResultEntryRadioButton';
-import { ResultEntryTable } from './ResultsEntryTable';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { useForm, FormProvider } from 'react-hook-form';
-import { InHouseLabOrderHistory } from './InHouseLabOrderHistory';
-import { BiotechOutlined } from '@mui/icons-material';
+import React from 'react';
+import { Box, Typography, Button, Paper } from '@mui/material';
 import { InHouseOrderDetailPageDTO } from 'utils/lib/types/data/in-house/in-house.types';
+import { getFormattedDiagnoses, DiagnosisDTO } from 'utils';
+import { FinalResultCard } from './FinalResultCard';
 
 interface FinalResultViewProps {
-  testDetails: InHouseOrderDetailPageDTO;
+  testDetails: InHouseOrderDetailPageDTO[] | undefined;
   onBack: () => void;
 }
 
 export const FinalResultView: React.FC<FinalResultViewProps> = ({ testDetails, onBack }) => {
-  const openPdf = (): void => {
-    if (testDetails.resultsPDFUrl) {
-      window.open(testDetails.resultsPDFUrl, '_blank');
-    }
-  };
-  const [showDetails, setShowDetails] = useState(false);
-
-  const radioResultMap = testDetails.labDetails.components.radioComponents.reduce((acc: any, item) => {
-    if (item.result?.entry) acc[item.observationDefinitionId] = item.result.entry;
+  const diagnoses = testDetails?.reduce((acc: DiagnosisDTO[], detail) => {
+    detail.diagnosesDTO.forEach((diagnoses) => {
+      if (!acc.some((d) => d.code === diagnoses.code)) {
+        acc.push(diagnoses);
+      }
+    });
     return acc;
-  }, {});
-  const tableResultMap = testDetails.labDetails.components.groupedComponents.reduce((acc: any, item) => {
-    if (item.result?.entry) acc[item.observationDefinitionId] = item.result.entry;
-    return acc;
-  }, {});
-  const methods = useForm<ResultEntryInput>({
-    defaultValues: { ...radioResultMap, ...tableResultMap },
-  });
+  }, []);
 
-  return (
-    <FormProvider {...methods}>
+  if (!testDetails) {
+    return (
       <Box>
-        <Typography variant="body1" sx={{ mb: 2, fontWeight: 'medium' }}>
-          {getFormattedDiagnoses(testDetails.diagnosesDTO)}
-        </Typography>
-
-        <Paper sx={{ mb: 2 }}>
-          <Box sx={{ p: 3 }}>
-            <Box display="flex" justifyContent="space-between" alignItems="center" mb={3}>
-              <Typography variant="h5" color="primary.dark" fontWeight="bold">
-                {testDetails.testItemName}
-              </Typography>
-              <Chip
-                label="FINAL"
-                sx={{
-                  color: '#1976D2',
-                  bgcolor: '#E6F4FF',
-                  fontWeight: 'bold',
-                  borderRadius: '4px',
-                  height: '24px',
-                }}
-              />
-            </Box>
-            {testDetails.labDetails.components.radioComponents.map((component) => {
-              return <ResultEntryRadioButton testItemComponent={component} disabled={true} />;
-            })}
-
-            {testDetails.labDetails.components.groupedComponents.length > 0 && (
-              <ResultEntryTable
-                testItemComponents={testDetails.labDetails.components.groupedComponents}
-                disabled={true}
-              />
-            )}
-            <Box display="flex" justifyContent="space-between" mt={2} mb={3}>
-              <Button
-                variant="outlined"
-                color="primary"
-                sx={{ borderRadius: '50px', textTransform: 'none' }}
-                onClick={() => openPdf()}
-                startIcon={<BiotechOutlined />}
-              >
-                Results PDF
-              </Button>
-              <Button
-                variant="text"
-                color="primary"
-                onClick={() => setShowDetails(!showDetails)}
-                endIcon={showDetails ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-              >
-                Details
-              </Button>
-            </Box>
-            <InHouseLabOrderHistory showDetails={showDetails} testDetails={testDetails} />
-          </Box>
+        <Paper sx={{ p: 3, textAlign: 'center' }}>
+          <Typography variant="h6" color="error">
+            Test details not found
+          </Typography>
         </Paper>
-        <Box display="flex" justifyContent="space-between" alignItems="center" mt={3}>
-          <Button variant="outlined" onClick={onBack} sx={{ borderRadius: '50px', px: 4 }}>
-            Back
-          </Button>
-        </Box>
       </Box>
-    </FormProvider>
+    );
+  }
+  return (
+    <Box>
+      <Typography variant="body1" sx={{ mb: 2, fontWeight: 'medium' }}>
+        {getFormattedDiagnoses(diagnoses || [])}
+      </Typography>
+
+      {testDetails.map((test) => (
+        <FinalResultCard testDetails={test} />
+      ))}
+
+      <Box display="flex" justifyContent="space-between" alignItems="center" mt={3}>
+        <Button variant="outlined" onClick={onBack} sx={{ borderRadius: '50px', px: 4 }}>
+          Back
+        </Button>
+      </Box>
+    </Box>
   );
 };
--- packages/zambdas/src/ehr/get-lab-orders/helpers.ts
+++ packages/zambdas/src/ehr/get-lab-orders/helpers.ts
@@ -42,10 +42,10 @@ import {
   fetchDocumentReferencesForDiagnosticReports,
   fetchLabOrderPDFs,
   Secrets,
+  compareDates,
 } from 'utils';
 import { GetZambdaLabOrdersParams } from './validateRequestParameters';
 import { DiagnosisDTO, LabOrderDTO, ExternalLabsStatus, LAB_ORDER_TASK, PSC_HOLD_CONFIG } from 'utils';
-import { DateTime } from 'luxon';
 import { captureSentryException } from '../../shared';
 import { sendErrors } from '../../shared';
 
@@ -1086,25 +1086,6 @@ const deletePrelimResultsIfFinalExists = (prelimMap: Map<string, DiagnosticRepor
       prelimMap.delete(id);
     }
   });
-};
-
-/**
- * Compares two dates.
- * The most recent date will be first,
- * invalid dates will be last
- */
-export const compareDates = (a: string | undefined, b: string | undefined): number => {
-  const dateA = DateTime.fromISO(a || '');
-  const dateB = DateTime.fromISO(b || '');
-  const isDateAValid = dateA.isValid;
-  const isDateBValid = dateB.isValid;
-
-  // if one date is valid and the other is not, the valid date should be first
-  if (isDateAValid && !isDateBValid) return -1;
-  if (!isDateAValid && isDateBValid) return 1;
-  if (!isDateAValid && !isDateBValid) return 0;
-
-  return dateB.toMillis() - dateA.toMillis();
 };
 
 export const parsePaginationFromResponse = (data: {
