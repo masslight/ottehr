45c4c9a4f63f0fa435a9b7053924f527fbe671db
szaccagni <sarahzaccagni@gmail.com>
2025-05-30 14:03:24 -0400

    fix build

--- apps/ehr/src/features/common/DetailPageContainer.tsx
+++ apps/ehr/src/features/common/DetailPageContainer.tsx
@@ -1,10 +1,10 @@
 import { Stack } from '@mui/material';
-import { ReactNode } from 'react';
+import { ReactNode, ReactElement } from 'react';
 
 interface DetailPageContainerProps {
   children: ReactNode;
 }
-export default function DetailPageContainer({ children }: DetailPageContainerProps): ReactNode {
+export default function DetailPageContainer({ children }: DetailPageContainerProps): ReactElement {
   return (
     <Stack id="detail-page-container" spacing={2} sx={{ p: 0, maxWidth: '680px !important', mx: 'auto' }}>
       {children}
