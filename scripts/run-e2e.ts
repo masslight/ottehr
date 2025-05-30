79a211a06044bc5cb23e98905c3dd8e65649ab8e
Alex Willingham <awillingham@masslight.com>
2025-05-30 08:58:19 -0400

    Revert "fix(ci): explicitly log zambda output in ci"
    This reverts commit c847c49f40144d8bde28b0207fba4036e341df2a.

--- scripts/run-e2e.ts
+++ scripts/run-e2e.ts
@@ -77,14 +77,9 @@ const waitForApp = async (app: (typeof supportedApps)[number]): Promise<void> =>
 };
 
 const startZambdas = (): void => {
-  const childProcess = spawn('cross-env', [`ENV=${envMapping['ehr'][ENV]}`, 'npm', 'run', `zambdas:start`], {
+  spawn('cross-env', [`ENV=${envMapping['ehr'][ENV]}`, 'npm', 'run', `zambdas:start`], {
     stdio: 'inherit',
     env: { ...process.env, ENV: envMapping['ehr'][ENV] },
-  });
-
-  childProcess.stdout?.on('data', (data) => {
-    const message = data.toString();
-    console.log(`[zambdas stdout]: ${message}`);
   });
 };
 
