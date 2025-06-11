a4fb4fcbf0a9534cf8d1033094f569371d22c1c5
bholyshevskyi <bholyshevskyi@masslight.com>
2025-06-11 11:39:25 +0000

    wip

--- packages/utils/lib/fhir/helpers.ts
+++ packages/utils/lib/fhir/helpers.ts
@@ -1306,3 +1306,21 @@ export const getSlugForBookableResource = (resource: BookableResource): string |
     return id.system === SLUG_SYSTEM;
   })?.value;
 };
+
+const OTTEHR_FHIR_URL = 'https://fhir.ottehr.com';
+
+export const ottehrCodeSystemUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/CodeSystem/' + name;
+};
+
+export const ottehrValueSetUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/ValueSet/' + name;
+};
+
+export const ottehrExtensionUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/Extension/' + name;
+};
+
+export const ottehrIdentifierSystem = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/Identifier/' + name;
+};
--- packages/utils/lib/types/api/procedures.constants.ts
+++ packages/utils/lib/types/api/procedures.constants.ts
@@ -1,12 +1,13 @@
-export const PROCEDURE_TYPES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/type';
-export const MEDICATIONS_USED_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/medications-used';
-export const BODY_SITES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/body-sites';
-export const BODY_SIDES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/body-sides';
-export const TECHNIQUES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/techniques';
-export const SUPPLIES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/supplies';
-export const COMPLICATIONS_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/complications';
-export const PATIENT_RESPONSES_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/patient-responses';
-export const POST_PROCEDURE_INSTRUCTIONS_VALUE_SET_URL =
-  'https://ottehr.com/FHIR/ValueSet/procedure/post-procedure-instructions';
-export const TIME_SPENT_VALUE_SET_URL = 'https://ottehr.com/FHIR/ValueSet/procedure/time-spent';
-export const PROCEDURE_TYPE_CPT_EXTENSION_URL = 'https://ottehr.com/FHIR/Procedures/procedure-type-cpt';
+import { ottehrExtensionUrl, ottehrValueSetUrl } from '../../fhir';
+
+export const PROCEDURE_TYPES_VALUE_SET_URL = ottehrValueSetUrl('procedure-type');
+export const MEDICATIONS_USED_VALUE_SET_URL = ottehrValueSetUrl('procedure-medications-used');
+export const BODY_SITES_VALUE_SET_URL = ottehrValueSetUrl('procedure-body-sites');
+export const BODY_SIDES_VALUE_SET_URL = ottehrValueSetUrl('procedure-body-sides');
+export const TECHNIQUES_VALUE_SET_URL = ottehrValueSetUrl('procedure-techniques');
+export const SUPPLIES_VALUE_SET_URL = ottehrValueSetUrl('procedure-supplies');
+export const COMPLICATIONS_VALUE_SET_URL = ottehrValueSetUrl('procedure-complications');
+export const PATIENT_RESPONSES_VALUE_SET_URL = ottehrValueSetUrl('procedure-patient-responses');
+export const POST_PROCEDURE_INSTRUCTIONS_VALUE_SET_URL = ottehrValueSetUrl('procedure-post-instructions');
+export const TIME_SPENT_VALUE_SET_URL = ottehrValueSetUrl('procedure-time-spent');
+export const PROCEDURE_TYPE_CPT_EXTENSION_URL = ottehrExtensionUrl('procedure-type-cpt');
