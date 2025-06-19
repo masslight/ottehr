77830d0f7b85d48e84a8b204db4440648a557642
bholyshevskyi <114926438+bholyshevskyi@users.noreply.github.com>
2025-06-19 18:39:19 +0300

    Procedures - Take values for dropdowns from FHIR instead of hardcoded (#2602)

--- packages/utils/lib/fhir/helpers.ts
+++ packages/utils/lib/fhir/helpers.ts
@@ -1307,6 +1307,24 @@ export const getSlugForBookableResource = (resource: BookableResource): string |
   })?.value;
 };
 
+const OTTEHR_FHIR_URL = 'https://fhir.ottehr.com';
+
+export const ottehrCodeSystemUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/CodeSystem/' + name;
+};
+
+export const ottehrValueSetUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/ValueSet/' + name;
+};
+
+export const ottehrExtensionUrl = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/Extension/' + name;
+};
+
+export const ottehrIdentifierSystem = (name: string): string => {
+  return OTTEHR_FHIR_URL + '/Identifier/' + name;
+};
+
 export async function getAllFhirSearchPages<T extends FhirResource>(
   fhirSearchParams: FhirSearchParams,
   oystehr: Oystehr,
