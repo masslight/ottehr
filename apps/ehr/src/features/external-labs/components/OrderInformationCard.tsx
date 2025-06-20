050579ebc20b1b9d5fba51e0180fffd05ed9214d
szaccagni <sarahzaccagni@gmail.com>
2025-06-20 14:43:29 -0400

    fix type

--- apps/ehr/src/features/external-labs/components/OrderInformationCard.tsx
+++ apps/ehr/src/features/external-labs/components/OrderInformationCard.tsx
@@ -3,16 +3,12 @@ import { Box, Button, Paper, Stack } from '@mui/material';
 import { AccordionCard } from '../../../telemed/components/AccordionCard';
 import React, { useState } from 'react';
 // import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-import { useApiClients } from '../../../hooks/useAppClients';
-import { useParams } from 'react-router';
 import { openPdf } from './OrderCollection';
-import { DocumentReference } from 'fhir/r4b';
-import { getPresignedFileUrl } from '../../../helpers/files.helper';
-import { useAuth0 } from '@auth0/auth0-react';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 interface OrderInfoProps {
+  orderPdfUrl: string | undefined;
   orderAddedDateTime?: string;
   orderingPhysician?: string;
   individualCollectingSample?: string;
@@ -20,41 +16,8 @@ interface OrderInfoProps {
   showInPatientPortal?: boolean;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const OrderInformationCard: React.FC<OrderInfoProps> = ({ orderAddedDateTime, orderingPhysician }) => {
-  const { oystehr } = useApiClients();
-  const { serviceRequestID } = useParams();
+export const OrderInformationCard: React.FC<OrderInfoProps> = ({ orderPdfUrl }) => {
   const [collapsed, setCollapsed] = useState(false);
-  const { getAccessTokenSilently } = useAuth0();
-
-  async function printOrder(): Promise<void> {
-    const documentReferenceSearch = await oystehr?.fhir.search<DocumentReference>({
-      resourceType: 'DocumentReference',
-      params: [
-        {
-          name: 'related',
-          value: `ServiceRequest/${serviceRequestID}`,
-        },
-        {
-          name: 'type',
-          value: '51991-8', // lab order doc
-        },
-      ],
-    });
-    if (documentReferenceSearch?.entry?.length !== 1) {
-      throw new Error('document reference not found');
-    }
-    const url = documentReferenceSearch?.entry?.[0]?.resource?.content?.[0]?.attachment.url;
-    if (!url) {
-      throw new Error('document reference URL not found');
-    }
-    const token = await getAccessTokenSilently();
-    const urlTemp = await getPresignedFileUrl(url, token);
-    if (!urlTemp) {
-      throw new Error('error with a presigned url');
-    }
-    await openPdf(urlTemp);
-  }
 
   return (
     <Box sx={{ mt: 2, mb: 2 }}>
@@ -72,7 +35,8 @@ export const OrderInformationCard: React.FC<OrderInfoProps> = ({ orderAddedDateT
               variant="outlined"
               type="button"
               sx={{ width: 170, borderRadius: '50px', textTransform: 'none' }}
-              onClick={printOrder}
+              disabled={!orderPdfUrl}
+              onClick={() => orderPdfUrl && openPdf(orderPdfUrl)}
             >
               Print order
             </Button>
