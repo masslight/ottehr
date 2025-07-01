559368062436d255ae61c8a41931a5ce69463774
Alex Willingham <awillingham@masslight.com>
2025-06-30 22:32:38 -0400

    feat(radiology): add placer order number, add some logs to pacs webhook, and update fhir terminology urls to our standard format

--- packages/zambdas/src/ehr/radiology/shared/index.ts
+++ packages/zambdas/src/ehr/radiology/shared/index.ts
@@ -1,14 +1,15 @@
 export const ADVAPACS_FHIR_BASE_URL = 'https://usa1.api.integration.advapacs.com/fhir/R5';
 export const ADVAPACS_VIEWER_LAUNCH_URL = 'https://usa1.api.integration.advapacs.com/viewer/launch';
-export const ORDER_TYPE_CODE_SYSTEM = 'https://terminology.fhir.ottehr.com/CodeSystem/order-type-tag';
+export const ORDER_TYPE_CODE_SYSTEM = 'https://fhir.ottehr.com/CodeSystem/order-type-tag';
 export const HL7_IDENTIFIER_TYPE_CODE_SYSTEM = 'http://terminology.hl7.org/CodeSystem/v2-0203';
 export const HL7_IDENTIFIER_TYPE_CODE_SYSTEM_ACCESSION_NUMBER = 'ACSN';
-export const ACCESSION_NUMBER_CODE_SYSTEM = 'https://terminology.fhir.ottehr.com/CodeSystem/accession-number';
-export const ADVAPACS_FHIR_RESOURCE_ID_CODE_SYSTEM =
-  'https://terminology.fhir.ottehr.com/CodeSystem/advapacs-fhir-resource-id';
+export const HL7_IDENTIFIER_TYPE_CODE_SYSTEM_PLACER_ORDER_NUMBER = 'PLAC';
+export const ACCESSION_NUMBER_CODE_SYSTEM = 'https://fhir.ottehr.com/Identifier/accession-number';
+export const PLACER_ORDER_NUMBER_CODE_SYSTEM = 'https://fhir.ottehr.com/Identifier/placer-order-number';
+export const ADVAPACS_FHIR_RESOURCE_ID_CODE_SYSTEM = 'https://fhir.ottehr.com/CodeSystem/advapacs-fhir-resource-id';
 export const DIAGNOSTIC_REPORT_PRELIMINARY_REVIEW_ON_EXTENSION_URL =
-  'https://extensions.fhir.oystehr.com/diagnostic-report-preliminary-review-on';
+  'https://fhir.ottehr.com/Extension/diagnostic-report-preliminary-review-on';
 export const SERVICE_REQUEST_PERFORMED_ON_EXTENSION_URL =
-  'https://extensions.fhir.oystehr.com/service-request-performed-on';
+  'https://fhir.ottehr.com/Extension/service-request-performed-on';
 export const SERVICE_REQUEST_REQUESTED_TIME_EXTENSION_URL =
-  'https://extensions.fhir.ottehr.com/service-request-requested-time';
+  'https://fhir.ottehr.com/Extension/service-request-requested-time';
