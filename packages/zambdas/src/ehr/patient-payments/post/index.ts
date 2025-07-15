3f15f593b7ffb0c03f298f20afe7542fd5cc1a7d
Alex Willingham <awillingham@masslight.com>
2025-07-15 19:54:22 -0400

    add wrapHandler to more zambdas

--- packages/zambdas/src/ehr/patient-payments/post/index.ts
+++ packages/zambdas/src/ehr/patient-payments/post/index.ts
@@ -31,13 +31,16 @@ import {
   makeBusinessIdentifierForStripePayment,
   performCandidPreEncounterSync,
   topLevelCatch,
+  wrapHandler,
   ZambdaInput,
 } from '../../../shared';
 import { getAccountAndCoverageResourcesForPatient } from '../../shared/harvest';
 
+const ZAMBDA_NAME = 'post-patient-payment';
+
 // Lifting up value to outside of the handler allows it to stay in memory across warm lambda invocations
 let oystehrM2MClientToken: string;
-export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+export const index = wrapHandler(ZAMBDA_NAME, async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
   try {
     const authorization = input.headers.Authorization;
     const secrets = input.secrets;
@@ -97,7 +100,7 @@ export const index = async (input: ZambdaInput): Promise<APIGatewayProxyResult> 
     const ENVIRONMENT = getSecret(SecretsKeys.ENVIRONMENT, input.secrets);
     return topLevelCatch('patient-payments-post', error, ENVIRONMENT);
   }
-};
+});
 
 const performEffect = async (
   input: ComplexValidationOutput,
