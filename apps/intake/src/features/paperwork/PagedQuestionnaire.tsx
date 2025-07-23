012579208a0b18817987ff12b06aad5d80da37d1
Samir Omarov <samir.omarov@gmail.com>
2025-07-23 17:04:17 +0200

    temporary: revert hookresolvers version & comment validation changes

--- apps/intake/src/features/paperwork/PagedQuestionnaire.tsx
+++ apps/intake/src/features/paperwork/PagedQuestionnaire.tsx
@@ -1,3 +1,4 @@
+// @ts-expect-error -temporary: testing validaiton with old version
 import { yupResolver } from '@hookform/resolvers/yup';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import {
