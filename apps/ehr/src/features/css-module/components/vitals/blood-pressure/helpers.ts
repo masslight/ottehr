241ade61729d42fcc549462139eae0584bfbc93c
imbenham <isaac.benham@gmail.com>
2025-07-30 14:39:09 -0500

    remove comment

--- apps/ehr/src/features/css-module/components/vitals/blood-pressure/helpers.ts
+++ apps/ehr/src/features/css-module/components/vitals/blood-pressure/helpers.ts
@@ -7,27 +7,3 @@ export const textToBloodPressureNumber = (text: string): number | undefined => {
 };
 
 const roundPressureValue = (bpVal: number): number => roundNumberToDecimalPlaces(bpVal, 0);
-
-/*
-export const composeBloodPressureVitalsHistoryEntries = (
-  encounterId: string,
-  userId: string | undefined,
-  vitalsEntitiesByEncounter: VitalsObservationDTO[],
-  vitalsEntitiesByPatient: VitalsObservationDTO[]
-): VitalBloodPressureHistoryEntry[] => {
-  return composeVitalsHistoryEntries<VitalsBloodPressureObservationDTO, VitalBloodPressureHistoryEntry>(
-    encounterId,
-    userId,
-    vitalsEntitiesByEncounter,
-    vitalsEntitiesByPatient,
-    isBloodPressureVitalObservation,
-    (observation) => {
-      return {
-        systolicPressure: observation.systolicPressure,
-        diastolicPressure: observation.diastolicPressure,
-        bloodPressureSeverity: getBloodPressureSeverity(observation.systolicPressure, observation.diastolicPressure),
-      };
-    }
-  );
-};
-*/
