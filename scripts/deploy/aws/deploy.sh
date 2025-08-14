5a6beeff13c942171ef87b2a781bf4b983c119a1
Colin <colin.t.welch@gmail.com>
2025-08-14 14:19:14 -0400

    feat(deploy/aws): Add support for `stack_prefix` and `aws_profile` (#3606)
    * feat(deploy/aws): Add support for `stack_suffix` and `aws_profile`
    
    * tweak(deploy): Change suffix to prefix

--- scripts/deploy/aws/deploy.sh
+++ scripts/deploy/aws/deploy.sh
@@ -2,6 +2,8 @@
 
 set -eo pipefail
 
+aws_profile=$(grep '"aws_profile"' "$(dirname "$0")/../deploy-config.json" | sed 's/.*: "\(.*\)".*/\1/')
+profile="${aws_profile:-ottehr}"
 project_id=$(grep '"project_id"' "$(dirname "$0")/../deploy-config.json" | sed 's/.*: "\(.*\)".*/\1/')
 access_token=$(grep '"access_token"' "$(dirname "$0")/../deploy-config.json" | sed 's/.*: "\(.*\)".*/\1/')
 provider_email=$(grep '"provider_email"' "$(dirname "$0")/../deploy-config.json" | sed 's/.*: "\(.*\)".*/\1/')
@@ -52,7 +54,7 @@ fi
 
 # first deploy creates infra
 pushd scripts/deploy/aws
-npx cdk deploy --require-approval=never "ottehr-infra-stack-${environment}"
+npx cdk deploy --require-approval=never --profile="${profile}" "ottehr-infra-stack-${environment}"
 # update env files
 npx ts-node ./bin/update-config.ts
 popd
@@ -67,5 +69,5 @@ popd
 
 # second cdk deploy uploads compiled apps
 pushd scripts/deploy/aws
-npx cdk deploy --require-approval=never "ottehr-data-stack-${environment}"
+npx cdk deploy --require-approval=never --profile="${profile}" "ottehr-data-stack-${environment}"
 popd
