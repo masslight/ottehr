c870c31db278645ede68500f815a5e30c7cf85af
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-08-20 07:55:15 -0400

    move project name var to env file

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -4,12 +4,6 @@ variable "project_id" {
   nullable    = false
 }
 
-variable "project_name" {
-  description = "Oystehr project name"
-  type        = string
-  nullable    = false
-}
-
 variable "client_id" {
   description = "Oystehr client ID"
   type        = string
