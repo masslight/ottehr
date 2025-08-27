9237dbe7c0d2608b02906f9cb763ce350a974420
Roman Golchuk <rgolchuk@masslight.com>
2025-08-27 16:32:27 +0200

    chore: move usePendingSupervisorApproval to hooks & clear comments

--- apps/ehr/src/telemed/features/appointment/ReviewTab/ReviewAndSignButton.tsx
+++ apps/ehr/src/telemed/features/appointment/ReviewTab/ReviewAndSignButton.tsx
@@ -2,10 +2,8 @@ import CheckIcon from '@mui/icons-material/Check';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { Box, Checkbox, DialogContentText, FormControlLabel, Stack, Tooltip, Typography } from '@mui/material';
 import { DateTime } from 'luxon';
-import { FC, useCallback, useMemo, useState } from 'react';
-import { pendingSupervisorApproval } from 'src/api/api';
+import { FC, useMemo, useState } from 'react';
 import { FEATURE_FLAGS } from 'src/constants/feature-flags';
-import { useApiClients } from 'src/hooks/useAppClients';
 import useEvolveUser from 'src/hooks/useEvolveUser';
 import {
   getPractitionerQualificationByLocation,
@@ -18,7 +16,7 @@ import { dataTestIds } from '../../../../constants/data-test-ids';
 import { useFeatureFlags } from '../../../../features/css-module/context/featureFlags';
 import { usePractitionerActions } from '../../../../features/css-module/hooks/usePractitioner';
 import { ConfirmationDialog } from '../../../components';
-import { useGetAppointmentAccessibility } from '../../../hooks';
+import { useGetAppointmentAccessibility, usePendingSupervisorApproval } from '../../../hooks';
 import { useOystehrAPIClient } from '../../../hooks/useOystehrAPIClient';
 import {
   useAppointmentData,
@@ -131,30 +129,6 @@ export const ReviewAndSignButton: FC<ReviewAndSignButtonProps> = ({ onSigned }) 
       throw new Error('api client not defined or appointmentId not provided');
     }
 
-    // if (css) {
-    //   try {
-    //     const tz = DateTime.now().zoneName;
-    //     await signAppointment({
-    //       apiClient,
-    //       appointmentId: appointment.id,
-    //       timezone: tz,
-    //     });
-    //     await appointmentRefetch();
-    //   } catch (error: any) {
-    //     console.log(error.message);
-    //   }
-    // } else {
-    //   await changeTelemedAppointmentStatus({
-    //     apiClient,
-    //     appointmentId: appointment.id,
-    //     newStatus: TelemedAppointmentStatusEnum.complete,
-    //   });
-    //   appointmentSetState({
-    //     encounter: { ...encounter, status: 'finished' },
-    //     appointment: { ...appointment, status: 'fulfilled' },
-    //   });
-    // }
-
     if (FEATURE_FLAGS.SUPERVISOR_APPROVAL_ENABLED && requireSupervisorApproval) {
       await updateVisitStatusToAwaitSupervisorApproval();
     } else {
@@ -265,60 +239,4 @@ export const ReviewAndSignButton: FC<ReviewAndSignButtonProps> = ({ onSigned }) 
       </Tooltip>
     </Box>
   );
-};
-
-export const usePendingSupervisorApproval = ({
-  encounterId,
-  practitionerId,
-}: {
-  encounterId: string;
-  practitionerId: string;
-}): {
-  loading: boolean;
-  error: Error | null;
-  updateVisitStatusToAwaitSupervisorApproval: () => Promise<void>;
-} => {
-  const { oystehrZambda } = useApiClients();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-
-  const updateVisitStatusToAwaitSupervisorApproval = useCallback(async (): Promise<void> => {
-    if (!oystehrZambda) {
-      console.error('oystehrZambda is not defined');
-      return;
-    }
-
-    if (!encounterId) {
-      console.error('encounterId is undefined — skipping update.');
-      return;
-    }
-
-    if (!practitionerId) {
-      console.error('practitionerId is undefined — skipping update.');
-      return;
-    }
-
-    setLoading(true);
-    setError(null);
-
-    try {
-      try {
-        await pendingSupervisorApproval(oystehrZambda, { encounterId, practitionerId });
-      } catch (err) {
-        console.error('Error updating nursing order:', err);
-        setError(err instanceof Error ? err : new Error('Unknown error occurred'));
-      }
-    } catch (error) {
-      console.error('error with setting pending supervisor approval:', error);
-      setError(error instanceof Error ? error : new Error('Unknown error occurred'));
-    } finally {
-      setLoading(false);
-    }
-  }, [oystehrZambda, encounterId, practitionerId]);
-
-  return {
-    loading,
-    error,
-    updateVisitStatusToAwaitSupervisorApproval,
-  };
 };
