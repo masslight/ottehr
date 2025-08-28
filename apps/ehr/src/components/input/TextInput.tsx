4737108a6845b13555b804fb9eff213f20cb0f91
bholyshevskyi <114926438+bholyshevskyi@users.noreply.github.com>
2025-08-28 11:42:27 +0300

    Immunization. Enable Immunization section in UI. Bugfixes and improvements (#3694)

--- apps/ehr/src/components/input/TextInput.tsx
+++ apps/ehr/src/components/input/TextInput.tsx
@@ -2,35 +2,47 @@ import { Box, FormHelperText, Skeleton, TextField } from '@mui/material';
 import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { REQUIRED_FIELD_ERROR_MESSAGE } from 'utils';
+import InputMask from '../InputMask';
 
 type Props = {
   name: string;
   label: string;
+  type?: 'text' | 'number' | 'phone';
   loading?: boolean;
   required?: boolean;
   multiline?: boolean;
+  validate?: (value: string) => boolean | string;
 };
 
-export const TextInput: React.FC<Props> = ({ name, label, loading, required, multiline }) => {
+export const TextInput: React.FC<Props> = ({ name, label, type, loading, required, multiline, validate }) => {
   const { control } = useFormContext();
   return !loading ? (
     <Controller
       name={name}
       control={control}
       defaultValue=""
-      rules={{ required: required ? REQUIRED_FIELD_ERROR_MESSAGE : false }}
+      rules={{ required: required ? REQUIRED_FIELD_ERROR_MESSAGE : required, validate: validate }}
       render={({ field, fieldState: { error } }) => (
         <Box sx={{ width: '100%' }}>
           <TextField
             value={field.value}
             label={label + (required ? '*' : '')}
             placeholder={`Select ${label}`}
+            type={type != 'phone' ? type : 'text'}
             error={error != null}
             onChange={(data) => field.onChange(data)}
             multiline={multiline}
             autoComplete="off"
             variant="outlined"
             size="small"
+            inputProps={type === 'phone' ? { mask: '(000) 000-0000' } : undefined}
+            InputProps={
+              type === 'phone'
+                ? {
+                    inputComponent: InputMask as any,
+                  }
+                : undefined
+            }
             fullWidth
           />
           {error && <FormHelperText error={true}>{error?.message}</FormHelperText>}
