9fe8f2ab12dbf4f71fd2441d8b89f0045edd19d4
bholyshevskyi <bholyshevskyi@masslight.com>
2025-08-28 11:15:59 +0000

    wip

--- apps/ehr/src/features/css-module/hooks/useImmunization.ts
+++ apps/ehr/src/features/css-module/hooks/useImmunization.ts
@@ -1,4 +1,5 @@
 import { useMutation, UseMutationResult, useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query';
+import { Identifier, Medication } from 'fhir/r4b';
 import { useApiClients } from 'src/hooks/useAppClients';
 import {
   AdministerImmunizationOrderRequest,
@@ -8,9 +9,17 @@ import {
   CreateUpdateImmunizationOrderResponse,
   GetImmunizationOrdersRequest,
   GetImmunizationOrdersResponse,
+  INVENTORY_VACCINE_TYPE_CODE,
+  MEDICATION_IDENTIFIER_NAME_SYSTEM,
 } from 'utils';
 
 const GET_IMMUNIZATION_ORDERS_KEY = 'get-immunization-orders';
+const GET_VACCINES_KEY = 'get-vaccines';
+
+export interface Vaccine {
+  id: string;
+  name: string;
+}
 
 export const useCreateUpdateImmunizationOrder = (): UseMutationResult<
   CreateUpdateImmunizationOrderResponse,
@@ -100,5 +109,36 @@ export const useCancelImmunizationOrder = (): UseMutationResult<void, Error, Can
         exact: false,
       });
     },
+  });
+};
+
+export const useGetVaccines = (): UseQueryResult<Vaccine[], Error> => {
+  const { oystehr } = useApiClients();
+  return useQuery({
+    queryKey: [GET_VACCINES_KEY],
+    queryFn: async () => {
+      if (!oystehr) throw new Error('oystehr not defined');
+      const data = await oystehr.fhir.search<Medication>({
+        resourceType: 'Medication',
+        params: [{ name: 'identifier', value: INVENTORY_VACCINE_TYPE_CODE }],
+      });
+      return data.unbundle().flatMap((medication) => {
+        const identifier = medication.identifier?.find(
+          (identifier: Identifier) => identifier.system === MEDICATION_IDENTIFIER_NAME_SYSTEM
+        );
+        if (identifier?.value && medication.id) {
+          return [
+            {
+              id: medication.id,
+              name: identifier?.value,
+            },
+          ];
+        }
+        return [];
+      });
+    },
+    enabled: oystehr != null,
+    retry: 2,
+    staleTime: 5 * 60 * 1000,
   });
 };
