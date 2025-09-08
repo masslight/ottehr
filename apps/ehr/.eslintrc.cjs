0925fece24a89a35df9e36e9f89a248d23bde7f9
imbenham <isaac.benham@gmail.com>
2025-09-08 15:16:43 -0700

    various ts config changes to fix build

--- apps/ehr/.eslintrc.cjs
+++ apps/ehr/.eslintrc.cjs
@@ -4,5 +4,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['tsconfig.json'],
   },
-  ignorePatterns: ['build', 'playwright-report'],
+  ignorePatterns: [
+    'build',
+    'playwright-report',
+    'playwright-report-login',
+    'setup-test-deps.js',
+    'validate-e2e-intake-user.js',
+    'auth.setup.js',
+  ],
 };
