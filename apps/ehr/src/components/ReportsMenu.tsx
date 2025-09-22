821bd2e330f5ab5afbbebca386482dd12cf2d0fb
saewitz <jonathansaewitz@gmail.com>
2025-09-22 12:17:08 -0400

    start data page reports issue #3869

---
+++ apps/ehr/src/components/ReportsMenu.tsx
@@ -0,0 +1,77 @@
+import { ArrowDropDown } from '@mui/icons-material';
+import { Menu, MenuItem } from '@mui/material';
+import { DateTime } from 'luxon';
+import React from 'react';
+import { useApiClients } from 'src/hooks/useAppClients';
+import { RoundedButton } from './RoundedButton';
+
+export function ReportsMenu(): JSX.Element {
+  const { oystehr } = useApiClients();
+  const [anchorElement, setAnchorElement] = React.useState<HTMLElement | null>(null);
+  const open = Boolean(anchorElement);
+
+  async function getUnsignedCharts(): Promise<void> {
+    if (!oystehr) {
+      console.log('oystehr client is undefined');
+      return;
+    }
+    const now = DateTime.now();
+    const yesterday = now.minus({ days: 1 }).endOf('day');
+    const ninetyDaysAgo = now.minus({ days: 90 }).startOf('day');
+    const encounters = await oystehr.fhir.search({
+      resourceType: 'Encounter',
+      params: [
+        {
+          name: 'status:not',
+          value: 'finished',
+        },
+        {
+          name: 'date',
+          value: `le${yesterday.toISODate()}`,
+        },
+        {
+          name: 'date',
+          value: `ge${ninetyDaysAgo.toISODate()}`,
+        },
+      ],
+    });
+    const element = document.createElement('a');
+    setAnchorElement(null);
+    element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(encounters)));
+    element.setAttribute('download', 'unsigned-charts.json');
+    element.click();
+  }
+
+  return (
+    <>
+      <RoundedButton
+        id="reports-menu-button"
+        variant="contained"
+        onClick={(event) => setAnchorElement(event.currentTarget)}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        aria-controls={open ? 'reports-menu' : undefined}
+        endIcon={<ArrowDropDown />}
+      >
+        Generate Report
+      </RoundedButton>
+      <Menu
+        id="reports-menu"
+        anchorEl={anchorElement}
+        open={open}
+        onClose={() => setAnchorElement(null)}
+        MenuListProps={{ 'aria-labelledby': 'reports-menu-button' }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={getUnsignedCharts}>Unsigned charts</MenuItem>
+      </Menu>
+    </>
+  );
+}
