7bda97b6f4604ecbd8f731ca21044a9f9b9e0614
Colin Welch <cwelch@masslight.com>
2025-09-22 17:02:25 -0400

    chore(deploy): More verbose debug comment

--- deploy/main.tf
+++ deploy/main.tf
@@ -40,7 +40,10 @@ provider "oystehr" {
 }
 
 locals {
-  # switch these to test infra deploys in local
+  # DEBUG: set to `1` to run non-local modules
+  # `1` is the magic number to run a module that checks this local variable.
+  # switch which line is commented out to run non-local modules like aws_infra
+  # while still in the `local` environment
   not_local_env_resource_count = var.environment == "local" ? 0 : 1
   # not_local_env_resource_count = 1
   aws_resource_count = var.aws_profile == null ? 0 : local.not_local_env_resource_count
