e4f9ea9901b97a9d4778ed411667e3bf2780ff87
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2025-10-01 19:24:03 +0300

    added payor id to dropdown list in patient info page

--- apps/ehr/src/components/patient/InsuranceContainer.tsx
+++ apps/ehr/src/components/patient/InsuranceContainer.tsx
@@ -271,7 +271,9 @@ export const InsuranceContainer: FC<InsuranceContainerProps> = ({
                 isOptionEqualToValue={(option, value) => {
                   return option?.id === value?.id;
                 }}
-                getOptionLabel={(option) => option.name || ''}
+                getOptionLabel={(option) =>
+                  option.payerId || option.name ? `${option?.payerId || 'N/A'} - ${option?.name || 'N/A'}` : ''
+                }
                 onChange={(_, newValue) => {
                   if (newValue) {
                     setValue(
