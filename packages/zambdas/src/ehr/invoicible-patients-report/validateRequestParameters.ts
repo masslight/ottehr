214dbd04a6d22eceef940d6638ee277584c99083
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2025-10-06 20:26:32 +0300

    made draft for new report endpoint

---
+++ packages/zambdas/src/ehr/invoicible-patients-report/validateRequestParameters.ts
@@ -0,0 +1,18 @@
+import { Secrets } from 'utils';
+import { ZambdaInput } from '../../shared';
+
+export function validateRequestParameters(input: ZambdaInput): {
+  secrets: Secrets;
+} {
+  console.log('validating request parameters');
+  if (!input.body) {
+    throw new Error('No request body provided');
+  }
+  if (!input.secrets) {
+    throw new Error('Input did not have any secrets');
+  }
+
+  return {
+    secrets: input.secrets,
+  };
+}
