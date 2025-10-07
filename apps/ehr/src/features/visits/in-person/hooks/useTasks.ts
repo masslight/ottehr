f2b4af379749ca6903bad24a67ef42d6475c4907
bholyshevskyi <bholyshevskyi@masslight.com>
2025-10-07 07:42:35 +0000

    stub

--- apps/ehr/src/features/visits/in-person/hooks/useTasks.ts
+++ apps/ehr/src/features/visits/in-person/hooks/useTasks.ts
@@ -1,10 +1,42 @@
 import { useMutation, UseMutationResult, useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query';
 import { Task as FhirTask } from 'fhir/r4b';
+import { DateTime } from 'luxon';
 import { useApiClients } from 'src/hooks/useAppClients';
-import { getCoding, TASK_INPUT_SYSTEM, TASK_TYPE_SYSTEM } from 'utils';
+import { codeableConcept, getCoding, TASK_CATEGORY_IDENTIFIER, TASK_INPUT_SYSTEM, TASK_TYPE_SYSTEM } from 'utils';
 
 const GET_TASKS_KEY = 'get-tasks';
 const GO_TO_LAB_TEST = 'Go to Lab Test';
+
+const STUB_TASK: FhirTask = {
+  resourceType: 'Task',
+  id: '12345',
+  intent: 'unknown',
+  status: 'requested',
+  groupIdentifier: {
+    system: TASK_CATEGORY_IDENTIFIER,
+    value: 'in-house-labs',
+  },
+  code: codeableConcept('collect-sample', TASK_TYPE_SYSTEM),
+  authoredOn: DateTime.now().toISO(),
+  input: [
+    {
+      type: codeableConcept('title', TASK_INPUT_SYSTEM),
+      valueString: 'Task title',
+    },
+    {
+      type: codeableConcept('subtitle', TASK_INPUT_SYSTEM),
+      valueString: 'Task subtitle',
+    },
+    {
+      type: codeableConcept('appointmentId', TASK_INPUT_SYSTEM),
+      valueString: 'stubAppointmentId',
+    },
+    {
+      type: codeableConcept('orderId', TASK_INPUT_SYSTEM),
+      valueString: 'stubOrderId',
+    },
+  ],
+};
 
 export interface Task {
   id: string;
@@ -43,18 +75,18 @@ export const useGetTasks = (): UseQueryResult<Task[], Error> => {
     queryKey: [GET_TASKS_KEY],
     queryFn: async () => {
       if (!oystehr) throw new Error('oystehr not defined');
-      const tasks = (
+      /*const tasks = (
         await oystehr.fhir.search<FhirTask>({
           resourceType: 'Task',
           params: [
             {
               name: '_tag',
-              value: 'todo',
+              value: 'task',
             },
-            // todo filter by status ?
           ],
         })
-      ).unbundle();
+      ).unbundle();*/
+      const tasks = [STUB_TASK];
       return tasks.map(fhirTaskToTask);
     },
     enabled: oystehr != null,
