f4c36630a73499a788ce6a70828d706c70072761
Alex Willingham <awillingham@masslight.com>
2025-10-17 10:52:54 -0400

    add a couple more chart data integration tests

--- packages/zambdas/test/integration/get-chart-data.test.ts
+++ packages/zambdas/test/integration/get-chart-data.test.ts
@@ -15,7 +15,13 @@ import {
   Slot,
 } from 'fhir/r4b';
 import { DateTime } from 'luxon';
-import { cleanAppointmentGraph, GetChartDataRequest, GetChartDataResponse } from 'utils';
+import {
+  cleanAppointmentGraph,
+  GetChartDataRequest,
+  GetChartDataResponse,
+  SaveChartDataRequest,
+  SaveChartDataResponse,
+} from 'utils';
 import { inject } from 'vitest';
 // Alex boldly imported this file from another package. R
 import fastSeedData from '../../../../apps/ehr/tests/e2e-utils/seed-data/seed-ehr-appointment-data.json' assert { type: 'json' };
@@ -191,11 +197,10 @@ describe('get-chart-data integration tests', () => {
     if (metaTagCoding?.tag?.[0]) {
       await cleanAppointmentGraph(metaTagCoding.tag[0], oystehr);
     }
-    console.log('Cleaned up test resources');
   };
 
   describe('get-chart-data happy paths', () => {
-    it('should get chart data with no params -- success', async () => {
+    it('should get chart data with no params on base chart-- success', async () => {
       const getChartDataInput: GetChartDataRequest = {
         encounterId: baseResources.encounter.id!,
       };
@@ -211,23 +216,136 @@ describe('get-chart-data integration tests', () => {
         console.error('Error executing zambda:', error);
         getChartDataOutput = error as Error;
       }
-      console.log('Get Chart Data Output:', JSON.stringify(getChartDataOutput, null, 2));
-      expect(getChartDataOutput).toBeDefined();
-      expect(getChartDataOutput).toHaveProperty('patientId');
-      expect(getChartDataOutput).toHaveProperty('conditions');
-      expect(getChartDataOutput).toHaveProperty('medications');
-      expect(getChartDataOutput).toHaveProperty('allergies');
-      expect(getChartDataOutput).toHaveProperty('surgicalHistory');
-      expect(getChartDataOutput).toHaveProperty('examObservations');
-      expect(getChartDataOutput).toHaveProperty('cptCodes');
-      expect(getChartDataOutput).toHaveProperty('instructions');
-      expect(getChartDataOutput).toHaveProperty('diagnosis');
-      expect(getChartDataOutput).toHaveProperty('schoolWorkNotes');
-      expect(getChartDataOutput).toHaveProperty('observations');
-      expect(getChartDataOutput).toHaveProperty('practitioners');
-      expect(getChartDataOutput).toHaveProperty('aiPotentialDiagnosis');
-      expect(getChartDataOutput).toHaveProperty('aiChat');
-      expect(getChartDataOutput).toHaveProperty('patientInfoConfirmed');
+      expect(getChartDataOutput instanceof Error).toBe(false);
+      const typedGetChartDataOutput = getChartDataOutput as GetChartDataResponse;
+      expect(typedGetChartDataOutput).toBeDefined();
+      expect(typedGetChartDataOutput).toHaveProperty('patientId');
+      expect(typedGetChartDataOutput.patientId).toEqual(baseResources.patient.id);
+      expect(typedGetChartDataOutput).toHaveProperty('conditions');
+      expect(typedGetChartDataOutput.conditions).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.conditions?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('medications');
+      expect(typedGetChartDataOutput.medications).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.medications?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('allergies');
+      expect(typedGetChartDataOutput.allergies).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.allergies?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('surgicalHistory');
+      expect(typedGetChartDataOutput.surgicalHistory).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.surgicalHistory?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('examObservations');
+      expect(typedGetChartDataOutput.examObservations).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.examObservations?.length).toBeGreaterThan(1);
+      expect(typedGetChartDataOutput).toHaveProperty('cptCodes');
+      expect(typedGetChartDataOutput.cptCodes).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.cptCodes?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('instructions');
+      expect(typedGetChartDataOutput.instructions).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.instructions?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('diagnosis');
+      expect(typedGetChartDataOutput.diagnosis).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.diagnosis?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('schoolWorkNotes');
+      expect(typedGetChartDataOutput.schoolWorkNotes).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.schoolWorkNotes?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('observations');
+      expect(typedGetChartDataOutput.observations).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.observations?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('practitioners');
+      expect(typedGetChartDataOutput.practitioners).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.practitioners?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('aiPotentialDiagnosis');
+      expect(typedGetChartDataOutput.aiPotentialDiagnosis).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.aiPotentialDiagnosis?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('aiChat');
+      expect(typedGetChartDataOutput.aiChat).toBeInstanceOf(Object);
+      expect(typedGetChartDataOutput.aiChat?.documents).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.aiChat?.documents?.length).toEqual(0);
+      expect(typedGetChartDataOutput.aiChat?.providers).toBeInstanceOf(Array);
+      expect(typedGetChartDataOutput.aiChat?.providers?.length).toEqual(0);
+      expect(typedGetChartDataOutput).toHaveProperty('patientInfoConfirmed');
+      expect(typedGetChartDataOutput.patientInfoConfirmed).toEqual({
+        value: true,
+      });
     });
+  });
+
+  it('should validate shape of examObservations -- success', async () => {
+    const getChartDataInput: GetChartDataRequest = {
+      encounterId: baseResources.encounter.id!,
+    };
+    let getChartDataOutput: any;
+    try {
+      getChartDataOutput = (
+        await oystehrLocalZambdas.zambda.execute({
+          id: 'GET-CHART-DATA',
+          ...getChartDataInput,
+        })
+      ).output as GetChartDataResponse;
+    } catch (error) {
+      console.error('Error executing zambda:', error);
+      getChartDataOutput = error as Error;
+    }
+    expect(getChartDataOutput instanceof Error).toBe(false);
+    const typedGetChartDataOutput = getChartDataOutput as GetChartDataResponse;
+    expect(typedGetChartDataOutput).toHaveProperty('examObservations');
+    expect(typedGetChartDataOutput.examObservations).toBeInstanceOf(Array);
+    expect(typedGetChartDataOutput.examObservations?.[0]).toMatchObject({
+      resourceId: expect.any(String),
+      field: expect.any(String),
+      value: expect.any(Boolean),
+    });
+  });
+
+  it('should validate shape of conditions -- success', async () => {
+    const conditionDTO = {
+      code: 'H54.8',
+      display: 'Legal blindness, as defined in USA',
+      current: true,
+    };
+    const saveChartInput: SaveChartDataRequest = {
+      encounterId: baseResources.encounter.id!,
+      conditions: [conditionDTO],
+    };
+    let saveChartOutput: any;
+    try {
+      saveChartOutput = (
+        await oystehrLocalZambdas.zambda.execute({
+          id: 'SAVE-CHART-DATA',
+          ...saveChartInput,
+        })
+      ).output as SaveChartDataResponse;
+    } catch (error) {
+      console.error('Error executing zambda:', error);
+      saveChartOutput = error as Error;
+    }
+    expect(saveChartOutput instanceof Error).toBe(false);
+    const typedSaveChartOutput = saveChartOutput as SaveChartDataResponse;
+    const newCondition = typedSaveChartOutput.chartData.conditions?.[0];
+    expect(newCondition).toMatchObject({
+      resourceId: expect.any(String),
+      ...conditionDTO,
+    });
+
+    const getChartDataInput: GetChartDataRequest = {
+      encounterId: baseResources.encounter.id!,
+    };
+    let getChartDataOutput: any;
+    try {
+      getChartDataOutput = (
+        await oystehrLocalZambdas.zambda.execute({
+          id: 'GET-CHART-DATA',
+          ...getChartDataInput,
+        })
+      ).output as GetChartDataResponse;
+    } catch (error) {
+      console.error('Error executing zambda:', error);
+      getChartDataOutput = error as Error;
+    }
+    expect(getChartDataOutput instanceof Error).toBe(false);
+    const typedGetChartDataOutput = getChartDataOutput as GetChartDataResponse;
+    expect(typedGetChartDataOutput).toHaveProperty('conditions');
+    expect(typedGetChartDataOutput.conditions).toBeInstanceOf(Array);
+    expect(typedGetChartDataOutput.conditions?.[0]).toEqual(newCondition);
   });
 });
