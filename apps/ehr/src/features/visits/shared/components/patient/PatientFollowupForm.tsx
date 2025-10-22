e189ed61fc11ad5289069c32ee4655951861c9e1
bxr1nG <dvoshchuk@masslight.com>
2025-10-22 17:18:29 +0300

    OTR-1085: Disable ability to create follow-ups in past

--- apps/ehr/src/features/visits/shared/components/patient/PatientFollowupForm.tsx
+++ apps/ehr/src/features/visits/shared/components/patient/PatientFollowupForm.tsx
@@ -266,6 +266,26 @@ export default function PatientFollowupForm({ patient, followupDetails }: Patien
     }
   };
 
+  const handleDateChange = (date: DateTime): void => {
+    if (!date || !formData.followupTime) return;
+
+    updateFormData('followupDate', date);
+    const isToday = date.hasSame(DateTime.now(), 'day');
+
+    const combinedDateTime = date.set({
+      hour: formData.followupTime.hour,
+      minute: formData.followupTime.minute,
+      second: formData.followupTime.second,
+    });
+
+    if (isToday && combinedDateTime < DateTime.now()) {
+      const currentTime = DateTime.now();
+      if (!formData.followupTime.hasSame(currentTime, 'minute')) {
+        updateFormData('followupTime', currentTime);
+      }
+    }
+  };
+
   return (
     <Paper elevation={3} sx={{ p: 3, mb: 3 }}>
       <form onSubmit={handleFormSubmit}>
@@ -373,10 +393,11 @@ export default function PatientFollowupForm({ patient, followupDetails }: Patien
           <Grid item xs={5}>
             <LocalizationProvider dateAdapter={AdapterLuxon}>
               <DatePicker
-                onChange={(val) => val && updateFormData('followupDate', val)}
+                onChange={(val) => val && handleDateChange(val)}
                 label="Follow-up date"
                 format="MM/dd/yyyy"
                 value={formData.followupDate}
+                minDate={DateTime.now().startOf('day')}
                 slotProps={{
                   textField: {
                     id: 'followup-date',
@@ -397,6 +418,7 @@ export default function PatientFollowupForm({ patient, followupDetails }: Patien
                 onChange={(val) => val && updateFormData('followupTime', val)}
                 value={formData.followupTime}
                 label="Follow-up time *"
+                minTime={formData.followupDate.hasSame(DateTime.now(), 'day') ? DateTime.now() : undefined}
                 slotProps={{
                   textField: {
                     fullWidth: true,
