30da169d7c99fbeb6b2c1f54af9f98fb431d51ef
Uladzislau Molastau <molostov.inc@gmail.com>
2025-10-23 22:14:36 +0200

    fix telemed exam page test

--- apps/ehr/tests/e2e/specs/telemed/examTab.spec.ts
+++ apps/ehr/tests/e2e/specs/telemed/examTab.spec.ts
@@ -23,12 +23,8 @@ test.describe('Persistence tests', async () => {
   test.beforeAll(async ({ browser }) => {
     const context = await browser.newContext();
     page = await context.newPage();
-    if (process.env.INTEGRATION_TEST === 'true') {
-      await resourceHandler.setResourcesFast();
-    } else {
-      await resourceHandler.setResources();
-      await resourceHandler.waitTillAppointmentPreprocessed(resourceHandler.appointment.id!);
-    }
+    await resourceHandler.setResources();
+    await resourceHandler.waitTillAppointmentPreprocessed(resourceHandler.appointment.id!);
 
     await page.goto(`telemed/appointments/${resourceHandler.appointment.id}`);
     await assignAppointmentIfNotYetAssignedToMeAndVerifyPreVideo(page, { forceWaitForAssignButton: true });
