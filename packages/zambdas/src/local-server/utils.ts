45b46cb4f9ccc4a1702733f6e9059248d0e6e100
Colin <colin.t.welch@gmail.com>
2025-10-26 02:45:56 -0400

    tweak(config): Further changes for IaC (#4709) (#4710)
    * tweak(config): Further changes for IaC
    
    * fix: Add iac scripts to top level
    
    * fix: Add iac scripts to turbo
    
    * tweak(local): Block requests on populating secrets
    
    * fix(local): Default to legacyValue instead of tf

--- packages/zambdas/src/local-server/utils.ts
+++ packages/zambdas/src/local-server/utils.ts
@@ -56,6 +56,7 @@ function parseArgs(args: string[]): Record<string, string> {
 const secrets: Record<string, string> = {};
 
 async function populateSecrets(): Promise<void> {
+  console.log('Populating secrets');
   const cliParams = parseArgs(process.argv.slice(2));
   const pathToEnvFile = cliParams['env'];
   const useIac = cliParams['iac'];
@@ -110,6 +111,7 @@ async function populateSecrets(): Promise<void> {
       secrets[key] = value;
     }
   });
+  console.log('Populated secrets' /*, JSON.stringify(secrets)*/);
 }
 
 export async function replaceSecretValue<T>(
@@ -122,38 +124,38 @@ export async function replaceSecretValue<T>(
   const refMatches = [...result.matchAll(REF_REGEX)];
   if (refMatches.length) {
     console.log(`Found ${refMatches.length} terraform references in secret ${secret.name}`);
-    if (useIac !== 'true') {
-      console.log(`Warning: not using IaC but reference found in secret ${secret.name}`);
-      if ('legacyValue' in secret && secret.legacyValue != null) {
-        const legacyValue = secret.legacyValue as string;
-        console.log(`Using legacy value for secret ${secret.name}: ${legacyValue}`);
-        result = schema.replaceVariableWithValue(legacyValue);
-      } else {
-        console.log(`Warning: no legacy value found for secret ${secret.name}`);
+    if ('legacyValue' in secret && secret.legacyValue != null) {
+      const legacyValue = secret.legacyValue as string;
+      console.log(`Using legacy value for secret ${secret.name}: ${legacyValue}`);
+      result = schema.replaceVariableWithValue(legacyValue);
+    } else {
+      console.log(`Warning: no legacy value found for secret ${secret.name}`);
+      if (useIac !== 'true') {
+        console.log(`Skipping terraform resolution for secret ${secret.name} because iac flag not set`);
+        return result;
       }
-      return result;
-    }
-    for (const match of refMatches) {
-      const [fullMatch, resourceType, resourceName, fieldName] = match;
-      const tfRef = schema.getTerraformResourceReference(
-        ottehrSpec as T,
-        resourceType as keyof T,
-        resourceName,
-        fieldName
-      );
-      if (tfRef) {
-        console.log(`Resolving terraform reference for ${fullMatch}: ${tfRef}`);
-        const tfOutputName = schema.getTerraformResourceOutputName(fullMatch, 'oystehr');
-        const opts: Options = {
-          cwd: resolve(__dirname, '../../../../deploy'),
-          input: `nonsensitive(${tfOutputName})`,
-        };
-        const tfConsoleRead = await $(opts)`terraform console`;
-        console.log(`Terraform console read for ${fullMatch}: ${tfConsoleRead.stdout}`);
-        const tfValue = tfConsoleRead.stdout;
-        // console value will either be the actual value or 'tostring(null)'
-        if (tfValue && typeof tfValue === 'string' && tfValue !== 'tostring(null)') {
-          result = result.replace(fullMatch, tfValue.slice(1, -1));
+      for (const match of refMatches) {
+        const [fullMatch, resourceType, resourceName, fieldName] = match;
+        const tfRef = schema.getTerraformResourceReference(
+          ottehrSpec as T,
+          resourceType as keyof T,
+          resourceName,
+          fieldName
+        );
+        if (tfRef) {
+          console.log(`Resolving terraform reference for ${fullMatch}: ${tfRef}`);
+          const tfOutputName = schema.getTerraformResourceOutputName(fullMatch, 'oystehr');
+          const opts: Options = {
+            cwd: resolve(__dirname, '../../../../deploy'),
+            input: `nonsensitive(${tfOutputName})`,
+          };
+          const tfConsoleRead = await $(opts)`terraform console`;
+          console.log(`Terraform console read for ${fullMatch}: ${tfConsoleRead.stdout}`);
+          const tfValue = tfConsoleRead.stdout;
+          // console value will either be the actual value or 'tostring(null)'
+          if (tfValue && typeof tfValue === 'string' && tfValue !== 'tostring(null)') {
+            result = result.replace(fullMatch, tfValue.slice(1, -1));
+          }
         }
       }
     }
@@ -170,13 +172,11 @@ const singleValueHeaders = (input: IncomingHttpHeaders): APIGatewayProxyEventHea
   return headers;
 };
 
+const populateSecretsPromise = populateSecrets();
+
 async function buildLambdaInput(req: Request): Promise<ZambdaInput> {
   console.log('build lambda body,', JSON.stringify(req.body));
-  if (!Object.keys(secrets).length) {
-    console.log('Populating secrets');
-    await populateSecrets();
-    console.log('Populated secrets' /*, JSON.stringify(secrets)*/);
-  }
+  await populateSecretsPromise;
   return {
     body: !_.isEmpty(req.body) ? req.body : null,
     headers: singleValueHeaders(req.headers),
