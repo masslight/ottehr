fe523ec635fb09c8608bf11ed3a091be27f3c033
Uladzislau Molastau <molostov.inc@gmail.com>
2025-10-28 20:55:14 +0100

    disable patient info fields on saving and data loading

--- apps/ehr/src/features/visits/shared/components/patient/PharmacyContainer.tsx
+++ apps/ehr/src/features/visits/shared/components/patient/PharmacyContainer.tsx
@@ -6,16 +6,16 @@ import { FormFields } from 'src/constants';
 
 const fields = FormFields.preferredPharmacy;
 
-export const PharmacyContainer: FC = () => {
+export const PharmacyContainer: FC<{ isLoading: boolean }> = ({ isLoading }) => {
   const { control } = useFormContext();
 
   return (
     <Section title="Preferred pharmacy">
       <Row label={fields.name.label} inputId={fields.name.key}>
-        <FormTextField name={fields.name.key} control={control} id={fields.name.key} />
+        <FormTextField name={fields.name.key} control={control} id={fields.name.key} disabled={isLoading} />
       </Row>
       <Row label={fields.address.label} inputId={fields.address.key}>
-        <FormTextField name={fields.address.key} control={control} id={fields.address.key} />
+        <FormTextField name={fields.address.key} control={control} id={fields.address.key} disabled={isLoading} />
       </Row>
     </Section>
   );
