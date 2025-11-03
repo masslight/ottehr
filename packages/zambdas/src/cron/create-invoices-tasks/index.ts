d74295aa1aee1aac80ac64664750cf31a5a078ba
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2025-11-03 16:23:29 +0200

    wip

---
+++ packages/zambdas/src/cron/create-invoices-tasks/index.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { getSecret, SecretsKeys } from 'utils';
+import { checkOrCreateM2MClientToken, topLevelCatch, wrapHandler, ZambdaInput } from '../../shared';
+
+let m2mToken: string;
+
+const ZAMBDA_NAME = 'send-invoice-to-patient';
+
+export const index = wrapHandler(ZAMBDA_NAME, async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+  try {
+    const { secrets } = input;
+    m2mToken = await checkOrCreateM2MClientToken(m2mToken, secrets);
+    // const oystehr = createOystehrClient(m2mToken, secrets);
+    // const candid = createCandidApiClient(secrets);
+    // const stripe = getStripeClient(secrets);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Invoice created and sent successfully' }),
+    };
+  } catch (error: unknown) {
+    const ENVIRONMENT = getSecret(SecretsKeys.ENVIRONMENT, input.secrets);
+    await topLevelCatch(ZAMBDA_NAME, error, ENVIRONMENT);
+    console.log('Error occurred:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal server error' }),
+    };
+  }
+});
+
+// async function getInvoiceableClaims() {}
