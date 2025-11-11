f9d7c4040660d261d34c2cac2ac11a6c36aa745b
Uladzislau Molastau <molostov.inc@gmail.com>
2025-11-11 14:32:07 +0100

    improve virtualLocationsPage tests

--- apps/ehr/tests/e2e/specs/virtualLocationsPage.spec.ts
+++ apps/ehr/tests/e2e/specs/virtualLocationsPage.spec.ts
@@ -1,75 +1,74 @@
-import { test } from '@playwright/test';
-import { expectStateDetailsPage } from '../page/VirtualLocationDetailsPage';
-import { expectVirtualLocationsPage, openVirtualLocationsPage } from '../page/VirtualLocationsPage';
+import { BrowserContext, Page, test } from '@playwright/test';
+import { expectStateDetailsPage, VirtualLocationDetailsPage } from '../page/VirtualLocationDetailsPage';
+import {
+  expectVirtualLocationsPage,
+  openVirtualLocationsPage,
+  VirtualLocationsPage,
+} from '../page/VirtualLocationsPage';
 
-test.beforeEach(async ({ page }) => {
-  await page.waitForTimeout(2000); // TODO what is this actually waiting for? Replace it with something faster.
+let page: Page;
+let context: BrowserContext;
+
+test.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  page = await context.newPage();
   await page.goto('/telemed-admin/virtual-locations');
 });
 
-test('Open "Virtual Locations page", enter state abbreviation,  correct search result is displayed', async ({
-  page,
-}) => {
-  const virtualLocationsPage = await expectVirtualLocationsPage(page);
-  const state = await virtualLocationsPage.getFirstLocation();
-  await virtualLocationsPage.searchStates(state.name);
-  await virtualLocationsPage.verifyLocationPresent(state.id);
+test.afterAll(async () => {
+  await page.close();
+  await context.close();
+});
+
+let firstState: { id: string; name: string };
+let virtualLocationsPage: VirtualLocationsPage;
+let stateDetailsPage: VirtualLocationDetailsPage;
+
+test.describe.configure({ mode: 'serial' });
+
+test('Open "Virtual Locations page", enter state abbreviation,  correct search result is displayed', async () => {
+  virtualLocationsPage = await expectVirtualLocationsPage(page);
+  firstState = await virtualLocationsPage.getFirstLocation();
+  await virtualLocationsPage.searchStates(firstState.name);
+  await virtualLocationsPage.verifyLocationPresent(firstState.id);
   await virtualLocationsPage.verifyPaginationState('1–1 of 1');
 
   await virtualLocationsPage.searchStates('AA');
   await virtualLocationsPage.verifyPaginationState('0–0 of 0');
 });
 
-test('Open "Virtual Locations page", click on state, virtual locations details page is opened', async ({ page }) => {
-  const virtualLocationsPage = await expectVirtualLocationsPage(page);
-  const state = await virtualLocationsPage.getFirstLocation();
-  await virtualLocationsPage.clickLocation(state.id);
-  await expectStateDetailsPage(state.id, state.name, page);
+test('Сlick on state, virtual locations details page is opened', async () => {
+  await virtualLocationsPage.searchStates(firstState.name);
+  await virtualLocationsPage.clickLocation(firstState.id);
+  stateDetailsPage = await expectStateDetailsPage(firstState.id, firstState.name, page);
 });
 
-test('Open "Virtual Locations details page", click cancel button,  virtual locations page is opened', async ({
-  page,
-}) => {
-  const virtualLocationsPage = await expectVirtualLocationsPage(page);
-  const location = await virtualLocationsPage.getFirstLocation();
-  await virtualLocationsPage.clickLocation(location.id);
-  const stateDetailsPage = await expectStateDetailsPage(location.id, location.name, page);
+test('Сlick cancel button, virtual locations page is opened', async () => {
   await stateDetailsPage.clickCancelButton();
   await expectVirtualLocationsPage(page);
 });
 
-test('Open "Virtual Locations details page", check title and state name field,  verify state name is correct in title', async ({
-  page,
-}) => {
-  const virtualLocationsPage = await expectVirtualLocationsPage(page);
-  const location = await virtualLocationsPage.getFirstLocation();
-  await virtualLocationsPage.clickLocation(location.id);
-  const stateDetailsPage = await expectStateDetailsPage(location.id, location.name, page);
-  await stateDetailsPage.verifyLocationNameTitle(location.name);
-  await stateDetailsPage.verifyLocationNameField(location.name);
+test('Open "Virtual Locations details page", check title and state name field, verify state name is correct in title', async () => {
+  await virtualLocationsPage.clickLocation(firstState.id);
+  const stateDetailsPage = await expectStateDetailsPage(firstState.id, firstState.name, page);
+  await stateDetailsPage.verifyLocationNameTitle(firstState.name);
+  await stateDetailsPage.verifyLocationNameField(firstState.name);
 });
 
-test('Open "Virtual Locations details page", toggle "Active" and save changes, verify changes are saved', async ({
-  page,
-}) => {
-  let virtualLocationsPage = await expectVirtualLocationsPage(page);
-  const location = await virtualLocationsPage.getFirstLocation();
-  await virtualLocationsPage.clickLocation(location.id);
-  const stateDetailsPage = await expectStateDetailsPage(location.id, location.name, page);
-
+test('Open "Virtual Locations details page", toggle "Active" and save changes, verify changes are saved', async () => {
   if (await stateDetailsPage.isToggleOn()) {
     await stateDetailsPage.setToggleOff();
     await stateDetailsPage.clickSaveChangesButton();
     await stateDetailsPage.reloadStateDetailsPage();
     await stateDetailsPage.verifyToggleOff();
     virtualLocationsPage = await openVirtualLocationsPage(page);
-    await virtualLocationsPage.verifyActive(location.id, false);
+    await virtualLocationsPage.verifyActive(firstState.id, false);
   } else {
     await stateDetailsPage.setToggleOn();
     await stateDetailsPage.clickSaveChangesButton();
     await stateDetailsPage.reloadStateDetailsPage();
     await stateDetailsPage.verifyToggleOn();
     virtualLocationsPage = await openVirtualLocationsPage(page);
-    await virtualLocationsPage.verifyActive(location.id, true);
+    await virtualLocationsPage.verifyActive(firstState.id, true);
   }
 });
