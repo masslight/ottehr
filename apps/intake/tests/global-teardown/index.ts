33edd86352371083036f546b1f1202b4756e7470
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-11-19 09:02:08 -0500

    fix typo

--- apps/intake/tests/global-teardown/index.ts
+++ apps/intake/tests/global-teardown/index.ts
@@ -9,7 +9,7 @@ import {
 } from 'utils';
 const globalTeardown = async (): Promise<void> => {
   // Global setup logic here
-  console.log('Running global teardown for EHR tests');
+  console.log('Running global teardown for intake tests');
   const playwrightSuiteId = process.env.PLAYWRIGHT_SUITE_ID;
   const FHIR_API = getSecret(SecretsKeys.FHIR_API, process.env as Secrets).replace(/\/r4/g, '');
   const PROJECT_API = getSecret(SecretsKeys.PROJECT_API, process.env as Secrets);
