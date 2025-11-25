3c8f21c2d348d2b0875a58addd51fef29325873f
imbenham <isaac.benham@gmail.com>
2025-11-24 16:59:14 -0800

    rollback vite config stuff

--- apps/ehr/vitest.config.ts
+++ apps/ehr/vitest.config.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(__dirname, 'env/.env.local') });
 
 export default defineConfig({
   test: {
-    globals: false,
+    globals: true,
     exclude: ['**/*.spec.ts', '**/*.test.tsx'],
   },
   plugins: [tsconfigPaths()],
--- packages/zambdas/vitest.config.ts
+++ packages/zambdas/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: false,
+    globals: true,
     environment: 'node',
     silent: true,
     testTimeout: 20000,
