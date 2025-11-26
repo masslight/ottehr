39da4d7bb85a6fc91765cdbbe55f7bbe481672c7
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-11-26 09:51:35 -0500

    update in-person setup tests to not reuse page context

--- apps/intake/tests/specs/0_prep/in-person.spec.ts
+++ apps/intake/tests/specs/0_prep/in-person.spec.ts
@@ -1,4 +1,4 @@
-import { BrowserContext, Page, test } from '@playwright/test';
+import { Page, test } from '@playwright/test';
 import * as fs from 'fs';
 import * as path from 'path';
 import { chooseJson, CreateAppointmentResponse, GetSlotDetailsResponse } from 'utils';
@@ -7,40 +7,7 @@ import { PrebookInPersonFlow } from '../../utils/in-person/PrebookInPersonFlow';
 import { Locators } from '../../utils/locators';
 import { Paperwork } from '../../utils/Paperwork';
 
-let page: Page;
-let context: BrowserContext;
-let flowClass: PrebookInPersonFlow;
-let paperwork: Paperwork;
-let locator: Locators;
-let fillingInfo: FillingInfo;
-let slotDetails: GetSlotDetailsResponse;
 const appointmentIds: string[] = [];
-
-test.beforeAll(
-  'setup patient with in-person with card/self-pay and insurance/not-self appointments',
-  async ({ browser }) => {
-    context = await browser.newContext();
-    page = await context.newPage();
-    page.on('response', async (response) => {
-      if (response.url().includes('/create-appointment/')) {
-        const { appointmentId } = chooseJson(await response.json()) as CreateAppointmentResponse;
-        if (!appointmentIds.includes(appointmentId)) {
-          appointmentIds.push(appointmentId);
-        }
-      }
-    });
-    page.on('response', async (response) => {
-      if (response.url().includes('/get-slot-details/')) {
-        const details = chooseJson(await response.json()) as GetSlotDetailsResponse;
-        slotDetails = details;
-      }
-    });
-    flowClass = new PrebookInPersonFlow(page);
-    paperwork = new Paperwork(page);
-    locator = new Locators(page);
-    fillingInfo = new FillingInfo(page);
-  }
-);
 
 function writeTestData(filename: string, data: unknown): void {
   const testDataPath = 'test-data';
@@ -50,14 +17,24 @@ function writeTestData(filename: string, data: unknown): void {
   fs.writeFileSync(path.join(testDataPath, filename), JSON.stringify(data, null, 2));
 }
 
-async function bookAppointmentForExistingPatient(bookingData: {
-  firstName: string;
-  lastName: string;
-  dateOfBirth: string;
-}): Promise<{
+async function bookAppointmentForExistingPatient(
+  bookingData: {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+  },
+  playwrightContext: {
+    page: Page;
+    flowClass: PrebookInPersonFlow;
+    paperwork: Paperwork;
+    locator: Locators;
+    fillingInfo: FillingInfo;
+  }
+): Promise<{
   slot: string | undefined;
   location: string | null;
 }> {
+  const { page, flowClass, paperwork, locator, fillingInfo } = playwrightContext;
   await page.goto('/home');
   await locator.scheduleInPersonVisitButton.click();
   const { selectedSlot, location } = await flowClass.additionalStepsForPrebook();
@@ -80,6 +57,30 @@ async function bookAppointmentForExistingPatient(bookingData: {
 
 test.describe.parallel('In-Person: Create test patients and appointments', () => {
   test('Create patient with self-pay and card payment appointment', async ({ page }) => {
+    const { slotDetails, flowClass, paperwork, locator, fillingInfo } =
+      await test.step('Set up playwright', async () => {
+        let slotDetails = {} as GetSlotDetailsResponse;
+
+        page.on('response', async (response) => {
+          if (response.url().includes('/create-appointment/')) {
+            const { appointmentId } = chooseJson(await response.json()) as CreateAppointmentResponse;
+            if (!appointmentIds.includes(appointmentId)) {
+              appointmentIds.push(appointmentId);
+            }
+          }
+        });
+        page.on('response', async (response) => {
+          if (response.url().includes('/get-slot-details/')) {
+            slotDetails = chooseJson(await response.json()) as GetSlotDetailsResponse;
+          }
+        });
+        const flowClass = new PrebookInPersonFlow(page);
+        const paperwork = new Paperwork(page);
+        const locator = new Locators(page);
+        const fillingInfo = new FillingInfo(page);
+        return { slotDetails, flowClass, paperwork, locator, fillingInfo };
+      });
+
     const { bookingData, stateValue } = await test.step('Book first appointment', async () => {
       const bookingData = await flowClass.startVisit();
       await page.goto(bookingData.bookingURL);
@@ -90,7 +91,13 @@ test.describe.parallel('In-Person: Create test patients and appointments', () =>
     });
 
     const { slot, location } = await test.step('Book second appointment without filling paperwork', async () => {
-      return await bookAppointmentForExistingPatient(bookingData);
+      return await bookAppointmentForExistingPatient(bookingData, {
+        page,
+        flowClass,
+        paperwork,
+        locator,
+        fillingInfo,
+      });
     });
 
     await test.step('Save test data', async () => {
@@ -112,6 +119,30 @@ test.describe.parallel('In-Person: Create test patients and appointments', () =>
   });
 
   test('Create patient without self-pay with insurance payment appointment', async ({ page }) => {
+    const { slotDetails, flowClass, paperwork, locator, fillingInfo } =
+      await test.step('Set up playwright', async () => {
+        let slotDetails = {} as GetSlotDetailsResponse;
+
+        page.on('response', async (response) => {
+          if (response.url().includes('/create-appointment/')) {
+            const { appointmentId } = chooseJson(await response.json()) as CreateAppointmentResponse;
+            if (!appointmentIds.includes(appointmentId)) {
+              appointmentIds.push(appointmentId);
+            }
+          }
+        });
+        page.on('response', async (response) => {
+          if (response.url().includes('/get-slot-details/')) {
+            slotDetails = chooseJson(await response.json()) as GetSlotDetailsResponse;
+          }
+        });
+        const flowClass = new PrebookInPersonFlow(page);
+        const paperwork = new Paperwork(page);
+        const locator = new Locators(page);
+        const fillingInfo = new FillingInfo(page);
+        return { slotDetails, flowClass, paperwork, locator, fillingInfo };
+      });
+
     const {
       bookingData,
       stateValue,
@@ -139,7 +170,13 @@ test.describe.parallel('In-Person: Create test patients and appointments', () =>
     });
 
     const { slot, location } = await test.step('Book second appointment without filling paperwork', async () => {
-      return await bookAppointmentForExistingPatient(bookingData);
+      return await bookAppointmentForExistingPatient(bookingData, {
+        page,
+        flowClass,
+        paperwork,
+        locator,
+        fillingInfo,
+      });
     });
 
     await test.step('Save test data', async () => {
@@ -165,7 +202,23 @@ test.describe.parallel('In-Person: Create test patients and appointments', () =>
     });
   });
 
-  test('Create patient without filling in paperwork', async () => {
+  test('Create patient without filling in paperwork', async ({ browser, page }) => {
+    const { flowClass, paperwork } = await test.step('Set up playwright', async () => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      page.on('response', async (response) => {
+        if (response.url().includes('/create-appointment/')) {
+          const { appointmentId } = chooseJson(await response.json()) as CreateAppointmentResponse;
+          if (!appointmentIds.includes(appointmentId)) {
+            appointmentIds.push(appointmentId);
+          }
+        }
+      });
+      const flowClass = new PrebookInPersonFlow(page);
+      const paperwork = new Paperwork(page);
+      return { flowClass, paperwork };
+    });
+
     const { bookingData } = await test.step('Create patient', async () => {
       const bookingData = await flowClass.startVisit();
       await page.goto(bookingData.bookingURL);
@@ -187,7 +240,13 @@ test.describe.parallel('In-Person: Create test patients and appointments', () =>
     });
   });
 
-  test('Create patient without appointments', async () => {
+  test('Create patient without appointments', async ({ browser }) => {
+    const flowClass = await test.step('Set up playwright', async () => {
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      return new PrebookInPersonFlow(page);
+    });
+
     const { bookingData } = await test.step('Create patient', async () => {
       const bookingData = await flowClass.startVisit();
       return { bookingData };
