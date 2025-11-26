9ae5cbb4da8700883f6c32a8965168ff1c9b6df2
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-11-26 13:01:18 -0500

    organize and rename reservation screen tests

--- apps/intake/tests/specs/in-person/ReservationScreen.spec.ts
+++ apps/intake/tests/specs/in-person/ReservationScreen.spec.ts
@@ -25,58 +25,37 @@ test.beforeAll(async ({ browser }) => {
   }
 });
 
-test('Review and Submit Screen check data is correct', async () => {
-  await test.step('Check title is visible', async () => {
+test('Reservation Screen check data is correct', async () => {
+  await test.step('Check titles and description', async () => {
     expect.soft(visitData.slotDetails).toBeDefined();
     await expect.soft(locator.flowHeading).toHaveText('Review and submit');
-  });
-
-  await test.step('Check description is visible', async () => {
     await expect.soft(locator.descReviewScreen).toBeVisible();
-  });
-
-  await test.step('Check patient title is visible', async () => {
-    await expect.soft(locator.titlePatient).toBeVisible();
-  });
-
-  await test.step('Check location title is visible', async () => {
-    await expect.soft(page.getByText(scheduleOwnerTypeExpected)).toBeVisible();
-  });
-
-  await test.step('Check visit details title is visible', async () => {
     await expect.soft(locator.titleVisitDetails).toBeVisible();
   });
 
-  await test.step('Check location value is visible', async () => {
-    await flowClass.checkValueIsNotEmpty(locator.locationName);
-    await expect.soft(locator.locationName).toBeVisible();
-  });
-
-  await test.step('Check prebook slot value is visible', async () => {
-    await flowClass.checkValueIsNotEmpty(locator.prebookSlotReviewScreen);
-    await expect.soft(locator.prebookSlotReviewScreen).toBeVisible();
-  });
-
-  await test.step('Check patient name is correct', async () => {
+  await test.step('Check patient details', async () => {
+    await expect.soft(locator.titlePatient).toBeVisible();
     const firstName = visitData.firstName;
     const lastName = visitData.lastName;
     await commonLocators.checkPatientNameIsCorrect({ firstName, lastName });
   });
 
-  await test.step('Check slot is correct', async () => {
-    await commonLocators.checkSlotIsCorrect(visitData.selectedSlot?.selectedSlot);
-  });
-
-  await test.step('Check location value is correct', async () => {
+  await test.step('Check location details', async () => {
+    await expect.soft(page.getByText(scheduleOwnerTypeExpected)).toBeVisible();
+    await flowClass.checkValueIsNotEmpty(locator.locationName);
+    await expect.soft(locator.locationName).toBeVisible();
     expect.soft(visitData.slotDetails?.ownerName).toBeDefined();
     await commonLocators.checkLocationValueIsCorrect(visitData.slotDetails?.ownerName ?? null);
   });
 
-  await test.step('Check privacy policy link', async () => {
-    await commonLocators.checkLinkOpensPdf(locator.privacyPolicyReviewScreen);
+  await test.step('Check slot details', async () => {
+    await flowClass.checkValueIsNotEmpty(locator.prebookSlotReviewScreen);
+    await expect.soft(locator.prebookSlotReviewScreen).toBeVisible();
+    await commonLocators.checkSlotIsCorrect(visitData.selectedSlot?.selectedSlot);
   });
 
-  await test.step('Check terms and conditions link', async () => {
+  await test.step('Check links', async () => {
+    await commonLocators.checkLinkOpensPdf(locator.privacyPolicyReviewScreen);
     await commonLocators.checkLinkOpensPdf(locator.termsAndConditions);
   });
 });
