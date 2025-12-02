6135c0aecb55ff93b1169dc4f6441fd1d4bc1ba4
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2025-12-02 08:07:45 -0500

    add labels to test, parallelize telemed homepage tests

--- apps/intake/tests/specs/in-person/ReservationScreen.spec.ts
+++ apps/intake/tests/specs/in-person/ReservationScreen.spec.ts
@@ -25,22 +25,22 @@ test.beforeAll(async ({ browser }) => {
   }
 });
 
-test('Reservation Screen check data is correct', async () => {
-  await test.step('Check titles and description', async () => {
+test('RS. Reservation Screen check data is correct', async () => {
+  await test.step('RS-1. Check titles and description', async () => {
     expect.soft(visitData.slotDetails).toBeDefined();
     await expect.soft(locator.flowHeading).toHaveText('Review and submit');
     await expect.soft(locator.descReviewScreen).toBeVisible();
     await expect.soft(locator.titleVisitDetails).toBeVisible();
   });
 
-  await test.step('Check patient details', async () => {
+  await test.step('RS-2. Check patient details', async () => {
     await expect.soft(locator.titlePatient).toBeVisible();
     const firstName = visitData.firstName;
     const lastName = visitData.lastName;
     await commonLocators.checkPatientNameIsCorrect({ firstName, lastName });
   });
 
-  await test.step('Check location details', async () => {
+  await test.step('RS-3. Check location details', async () => {
     await expect.soft(page.getByText(scheduleOwnerTypeExpected)).toBeVisible();
     await flowClass.checkValueIsNotEmpty(locator.locationName);
     await expect.soft(locator.locationName).toBeVisible();
@@ -48,13 +48,13 @@ test('Reservation Screen check data is correct', async () => {
     await commonLocators.checkLocationValueIsCorrect(visitData.slotDetails?.ownerName ?? null);
   });
 
-  await test.step('Check slot details', async () => {
+  await test.step('RS-4. Check slot details', async () => {
     await flowClass.checkValueIsNotEmpty(locator.prebookSlotReviewScreen);
     await expect.soft(locator.prebookSlotReviewScreen).toBeVisible();
     await commonLocators.checkSlotIsCorrect(visitData.selectedSlot?.selectedSlot);
   });
 
-  await test.step('Check links', async () => {
+  await test.step('RS-5. Check links', async () => {
     await commonLocators.checkLinkOpensPdf(locator.privacyPolicyReviewScreen);
     await commonLocators.checkLinkOpensPdf(locator.termsAndConditions);
   });
