d371356e2b1dd273675bad361c713a6ddab89ba4
Uladzislau Molastau <molostov.inc@gmail.com>
2025-12-09 13:18:10 +0100

    fix patientRecordPageInsuranceSection tests

--- apps/ehr/tests/e2e/specs/patientRecordPageInsuranceSection.spec.ts
+++ apps/ehr/tests/e2e/specs/patientRecordPageInsuranceSection.spec.ts
@@ -12,6 +12,7 @@ import {
   getPaymentOptionInsuranceAnswers,
   getPrimaryCarePhysicianStepAnswers,
   getResponsiblePartyStepAnswers,
+  hasEmployerInformationPage,
   isoToDateObject,
   ORG_TYPE_CODE_SYSTEM,
   ORG_TYPE_PAYER_CODE,
@@ -422,7 +423,8 @@ async function createResourceHandler(): Promise<[ResourceHandler, string, string
   let insuranceCarrier2: QuestionnaireItemAnswerOption | undefined = undefined;
   const PROCESS_ID = `patientRecordInsuranceSection-${DateTime.now().toMillis()}`;
   const resourceHandler = new ResourceHandler(PROCESS_ID, 'in-person', async ({ patientInfo }) => {
-    return [
+    const answers = [];
+    answers.push(
       getContactInformationAnswers({
         firstName: patientInfo.firstName,
         lastName: patientInfo.lastName,
@@ -468,8 +470,12 @@ async function createResourceHandler(): Promise<[ResourceHandler, string, string
       getEmployerInformationStepAnswers({}),
       getEmergencyContactStepAnswers({}),
       getConsentStepAnswers({}),
-      getPrimaryCarePhysicianStepAnswers({}),
-    ];
+      getPrimaryCarePhysicianStepAnswers({})
+    );
+    if (hasEmployerInformationPage()) {
+      answers.push(getEmployerInformationStepAnswers({}));
+    }
+    return answers;
   });
   const oystehr = await ResourceHandler.getOystehr();
   const insuranceCarriersOptions = (
