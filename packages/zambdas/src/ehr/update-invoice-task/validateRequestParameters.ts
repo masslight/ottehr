5c96029b5d5e3c13aad87d4a6a3caec23c5694b0
bholyshevskyi <114926438+bholyshevskyi@users.noreply.github.com>
2025-12-10 18:52:30 +0200

    EHR. Use user's timezone in statement PDF name and content. (#5449)

--- packages/zambdas/src/ehr/update-invoice-task/validateRequestParameters.ts
+++ packages/zambdas/src/ehr/update-invoice-task/validateRequestParameters.ts
@@ -1,24 +1,25 @@
 import {
   MISSING_REQUEST_BODY,
   MISSING_REQUEST_SECRETS,
-  PrefilledInvoiceInfo,
+  UpdateInvoiceTaskZambdaInput,
   UpdateInvoiceTaskZambdaInputSchema,
 } from 'utils';
 import { ZambdaInput } from '../../shared';
 
 export function validateRequestParameters(
   input: ZambdaInput
-): { taskId: string; status: string; prefilledInfo: PrefilledInvoiceInfo } & Pick<ZambdaInput, 'secrets'> {
+): UpdateInvoiceTaskZambdaInput & Pick<ZambdaInput, 'secrets'> {
   if (!input.body) throw MISSING_REQUEST_BODY;
   if (input.secrets == null) throw MISSING_REQUEST_SECRETS;
 
   const parsedJSON = JSON.parse(input.body);
-  const { taskId, status, prefilledInvoiceInfo } = UpdateInvoiceTaskZambdaInputSchema.parse(parsedJSON);
+  const { taskId, status, prefilledInvoiceInfo, userTimezone } = UpdateInvoiceTaskZambdaInputSchema.parse(parsedJSON);
 
   return {
     taskId,
     status,
-    prefilledInfo: prefilledInvoiceInfo,
+    prefilledInvoiceInfo,
     secrets: input.secrets,
+    userTimezone,
   };
 }
