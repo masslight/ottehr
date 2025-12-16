1531b7b5563ab306cf48a4ec6efbb46492982712
imbenham <isaac.benham@gmail.com>
2025-12-15 21:40:54 -0800

    fix test

--- apps/intake/tests/specs/in-person/PastVisits.spec.ts
+++ apps/intake/tests/specs/in-person/PastVisits.spec.ts
@@ -72,7 +72,7 @@ test.describe.parallel('Past Visits', async () => {
       await homepage.clickContinue();
     });
 
-    test('PV-2.2. Check non-empty state', async () => {
+    await test.step('PV-2.2. Check non-empty state', async () => {
       pastVisitsPage = new PastVisitsPage(page);
       await pastVisitsPage.verifyNonEmptyState();
     });
