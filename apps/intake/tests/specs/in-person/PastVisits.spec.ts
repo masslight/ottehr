224fb597a878412b66797be1961d9a93a3102e2a
imbenham <isaac.benham@gmail.com>
2025-12-15 21:18:22 -0800

    unskip pv2.2

--- apps/intake/tests/specs/in-person/PastVisits.spec.ts
+++ apps/intake/tests/specs/in-person/PastVisits.spec.ts
@@ -72,7 +72,7 @@ test.describe.parallel('Past Visits', async () => {
       await homepage.clickContinue();
     });
 
-    await test.skip('PV-2.2. Check non-empty state', async () => {
+    test('PV-2.2. Check non-empty state', async () => {
       pastVisitsPage = new PastVisitsPage(page);
       await pastVisitsPage.verifyNonEmptyState();
     });
