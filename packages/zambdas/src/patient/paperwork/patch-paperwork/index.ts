78dd681219a05734b4d2958f1eb70371f2c30bbf
Uladzislau Molastau <molostov.inc@gmail.com>
2025-12-28 23:53:10 +0100

    Complete paperwork after consent forms page submission

--- packages/zambdas/src/patient/paperwork/patch-paperwork/index.ts
+++ packages/zambdas/src/patient/paperwork/patch-paperwork/index.ts
@@ -65,6 +65,15 @@ const performEffect = async (input: PatchPaperworkEffectInput, oystehr: Oystehr)
     });
   }
 
+  // temp fix to harvest paperwork after consent forms page is complete
+  if (questionnaireResponseId === 'consent-forms-page') {
+    operations.push({
+      op: 'replace',
+      path: '/status',
+      value: 'completed',
+    });
+  }
+
   return oystehr.fhir.patch<QuestionnaireResponse>({
     id: questionnaireResponseId,
     resourceType: 'QuestionnaireResponse',
