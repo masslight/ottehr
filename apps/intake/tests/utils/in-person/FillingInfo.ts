00f7861f284ec5aa08c9d6efafe2892ccdd9e943
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2026-01-05 10:43:38 -0500

    use getByRole instead of getByText for service category selection

--- apps/intake/tests/utils/in-person/FillingInfo.ts
+++ apps/intake/tests/utils/in-person/FillingInfo.ts
@@ -143,7 +143,7 @@ export class FillingInfo {
     assert(firstCategory.display);
 
     if (firstCategory) {
-      await this.page.getByText(firstCategory.display).click();
+      await this.page.getByRole('button', { name: firstCategory.display }).click();
     }
   }
 }
