a1f7d74f99f3ce66bad45c7597559be8555e4548
bxr1nG <dvoshchuk@masslight.com>
2026-01-07 14:18:44 +0300

    Fix exams with no selected checkboxes

--- apps/ehr/src/features/visits/shared/components/exam-tab/ExamTab.tsx
+++ apps/ehr/src/features/visits/shared/components/exam-tab/ExamTab.tsx
@@ -1,9 +1,9 @@
 import { CircularProgress, Stack } from '@mui/material';
 import { FC } from 'react';
 import { AccordionCard } from 'src/components/AccordionCard';
-import { useExamObservations } from 'src/features/visits/telemed/hooks/useExamObservations';
 import { examConfig } from 'utils';
 import { useGetAppointmentAccessibility } from '../../hooks/useGetAppointmentAccessibility';
+import { useChartData } from '../../stores/appointment/appointment.store';
 import { useAppFlags } from '../../stores/contexts/useAppFlags';
 import { ExaminationContainer } from '../review-tab/components/ExaminationContainer';
 import { ExamTable } from './ExamTable';
@@ -11,13 +11,13 @@ import { ExamTable } from './ExamTable';
 export const ExamTab: FC = () => {
   const { isAppointmentReadOnly: isReadOnly } = useGetAppointmentAccessibility();
   const { isInPerson } = useAppFlags();
-  const { value: examObservations } = useExamObservations();
+  const { isChartDataLoading } = useChartData();
 
   const config = examConfig[isInPerson ? 'inPerson' : 'telemed'].default.components;
 
   return (
     <Stack direction="column" gap={1}>
-      {examObservations.length === 0 ? (
+      {isChartDataLoading ? (
         <Stack direction="row" justifyContent="center">
           <CircularProgress />
         </Stack>
