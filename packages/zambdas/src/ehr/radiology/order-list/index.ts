99432c3f0be72e30bb63b87103a98428143c2f09
szaccagni <sarahzaccagni@gmail.com>
2026-01-07 16:55:54 -0500

    small logic update for grabbing radiology final review task

--- packages/zambdas/src/ehr/radiology/order-list/index.ts
+++ packages/zambdas/src/ehr/radiology/order-list/index.ts
@@ -218,11 +218,16 @@ const parseResultsToOrder = (
 
   let status: RadiologyOrderStatus | undefined;
 
-  const myReviewTask = tasks.find(
-    (task) => task.basedOn?.some((basedOn) => basedOn.reference === `ServiceRequest/${serviceRequest.id}`)
-  );
-  console.log('myReviewTask found: ', myReviewTask?.id);
-  let finalReviewTask: DetailPageTask | undefined;
+  const finalReviewTask = tasks.find((task) => {
+    const basedOnSr = task.basedOn?.some((basedOn) => basedOn.reference === `ServiceRequest/${serviceRequest.id}`);
+    const isRadiologyTask = task.groupIdentifier?.value === RADIOLOGY_TASK.category;
+    const isFinalReview = task.code?.coding?.some(
+      (c) => c.system === RADIOLOGY_TASK.system && c.code === RADIOLOGY_TASK.code.reviewFinalResultTask
+    );
+    return basedOnSr && isRadiologyTask && isFinalReview;
+  });
+  console.log('finalReviewTask found: ', finalReviewTask?.id);
+  let formattedFinalReviewTask: DetailPageTask | undefined;
 
   // Get all diagnostic reports related to this service request
   const relatedDiagnosticReports = diagnosticReports.filter(
@@ -243,32 +248,34 @@ const parseResultsToOrder = (
   } else if (bestDiagnosticReport?.status === 'preliminary') {
     status = RadiologyOrderStatus.preliminary;
   } else if (bestDiagnosticReport?.status === 'final') {
-    if (myReviewTask?.status === 'completed') {
+    if (finalReviewTask?.status === 'completed') {
       status = RadiologyOrderStatus.reviewed;
     } else {
       status = RadiologyOrderStatus.final;
-      const orderDate = serviceRequest.extension?.find(
-        (ext) => ext.url === SERVICE_REQUEST_REQUESTED_TIME_EXTENSION_URL
-      )?.valueDateTime;
-      finalReviewTask = {
-        id: myReviewTask?.id || '',
-        category: RADIOLOGY_TASK.category,
-        createdDate: myReviewTask?.authoredOn ?? '',
-        title: 'Review Radiology Final Results',
-        subtitle: `Ordered by ${providerFirstName} ${providerLastName} on ${formatDate(
-          orderDate ?? '',
-          'MM/dd/yyyy h:mm a'
-        )}`,
-        status: myReviewTask?.status || 'unknown',
-        assignee: myReviewTask?.owner
-          ? {
-              id: myReviewTask.owner?.reference?.split('/')?.[1] ?? '',
-              name: myReviewTask.owner?.display ?? '',
-              date: getExtension(myReviewTask.owner, TASK_ASSIGNED_DATE_TIME_EXTENSION_URL)?.valueDateTime ?? '',
-            }
-          : undefined,
-        completable: true,
-      };
+      if (finalReviewTask) {
+        const orderDate = serviceRequest.extension?.find(
+          (ext) => ext.url === SERVICE_REQUEST_REQUESTED_TIME_EXTENSION_URL
+        )?.valueDateTime;
+        formattedFinalReviewTask = {
+          id: finalReviewTask?.id || '',
+          category: RADIOLOGY_TASK.category,
+          createdDate: finalReviewTask?.authoredOn ?? '',
+          title: 'Review Radiology Final Results',
+          subtitle: `Ordered by ${providerFirstName} ${providerLastName} on ${formatDate(
+            orderDate ?? '',
+            'MM/dd/yyyy h:mm a'
+          )}`,
+          status: finalReviewTask?.status || 'unknown',
+          assignee: finalReviewTask?.owner
+            ? {
+                id: finalReviewTask.owner?.reference?.split('/')?.[1] ?? '',
+                name: finalReviewTask.owner?.display ?? '',
+                date: getExtension(finalReviewTask.owner, TASK_ASSIGNED_DATE_TIME_EXTENSION_URL)?.valueDateTime ?? '',
+              }
+            : undefined,
+          completable: true,
+        };
+      }
     }
   } else {
     throw new Error('Order is in an invalid state, could not determine status.');
@@ -293,7 +300,7 @@ const parseResultsToOrder = (
     result,
     clinicalHistory,
     history,
-    task: finalReviewTask,
+    task: formattedFinalReviewTask,
   };
 };
 
