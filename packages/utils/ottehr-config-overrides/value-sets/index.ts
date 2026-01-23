2dfae1ee30e343e81975d7496344e7862ab3cb53
szaccagni <sarahzaccagni@gmail.com>
2026-01-23 15:11:17 -0500

    no valueset overrides for core

--- packages/utils/ottehr-config-overrides/value-sets/index.ts
+++ packages/utils/ottehr-config-overrides/value-sets/index.ts
@@ -1,8 +1,1 @@
-export const VALUE_SET_OVERRIDES = {
-  externalLabAdditionalCptCodesToAdd: [
-    {
-      label: 'Handling and/or conveyance of specimen for transfer to a laboratory',
-      value: '99001',
-    },
-  ],
-};
+export const VALUE_SET_OVERRIDES = {};
