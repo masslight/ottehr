92175280213ec19a5f422350410c906e3c73d363
bholyshevskyi <bholyshevskyi@masslight.com>
2026-02-26 09:50:56 +0000

    filter

--- apps/ehr/src/features/visits/in-person/pages/ProceduresNew.tsx
+++ apps/ehr/src/features/visits/in-person/pages/ProceduresNew.tsx
@@ -731,6 +731,10 @@ export default function ProceduresNew(): ReactElement {
     });
   };
 
+  const selectedProcedureTypeCode = selectOptions?.procedureTypes?.find(
+    (procedureType) => procedureType.name === formValues.procedureType
+  )?.code;
+
   return (
     <FormProvider {...methods}>
       <Stack spacing={1}>
@@ -758,7 +762,10 @@ export default function ProceduresNew(): ReactElement {
 
             {!procedureId && PROCEDURES_CONFIG.favorites.length > 0 ? (
               <SelectFromFavoritesButton
-                favorites={PROCEDURES_CONFIG.favorites}
+                favorites={PROCEDURES_CONFIG.favorites.filter(
+                  (favorite) =>
+                    selectedProcedureTypeCode == null || selectedProcedureTypeCode === favorite.procedureType
+                )}
                 getLabel={(favorite) => favorite.name}
                 onSelect={onFavoriteSelect}
               />
