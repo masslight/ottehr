507c533d4894b239826d9d3adb9ac3cf275e11ec
bholyshevskyi <bholyshevskyi@masslight.com>
2026-03-13 10:08:16 +0000

    impl

--- packages/utils/lib/types/data/tasks/types.ts
+++ packages/utils/lib/types/data/tasks/types.ts
@@ -87,3 +87,11 @@ export const RADIOLOGY_TASK = {
     studyTypeCode: 'study-type-code',
   },
 } as const;
+
+export const ERX_TASK = {
+  category: 'erx',
+  system: 'erx-task',
+  code: {
+    providerNotification: 'provider-notification',
+  },
+} as const;
