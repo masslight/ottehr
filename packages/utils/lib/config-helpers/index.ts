a168aee1c97d552e7f4f99d72887ed555b53c99e
imbenham <isaac.benham@gmail.com>
2026-03-23 23:53:43 -0700

    more type cleanup

--- packages/utils/lib/config-helpers/index.ts
+++ packages/utils/lib/config-helpers/index.ts
@@ -1,8 +1,9 @@
 export * from './helpers';
 export * from './locations';
-export { FIELDS_TO_TRACK_CLEARING, normalizeFormDataToQRItems, mapBookingQRItemToPatientInfo } from './booking';
+export * from './intake-paperwork';
+export * from './booking';
 export * from './examination';
-export { getIntakeFormPageSubtitle, buildConsentFormCheckboxItems } from './intake-paperwork';
+export * from './patient-record';
 export * from './vitals';
 
 // Form field schemas and types from config-types, surfaced via 'utils'
