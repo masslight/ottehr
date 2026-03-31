4853ae9826e633174fc2a936eaa010dfbfacc9a7
imbenham <isaac.benham@gmail.com>
2026-03-30 21:51:15 -0700

    add feature flags to ottehr-config

---
+++ packages/config-types/config/feature-flags.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+export const FeatureFlagsConfigSchema = z.object({
+  labOrdersEnabled: z.boolean().default(false),
+  inHouseLabsEnabled: z.boolean().default(false),
+  radiologyEnabled: z.boolean().default(false),
+  nursingOrdersEnabled: z.boolean().default(false),
+  supervisorApprovalEnabled: z.boolean().default(false),
+  demoVisitsEnabled: z.boolean().default(false),
+  globalTemplatesEnabled: z.boolean().default(false),
+  formsEnabled: z.boolean().default(false),
+  legacyDataEnabled: z.boolean().default(false),
+  mailingPaperStatementsEnabled: z.boolean().default(false),
+  sendgridEnabled: z.boolean().default(false),
+});
+
+export type FeatureFlagsConfig = z.infer<typeof FeatureFlagsConfigSchema>;
---
+++ packages/utils/lib/ottehr-config/feature-flags/index.ts
@@ -0,0 +1,17 @@
+import { FeatureFlagsConfigSchema } from 'config-types';
+
+const FEATURE_FLAGS_DATA = {
+  labOrdersEnabled: false,
+  inHouseLabsEnabled: false,
+  radiologyEnabled: false,
+  nursingOrdersEnabled: false,
+  supervisorApprovalEnabled: false,
+  demoVisitsEnabled: false,
+  globalTemplatesEnabled: false,
+  formsEnabled: false,
+  legacyDataEnabled: false,
+  mailingPaperStatementsEnabled: false,
+  sendgridEnabled: false,
+};
+
+export const FEATURE_FLAGS_CONFIG = Object.freeze(FeatureFlagsConfigSchema.parse(FEATURE_FLAGS_DATA));
