5422dd444802f96d490e75129a862dca309ea9c6
Valerii Diachenko <vdyachenko@masslight.com>
2026-03-31 19:32:51 +0400

    fix: remove duplicated import

--- apps/ehr/src/components/AppointmentTableRow.tsx
+++ apps/ehr/src/components/AppointmentTableRow.tsx
@@ -44,7 +44,6 @@ import {
   getVisitTotalTime,
   GetVitalsResponseData,
   InPersonAppointmentInformation,
-  makeAbbreviation,
   mdyStringFromISOString,
   NON_LOS_STATUSES,
   OrdersForTrackingBoardRow,
