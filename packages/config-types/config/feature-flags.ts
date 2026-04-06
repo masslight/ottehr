5c543427ba0f4aa1b11fd00889e90941273b6158
imbenham <isaac.benham@gmail.com>
2026-04-06 16:15:47 -0700

    remove default vals from feature flags schema, enforce their explicit provision

--- packages/config-types/config/feature-flags.ts
+++ packages/config-types/config/feature-flags.ts
@@ -1,19 +1,19 @@
 import z from 'zod';
 
 export const FeatureFlagsConfigSchema = z.object({
-  labOrdersEnabled: z.boolean().default(false),
-  inHouseLabsEnabled: z.boolean().default(false),
-  radiologyEnabled: z.boolean().default(false),
-  nursingOrdersEnabled: z.boolean().default(false),
-  supervisorApprovalEnabled: z.boolean().default(false),
-  demoVisitsEnabled: z.boolean().default(false),
-  globalTemplatesEnabled: z.boolean().default(false),
-  formsEnabled: z.boolean().default(false),
-  legacyDataEnabled: z.boolean().default(false),
-  mailingPaperStatementsEnabled: z.boolean().default(false),
-  legacyPatientFollowupsEnabled: z.boolean().default(false),
-  skipSendingVisitNoteToPatientPortalEnabled: z.boolean().default(false),
-  sendgridEnabled: z.boolean().default(false),
+  labOrdersEnabled: z.boolean(),
+  inHouseLabsEnabled: z.boolean(),
+  radiologyEnabled: z.boolean(),
+  nursingOrdersEnabled: z.boolean(),
+  supervisorApprovalEnabled: z.boolean(),
+  demoVisitsEnabled: z.boolean(),
+  globalTemplatesEnabled: z.boolean(),
+  formsEnabled: z.boolean(),
+  legacyDataEnabled: z.boolean(),
+  mailingPaperStatementsEnabled: z.boolean(),
+  legacyPatientFollowupsEnabled: z.boolean(),
+  skipSendingVisitNoteToPatientPortalEnabled: z.boolean(),
+  sendgridEnabled: z.boolean(),
 });
 
 export type FeatureFlagsConfig = z.infer<typeof FeatureFlagsConfigSchema>;
