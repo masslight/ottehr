3c2ad99b75ad34588e86eadfda20be1b5126e76a
bholyshevskyi <bholyshevskyi@masslight.com>
2026-05-15 15:12:35 +0000

    fix

--- packages/zambdas/src/ehr/get-appointments/helpers.ts
+++ packages/zambdas/src/ehr/get-appointments/helpers.ts
@@ -138,7 +138,11 @@ export const getTimezone = async ({
       } else {
         console.error(`timezone not set for ${resourceId}`);
       }
-    } catch (e) {
+    } catch (e: any) {
+      if (e?.code === 404) {
+        console.log(`resource "${resourceType}/${resourceId}" not found`, e);
+        return undefined;
+      }
       console.log('error getting location', JSON.stringify(e));
       throw new Error('location is not found');
     }
