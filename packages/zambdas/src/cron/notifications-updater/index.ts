3b6b539c64b60ce7fe6d5f743f2de3277c7d1922
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2026-05-25 12:26:20 -0400

    add unit tests

--- packages/zambdas/src/cron/notifications-updater/index.ts
+++ packages/zambdas/src/cron/notifications-updater/index.ts
@@ -332,21 +332,14 @@ export const index = wrapHandler('notification-Updater', async (input: ZambdaInp
         let title = 'A new task has been assigned to you: ' + (task.description ?? `task ID ${task.id}`);
         let status = getCommunicationStatus(notificationSettings!, busyPractitionerIds, recipient);
 
-        // todo: if we remove busy practitioner filtering, rework this logic
-        // waiting room practitioners will always become "busy" (status "preparation"), so we force
-        // "in-progress" to ensure they receive the notification. similarly, practitioners
-        // prescribing eRX are assigned to an appointment already. phone-only notifications require
-        // a status of "completed" but phone and computer ones require "in-progress".
-        const newStatus =
-          notificationSettings!.method === ProviderNotificationMethod.phone ? 'completed' : 'in-progress';
         switch (taskCode) {
           case VIDEO_CHAT_WAITING_ROOM_NOTIFICATION_TASK_CODE: {
             title = task.description ?? `task ID ${task.id}`;
-            status = newStatus;
+            status = getStatusOverridingBusy(notificationSettings!);
             break;
           }
           case ERX_TASK.code.providerNotification: {
-            status = newStatus;
+            status = getStatusOverridingBusy(notificationSettings!);
             break;
           }
           default: {
@@ -852,6 +845,15 @@ function getCommunicationStatus(
     status = 'completed';
   }
   return status;
+}
+
+// todo: if we remove busy practitioner filtering, rework this logic
+// waiting room practitioners will always become "busy" (status "preparation"), so we force
+// "in-progress" to ensure they receive the notification. similarly, practitioners prescribing eRX
+// are assigned to an appointment already. phone-only notifications require a status of "completed"
+// but phone and computer ones require "in-progress".
+export function getStatusOverridingBusy(notificationSettings: ProviderNotificationSettings): Communication['status'] {
+  return notificationSettings.method === ProviderNotificationMethod.phone ? 'completed' : 'in-progress';
 }
 
 const getTelemedEncounterAppointmentId = (encounterResource: Resource): string | undefined => {
