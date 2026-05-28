3ac430f9865fa7e6175ccde484eebc47c63e1bf1
Your Name <darielurbina@gmail.com>
2026-05-27 20:04:07 -0400

    reverted not specified on issue

--- apps/ehr/src/features/visits/shared/components/review-tab/components/PatientInformationContainer.tsx
+++ apps/ehr/src/features/visits/shared/components/review-tab/components/PatientInformationContainer.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material';
 import { DateTime } from 'luxon';
 import { FC } from 'react';
 import { getPatientName } from 'src/shared/utils';
-import { getPatientFriendlyId, getQuestionnaireResponseByLinkId } from 'utils';
+import { getQuestionnaireResponseByLinkId } from 'utils';
 import { useAppointmentData } from '../../../stores/appointment/appointment.store';
 import { VisitNoteItem } from '../../VisitNoteItem';
 
@@ -10,7 +10,6 @@ export const PatientInformationContainer: FC = () => {
   const { patient, questionnaireResponse } = useAppointmentData();
   const patientName = getPatientName(patient?.name).fullDisplayName;
   const dob = patient?.birthDate && DateTime.fromFormat(patient.birthDate, 'yyyy-MM-dd').toFormat('MM/dd/yyyy');
-  const patientId = patient ? getPatientFriendlyId(patient) : undefined;
   const phone = getQuestionnaireResponseByLinkId('guardian-number', questionnaireResponse)?.answer?.[0]?.valueString;
 
   const accompanyingPerson = {
@@ -28,7 +27,6 @@ export const PatientInformationContainer: FC = () => {
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 0.5 }}>
         <VisitNoteItem label="Patient name" value={patientName} />
         <VisitNoteItem label="Date of birth" value={dob} />
-        <VisitNoteItem label="PID" value={patientId} />
         {accompanyingPerson.firstName && accompanyingPerson.lastName && (
           <VisitNoteItem
             label="Person accompanying the minor patient"
