ddc17e97dedf29517988aed4c69840dca0aa351e
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2026-06-01 12:56:24 -0400

    create progress note config types and extensions

---
+++ packages/utils/lib/types/api/progress-note-config/progress-note-config.types.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+import { Secrets } from '../../../secrets';
+
+export interface ProgressNoteConfig {
+  mdmRequired: boolean;
+}
+
+export type GetProgressNoteConfigInput = Record<string, never>;
+export type GetProgressNoteConfigOutput = ProgressNoteConfig;
+
+export const UpdateProgressNoteConfigInputSchema = z.object({
+  mdmRequired: z.boolean(),
+});
+export type UpdateProgressNoteConfigInput = z.infer<typeof UpdateProgressNoteConfigInputSchema>;
+
+export const UpdateProgressNoteConfigInputValidatedSchema = UpdateProgressNoteConfigInputSchema.extend({
+  secrets: z.custom<Secrets>().nullable(),
+});
+export type UpdateProgressNoteConfigInputValidated = z.infer<typeof UpdateProgressNoteConfigInputValidatedSchema>;
