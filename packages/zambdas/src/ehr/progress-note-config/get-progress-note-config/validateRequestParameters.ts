8a00d0cd64c1a56149cf010ba3e4358ba1095fda
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2026-06-02 11:15:27 -0400

    remove unused usertoken check

--- packages/zambdas/src/ehr/progress-note-config/get-progress-note-config/validateRequestParameters.ts
+++ packages/zambdas/src/ehr/progress-note-config/get-progress-note-config/validateRequestParameters.ts
@@ -1,15 +1,11 @@
 import { MISSING_AUTH_TOKEN, Secrets } from 'utils';
 import { ZambdaInput } from '../../../shared';
 
-export function validateRequestParameters(input: ZambdaInput): { secrets: Secrets | null; userToken: string } {
+export function validateRequestParameters(input: ZambdaInput): { secrets: Secrets | null } {
   if (input.headers.Authorization === undefined) {
-    throw MISSING_AUTH_TOKEN;
-  }
-  const userToken = input.headers.Authorization.replace('Bearer ', '');
-  if (!userToken) {
     throw MISSING_AUTH_TOKEN;
   }
 
   const secrets = input.secrets;
-  return { secrets, userToken };
+  return { secrets };
 }
