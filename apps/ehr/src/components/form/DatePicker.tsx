57b01c735fddb1bed7565acb156128b6a0376283
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2026-06-24 20:00:55 +0300

    fix pr comments

--- apps/ehr/src/components/form/DatePicker.tsx
+++ apps/ehr/src/components/form/DatePicker.tsx
@@ -117,6 +117,7 @@ export function BasicDatePicker({
                     },
                   }}
                   disablePast={disablePast}
+                  minDate={minDate ? dayjs(minDate) : undefined}
                 />
               );
             }
