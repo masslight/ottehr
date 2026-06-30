be50b019c605fdc01889189f149696bf19dc68a7
bholyshevskyi <bholyshevskyi@masslight.com>
2026-06-30 12:44:20 +0000

    cahnges after review

--- apps/billing/src/components/AlertDialog.tsx
+++ apps/billing/src/components/AlertDialog.tsx
@@ -1,5 +1,5 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 
 interface Props {
   title: string;
@@ -9,18 +9,16 @@ interface Props {
 
 export default function AlertDialog({ title, text, onClose }: Props): ReactElement {
   return (
-    <React.Fragment>
-      <Dialog open={true} onClose={onClose} maxWidth={false} PaperProps={{ sx: { width: 680, maxWidth: '95vw' } }}>
-        <DialogTitle>{title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>{text}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={onClose} autoFocus>
-            Ok
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+    <Dialog open={true} onClose={onClose} maxWidth={false} PaperProps={{ sx: { width: 680, maxWidth: '95vw' } }}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{text}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} autoFocus>
+          Ok
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
