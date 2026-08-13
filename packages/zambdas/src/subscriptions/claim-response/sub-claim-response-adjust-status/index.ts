f9e4d500237fe45e9605b00c8a4061a61d77e0a3
Colin <colin.t.welch@gmail.com>
2026-08-12 20:24:13 -0400

    feat(billing): Subscription to update claim status on match (#9113)
    * feat(billing): Subscription to update claim status on match
    
    * tweak(billing): Patch claim ins-ar-status w/ opt locking

---
+++ packages/zambdas/src/subscriptions/claim-response/sub-claim-response-adjust-status/index.ts
@@ -0,0 +1,77 @@
+import Oystehr, { FhirResourceReturnValue } from '@oystehr/sdk';
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { Claim, ClaimResponse } from 'fhir/r4b';
+import { AR_STAGE, CLAIM_STATUS_TAG_SYSTEMS, getPatchOperationForNewMetaTag, patchWithOptimisticLock } from 'utils';
+import { createBillingClient, getTag } from '../../../billing/shared';
+import { checkOrCreateM2MClientToken, wrapHandler, ZambdaInput } from '../../../shared';
+import { validateRequestParameters } from './validateRequestParameters';
+
+const ZAMBDA_NAME = 'sub-claim-response-adjust-status';
+
+let m2mToken: string;
+
+export const index = wrapHandler(ZAMBDA_NAME, async (input: ZambdaInput): Promise<APIGatewayProxyResult> => {
+  console.group('validateRequestParameters');
+  const params = validateRequestParameters(input);
+  const { secrets, ...restOfParams } = params;
+  console.groupEnd();
+  console.debug('validateRequestParameters success', restOfParams);
+
+  m2mToken = await checkOrCreateM2MClientToken(m2mToken, secrets);
+  const oystehr = createBillingClient(m2mToken, secrets);
+
+  console.group('complexValidation');
+  const validated = await complexValidation(oystehr, params.claimResponseId);
+  console.groupEnd();
+
+  console.group('performEffect');
+  const response = await performEffect(oystehr, validated);
+  console.groupEnd();
+  console.debug('performEffect success', response);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+});
+
+export interface ComplexValidationOutput {
+  claimResponseId: string;
+  claimResponse: FhirResourceReturnValue<ClaimResponse>;
+  claim: FhirResourceReturnValue<Claim>;
+}
+
+export async function complexValidation(oystehr: Oystehr, claimResponseId: string): Promise<ComplexValidationOutput> {
+  const claimResponse = await oystehr.fhir.get<ClaimResponse>({ resourceType: 'ClaimResponse', id: claimResponseId });
+  if (!claimResponse.request?.reference) {
+    throw new Error(`Subscription called for ClaimResponse without 'request'`);
+  }
+  const claim = await oystehr.fhir.get<Claim>({
+    resourceType: 'Claim',
+    id: claimResponse.request.reference.replace('Claim/', ''),
+  });
+
+  return {
+    claim,
+    claimResponse,
+    claimResponseId,
+  };
+}
+
+export async function performEffect(oystehr: Oystehr, validated: ComplexValidationOutput): Promise<void> {
+  const { claim } = validated;
+  const arStage = getTag(claim, CLAIM_STATUS_TAG_SYSTEMS.arStage);
+  if (arStage !== AR_STAGE.insurancePayer) {
+    return;
+  }
+  const insuranceArStatus = getTag(claim, CLAIM_STATUS_TAG_SYSTEMS.insuranceArStatus);
+  if (insuranceArStatus === 'adjudicated') {
+    return;
+  }
+  await patchWithOptimisticLock(oystehr, claim, (claim) => [
+    getPatchOperationForNewMetaTag(claim, {
+      system: CLAIM_STATUS_TAG_SYSTEMS.insuranceArStatus,
+      code: 'adjudicated',
+    }),
+  ]);
+}
