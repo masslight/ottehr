ca9d82fddc54d71deeec6ee3dd1d47f82b168a6c
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2026-09-17 13:07:26 -0400

    fix tests

--- apps/billing/tests/component/ClaimsList.test.tsx
+++ apps/billing/tests/component/ClaimsList.test.tsx
@@ -62,7 +62,7 @@ vi.mock('../../src/utils/pollExportTask', () => ({
   pollExportTask: pollExportTaskMock,
 }));
 
-vi.mock('../../src/utils/downloadTextFile', () => ({
+vi.mock('../../src/utils/downloadFile', () => ({
   downloadTextFile: downloadTextFileMock,
 }));
 
